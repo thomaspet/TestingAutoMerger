43debdf981275fa04a3aa8335fcfd2bdd36af35a
jorge <jorge@unimicro.no>
2015-11-27 15:50:39 +0100

    hide bug from conflict between jquery and angular2 cssSelector

--- src/framework/controls/autocomplete/autocomplete.ts
+++ src/framework/controls/autocomplete/autocomplete.ts
@@ -29,7 +29,8 @@ export class Autocomplete implements AfterViewInit {
 		
 		// Update control value and set validSelection to true. Select event only fires when input text is valid.
 		options.select = function(event: kendo.ui.AutoCompleteSelectEvent) {
-			var dataItem = this.dataItem(event.item.index());
+			var item: any = event.item;
+			var dataItem = this.dataItem(item.index());
 			control.updateValue(dataItem.toJSON());
 			validSelection = true;
 		};
