cd449bf18d4d79c5b3656b70da35bdeb8f28f765
unknown <jorgen.kristiansen@jorgenk10.modalen.unimicro.no>
2015-12-10 15:48:06 +0100

    feat(navbar tabstrip component): First draft

--- src/app/components/navbar/tabstrip/tabstrip.ts
+++ src/app/components/navbar/tabstrip/tabstrip.ts
@@ -1,5 +1,5 @@
 import {Component, AfterViewInit} from 'angular2/angular2';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
 import {Routes, APP_ROUTES} from '../../../route.config';
 
 @Component({
@@ -8,9 +8,37 @@ import {Routes, APP_ROUTES} from '../../../route.config';
 	directives: [ROUTER_DIRECTIVES]
 })
 export class Tabstrip implements AfterViewInit {
-	public routes = Routes;
-	
-	constructor() {}
-	
+    public routes = Routes;
+    openTabs: Array<any>;
+
+    constructor(public router: Router) {
+        this.openTabs = this.getOpenTabs();
+    }
+
+    getOpenTabs(): Array<any> {
+        return [
+            { name: 'Dashboard', unsavedWork: true, href: '/' },
+            { name: 'UniFromDemo', unsavedWork: false, href: '/uniformdemo' },
+        ];
+    }
+
+    addNewTab(newTab): void {
+        //this.openTabs.push(newTab);
+        this.openTabs.push({ name: 'Kitchensink', unsavedWork: false, href: '/kitchensink' });
+    }
+
+    tabClicked(tab): void {
+        this.router.navigateByUrl(tab.href);
+    }
+
+    closeTab(tab, index): void {
+        if (tab.unsavedWork) {
+            //Needs custom alert box with options? 
+            alert('The tab you are about to close has unsaved work. Would you like to dismiss this work?');
+        } else {
+            this.openTabs.splice(index, 1);
+        }
+    }
+
 	ngAfterViewInit() {}
 }
