907d62c6caada6db435c1e1712e3fb008588bcc3
Jørgen Kristiansen <jorgen.kristiansen@unimicro.no>
2015-12-14 15:37:27 +0100

    feat(navbar hamburger dropdown menu): First draft

--- src/app/components/navbar/hamburgerMenu/hamburgerMenu.ts
+++ src/app/components/navbar/hamburgerMenu/hamburgerMenu.ts
@@ -1,6 +1,7 @@
 import {Component, AfterViewInit} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Routes, APP_ROUTES} from '../../../route.config';
+declare var jQuery;
 
 @Component({
 	selector: 'uni-hamburger-menu',
@@ -8,10 +9,18 @@ import {Routes, APP_ROUTES} from '../../../route.config';
 	directives: [ROUTER_DIRECTIVES]
 })
 export class HamburgerMenu implements AfterViewInit {
-	public routes = Routes;
+    public routes = Routes;
 	
-	constructor() {
-	}
-	
+    constructor() {
+        jQuery('.listElement').mouseover(function (e) {
+            jQuery(this).addClass('is-active');
+            jQuery(this).siblings().removeClass('is-active');
+        });
+    }
+
+    openBurgerMenu(): void {
+        jQuery('.navbar_hamburger').toggleClass('is-active');
+    }
+
 	ngAfterViewInit() {}
 }
