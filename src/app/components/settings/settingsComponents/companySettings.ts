676438046a17741a7821caaad0e15494d475b817
Jørgen Kristiansen <jorgen.kristiansen@unimicro.no>
2016-01-13 15:04:26 +0100

    feat(company settings): implemented formbuilder for the company settings object

--- src/app/components/settings/settingsComponents/companySettings.ts
+++ src/app/components/settings/settingsComponents/companySettings.ts
@@ -9,6 +9,7 @@ import {UniForm, FIELD_TYPES} from '../../../../framework/forms/uniForm';
 import {UniFormBuilder} from "../../../../framework/forms/uniFormBuilder";
 import {UniFieldsetBuilder} from "../../../../framework/forms/uniFieldsetBuilder";
 import {UniFieldBuilder} from "../../../../framework/forms/uniFieldBuilder";
+import {UNI_CONTROL_TYPES} from '../../../../framework/controls/types';
 
 @Component({
     selector: 'settings',
@@ -20,69 +21,84 @@ export class CompanySettings implements OnInit {
 
     id: any;
     form: any;
+    headers: Headers;
     company: any;
     activeCompany: any;
 
-    constructor(public http: Http, public formBuilder: FormBuilder) {
-        this.company = {};
-        this.form = [];
-    }
+    constructor(public http: Http) { }
 
     ngOnInit() {
 
         //ID of active company used to GET company settings
         this.id = JSON.parse(localStorage.getItem('activeCompany')).id;
 
-        //Dummy method for dev. REMOVE!!
-        if (this.id !== 1) {
-            this.id = 1;
-        }
+        //Dummy method before real companies is fetched! REMOVE!!
+        //if (this.id !== 1) {
+        //    this.id = 1;
+        //}
 
-        var headers = new Headers();
-        headers.append('Client', 'client1');
+        this.headers = new Headers();
+        this.headers.append('Client', 'client1');
 
         var url = 'http://devapi.unieconomy.no:80/api/biz/companysettings/' + this.id + '?expand=Address,Emails,Phones';
 
         //Gets settings for the company currently active
-        this.http.get(url, { headers: headers })
+        this.http.get(url, { headers: this.headers })
             .map(res => res.json())
-            .subscribe(data => this.dateIsReady(data))
+            .subscribe(data => this.dataReady(data));
+
     }
 
     //Called when data is returned from the API
-    dateIsReady(data) {
+    dataReady(data) {
         this.company = data;
 
-        var model = {};
-
-        var formBuilder = new FormBuilder()
-
-        var name = new UniFieldBuilder()
-            .setLabel('TESTING')
-            .setModel(model)
-            .setModelField('name')
-            .setType('Text');
-
-        this.formBuilder.addFields(name);
-
-        var organizationNumber = new UniFieldBuilder()
-            .setLabel('ORG-NUMBER')
-            .setModel(model)
-            .setModelField('name')
-            .setType('TEXT')
-
-        this.formBuilder.addFields(organizationNumber);
-
         console.log(this.company);
+        
+        var formBuilder = new UniFormBuilder();
 
         for (var value in this.company) {
-            //Finds arrays inside object
+
             if (typeof (this.company[value]) === 'object') {
                 //Loops through array 
                 for (var val in this.company[value]) {
-                    console.log(this.company[value][val]);
+                    var tempFieldset = new UniFieldsetBuilder();
+                    for (var v in this.company[value][val]) {
+                        if (v.slice(-2) !== 'ID') {
+                            var temp = new UniFieldBuilder();
+                            temp.setLabel(v)
+                                .setModel(this.company[value][val])
+                                .setModelField(v)
+                                .setType(UNI_CONTROL_TYPES.TEXT);
+                            
+                            tempFieldset.addField(temp);
+                        }
+                    }
+                    formBuilder.addField(tempFieldset);
+                }
+            } else {
+                if (value.slice(-2) !== 'ID') {
+                    var temp = new UniFieldBuilder();
+                    temp.setLabel(value)
+                        .setModel(this.company)
+                        .setModelField(value)
+                        .setType(UNI_CONTROL_TYPES.TEXT);
+                    
+                    formBuilder.addField(temp);
                 }
             } 
         }
+        console.log(formBuilder);
+
+        this.form = formBuilder;
+    }
+
+    submitForm() {
+        console.log(this.company);
+        this.http.put(
+            'http://devapi.unieconomy.no:80/api/biz/companysettings',
+            JSON.stringify(this.company),
+            { headers: this.headers })
+            .map(res => console.log(res.json()))
     }
 }
