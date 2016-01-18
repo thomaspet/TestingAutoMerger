71a87954688b5a1f4214c3966b2a4d40038cd32e
Jørgen Kristiansen <jorgen.kristiansen@unimicro.no>
2016-01-18 15:24:38 +0100

    Added empty component for accounts to fill out and test navigation menu

---
+++ src/app/components/settings/settingsComponents/accountSettings.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'account-settings',
+    templateUrl: 'app/components/settings/settingsComponents/accountSettings.html'
+})
+
+export class AccountSettings {
+    constructor() { }
+}
