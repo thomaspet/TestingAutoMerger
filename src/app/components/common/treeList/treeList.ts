fb4d0efed74ab096131cc3d71b4e953a84fe0d6f
Jørgen Kristiansen <jorgen.kristiansen@unimicro.no>
2016-01-25 10:46:05 +0100

    Added hide/expand all function

--- src/app/components/common/treeList/treeList.ts
+++ src/app/components/common/treeList/treeList.ts
@@ -13,17 +13,29 @@ declare var jQuery;
 export class TreeList {
     
     @Input() directories: Array<Directory>;
-    compRef: any;
-    current;
-    count = 0;
+    expanded: boolean = false;
 
     constructor(public dynamicComponentLoader: DynamicComponentLoader, public elementRef: ElementRef) { }
 
     showContent(event) {
         //Slides up all .content_div
         jQuery('#tree_list').find('.content_div').slideUp(500);
-
         //Slides down current .content_div
         jQuery(event.target).next('.content_div').slideDown(500);
+    }
+
+    showHideAll() {
+        this.expanded = !this.expanded;
+        this.revertAllExpanded(this.directories);
+    }
+
+    revertAllExpanded(dir) {
+        //Needs to set explicitly true or false??
+        dir.forEach((d) => {
+            d.expanded = this.expanded;
+            if (d.directories) {
+                this.revertAllExpanded(d.directories);
+            }
+        })
     }
 }
