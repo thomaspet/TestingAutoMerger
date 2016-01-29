ff39446d057026e4d44b5c414174f40f271ba2c4
Anders Urrang <anders@urrang.no>
2016-01-29 12:26:16 +0100

    feat(signup): Testing post to resource

--- src/app/components/login/signup.ts
+++ src/app/components/login/signup.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {Http, Headers, Response} from 'angular2/http';
 
 @Component({
     selector: 'uni-signup',
@@ -15,7 +16,7 @@ export class Signup {
         password: string    
     }
     
-    constructor() {
+    constructor(private http: Http) {
         this.newUser = {
             companyName: '',
             name: '',
@@ -27,8 +28,25 @@ export class Signup {
     
     signUp(event) {
         event.preventDefault();
-        // post @ api
-        // authenticate
-        console.log(this.newUser);
+        
+        var serializedParams = 'company-name=' + this.newUser.companyName +
+            '&name=' + this.newUser.name + '&email=' + this.newUser.email +
+            '&user-name=' + this.newUser.username + '&password=' + this.newUser.password;
+        
+        // POST to api endpoint. Should use http service when its ready 
+        this.http.post('http://devapi.unieconomy.no:80/api/biz/companies', 
+            serializedParams,
+            { headers: new Headers({'Client': 'client1'}) } 
+        )
+        .subscribe(
+            (response) => {
+                console.log(response);
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+        
+        // authenticate with the newly created user, or wait for email confirmation?
     }
 }
