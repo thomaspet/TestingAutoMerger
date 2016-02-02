1063bf01c34f73dd1ca856c96ac177a24f79c947
Arild <Arild@unimicro.no>
2016-02-02 10:47:22 +0100

    feat(employment) init #713

--- src/app/components/employee/employments/employments.ts
+++ src/app/components/employee/employments/employments.ts
@@ -1,9 +1,53 @@
-import {Component} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+import {Component, Injector, OnInit} from 'angular2/core';
+import {UniForm} from '../../../../framework/forms/uniForm';
+import {EmployeeDS} from '../../../../framework/data/employee';
+import {UNI_CONTROL_TYPES} from '../../../../framework/controls/types';
+import {UniFormBuilder} from '../../../../framework/forms/uniFormBuilder';
+import {UniFieldBuilder} from '../../../../framework/forms/uniFieldBuilder';
 
 @Component({
     selector: 'employee-employment',
-    template: '<p>This is the employment child-view</p>'
+    templateUrl: 'app/components/employee/employments/employments.html'
 })
 export class Employment {
-    constructor() {}
+    currentEmployee;
+    employmentList;
+    form: UniFormBuilder;
+    
+    constructor(private Injector:Injector, employeeDS:EmployeeDS) {
+        let params = Injector.parent.parent.get(RouteParams);
+        employeeDS.get(params.get('id'))
+        .subscribe(response => {
+            this.currentEmployee = response;
+            this.employmentList = this.currentEmployee.Employments;
+            
+            console.log(this.currentEmployee);
+            
+        },error => console.log(error));
+    }
+    
+    buildForm(employment) {
+        var form = new UniFormBuilder();
+    
+        var jobCode = new UniFieldBuilder();
+        jobCode.setLabel('stillingskode')
+        .setModel(employment)
+        .setModelField('jobcode')
+        .setType(UNI_CONTROL_TYPES.TEXT);
+    
+        var jobName = new UniFieldBuilder();
+        jobName.setLabel('Navn')
+        .setModel(employment)
+        .setModelField('jobname')
+        .setType(UNI_CONTROL_TYPES.TEXT)
+    
+        form.addFields(jobCode, jobName);
+        
+        return form;
+    }
+    
+    onsubmit(data) {
+        this.employmentList = data;
+    }
 }
