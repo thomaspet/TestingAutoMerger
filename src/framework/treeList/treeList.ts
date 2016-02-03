02d2c47d94a757e32df05c2a066a81182bedef8f
unknown <jorgen.kristiansen@jorgenk10.modalen.unimicro.no>
2016-02-03 13:08:34 +0100

    feat(treelist component): Rewrite and renamed showContent => showHideContent

--- src/framework/treeList/treeList.ts
+++ src/framework/treeList/treeList.ts
@@ -19,33 +19,31 @@ export class TreeList {
     type = TREE_LIST_TYPE;
     current: any;
     count: number = 0;
+    allContentDivHidden: boolean = true;
 
     constructor(public elementRef: ElementRef) { }
 
-    showContent(event) {
-        //Slides up all .content_div
-        jQuery('.tree_list').find('.content_div').slideUp(500);
-
-        //Slides down current .content_div
-        if (this.current !== event.target || this.count === 2) {
-            jQuery(event.target).next('.content_div').slideDown(500);
-            this.count = 0;
-        }
-
-        this.current = event.target;
-        this.count++;
+    showHideContent(event) {
+        jQuery(event.target).next('.content_div').slideToggle(500);
     }
 
     showHideAll() {
         this.expanded = !this.expanded;
-        this.revertAllExpanded(this.treeListItems);
+        
+        //Slides all content up if user closes the whole list.. If this is not a good idea, just remove!!
+        if (!this.expanded) {
+            jQuery('.tree_list').find('.content_div').slideUp(500);
+        }
+
+        this.updateExpandedState(this.treeListItems);
+        
     }
 
-    revertAllExpanded(items) {
+    updateExpandedState(items) {
         items.forEach((item) => {
             item.expanded = this.expanded;
             if (item.treeListItems) {
-                this.revertAllExpanded(item.treeListItems);
+                this.updateExpandedState(item.treeListItems);
             }
         })
     }
