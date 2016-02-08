3aef9d4b3eab3b5bc442fe3ceeac9ba818bc808b
Jørgen Kristiansen <j.kristiansen19@gmail.com>
2016-02-08 16:09:44 +0100

    Implemented post to user-vertification

--- src/app/components/settings/users/users.ts
+++ src/app/components/settings/users/users.ts
@@ -12,26 +12,37 @@ declare var jQuery;
 
 export class Users {
 
-    newUser = {};
+    newUser;
     success: boolean = false;
     users: Array<any> = [];
     usersConfig: UniTableConfig;
 
     constructor(private http: UniHttpService) {
+        this.newUser = {};
+        this.newUser.CompanyId = JSON.parse(localStorage.getItem('activeCompany')).id;
+        this.createUserTable();
+    }
+
+    createUserTable() {
         this.usersConfig = new UniTableConfig('http://devapi.unieconomy.no/api/biz/users', false, false)
-            .setDsModel({
-                fields: {
-                    Name: { type: 'text' },
-                    Email: { type: 'text' },
-                    Username: { type : 'text'}
-                }
-            })
-        
+
             .setColumns([
                 { field: 'Name', title: 'Navn' },
                 { field: 'Email', title: 'Epost' },
                 { field: 'Username', title: 'Brukernavn' },
+                { field: 'Status', title: 'Status' },
+                { field: 'Role', title: 'Rolle' }
             ]);
+    }
 
+    inviteNewUser() {
+        this.http.get({ resource: '/users'})
+            .subscribe(
+            (data) => {
+                console.log(data);
+                this.createUserTable();
+            },
+            error => console.log(error)
+        )
     }
 }
