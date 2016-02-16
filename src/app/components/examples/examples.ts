85977f650c274a667a0502f8e8a9bb87a85bbcf1
jorge <jorgeparent@gmail.com>
2016-02-16 12:11:19 +0100

    remove spinner since we just need an aria-busy attribute and some css

--- src/app/components/examples/examples.ts
+++ src/app/components/examples/examples.ts
@@ -1,31 +1,29 @@
-import {Component} from 'angular2/core';
-import {RouteConfig, RouteDefinition, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Component} from "angular2/core";
+import {RouteConfig, RouteDefinition, ROUTER_DIRECTIVES} from "angular2/router";
 
-import {UniTabs} from '../layout/uniTabs/uniTabs';
-import {UniFormDemo} from './form/formDemo';
-import {UniTableDemo} from './table/tableDemo';
-import {UniTreelistDemo} from './treelist/treelistDemo';
-import {SpinnerExample} from "./spinner/spinner";
+import {UniTabs} from "../layout/uniTabs/uniTabs";
+import {UniFormDemo} from "./form/formDemo";
+import {UniTableDemo} from "./table/tableDemo";
+import {UniTreelistDemo} from "./treelist/treelistDemo";
 
 const CHILD_ROUTES = [
-    {path: '/', redirectTo: ['./UniFormDemo']},
-    {path: '/form', component: UniFormDemo , as: 'UniFormDemo'},
-    {path: '/table', component: UniTableDemo , as: 'UniTableDemo'},
-    {path: '/treelist', component: UniTreelistDemo , as: 'UniTreelistDemo'},
-    {path: '/spinner', component: SpinnerExample , as: 'SpinnerExample'},
+    {path: "/", redirectTo: ["./UniFormDemo"]},
+    {path: "/form", component: UniFormDemo, as: "UniFormDemo"},
+    {path: "/table", component: UniTableDemo, as: "UniTableDemo"},
+    {path: "/treelist", component: UniTreelistDemo, as: "UniTreelistDemo"},
 ];
 
 @RouteConfig(CHILD_ROUTES)
 @Component({
-    selector: 'uni-examples',
-    templateUrl: 'app/components/examples/examples.html',
+    selector: "uni-examples",
+    templateUrl: "app/components/examples/examples.html",
     directives: [ROUTER_DIRECTIVES, UniTabs]
 })
 export class Examples {
     childRoutes: RouteDefinition[];
-    
+
     constructor() {
         this.childRoutes = CHILD_ROUTES.slice(1); // we dont want the redirect route to be included in the navbar 
     }
-    
+
 }
