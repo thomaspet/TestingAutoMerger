4afeb2263cf218e71a7505edab2be51c4d79ba5b
Terje Pedersen <terje@senikk.com>
2016-02-19 16:01:15 +0100

    added treelist

--- src/app/components/settings/vatsettings/vattypelist/vatTypeList.ts
+++ src/app/components/settings/vatsettings/vattypelist/vatTypeList.ts
@@ -1,9 +1,11 @@
-import {Component} from 'angular2/core';
+import {Component, ViewChild} from 'angular2/core';
 import {IVatType} from '../../../../../framework/interfaces/interfaces';
 import {VatTypeService} from '../../../../services/services';
 import {UNI_CONTROL_DIRECTIVES} from '../../../../../framework/controls';
 import {UNI_CONTROL_TYPES} from '../../../../../framework/controls/types';
 import {UniForm, UniFormBuilder, UniFieldsetBuilder, UniFieldBuilder, UniComboFieldBuilder, UniSectionBuilder} from '../../../../../framework/forms';
+import {UniTable, UniTableBuilder, UniTableColumn} from '../../../../../framework/uniTable';
+import {TreeList, TreeListItem, TREE_LIST_TYPE} from "../../../../../framework/treeList";
 
 @Component({
     selector: 'vattype-list',
@@ -11,15 +13,43 @@ import {UniForm, UniFormBuilder, UniFieldsetBuilder, UniFieldBuilder, UniComboFi
     providers: [VatTypeService]
 })
 export class VatTypeList {
+    @ViewChild(TreeList) treeList: TreeList;
+    vatListItems: TreeListItem[] = [];
+    vatgroups;
+
+    constructor(private vatTypeService: VatTypeService) {}
     
-    constructor(private vatTypeService: VatTypeService) {
-        
+    loopGroups() {
+        this.vatgroups.forEach((vatgroup: any) => {           
+            console.log(vatgroup);
+            
+            var nameCol = new UniTableColumn('Name', 'Navn', 'string'); 
+                        
+            var tableConfig = new UniTableBuilder("vattype", false)
+            .setPageSize(10)
+            .addColumns(nameCol)
+       
+            var list = new TreeListItem(vatgroup.Name)
+                .setType(TREE_LIST_TYPE.TABLE)
+                .setContent(tableConfig);
+                
+            this.vatListItems.push(list);
+        });
     }
+
     
     ngOnInit() {
         console.log('vatlist initializing');   
         
-        //var vattype = this.vatTypeService.Get(XX);             
+        this.vatgroups = [
+            {ID: 1, No: 1, Name: "Innenlands omsetning og uttak" },
+            {ID: 2, No: 2, Name: "Utførsel" }
+        ];
+        
+        this.loopGroups();
+        //var vattype = this.vatTypeService.Get(XX);    
+        
+                 
     }    
 }
     
