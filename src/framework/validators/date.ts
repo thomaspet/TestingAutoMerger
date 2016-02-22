28593242a24e13f55f1a5d7bf0519fa9fd622922
jorge <jorgeparent@gmail.com>
2016-02-22 15:52:46 +0100

    we can add now validations from layout

--- src/framework/validators/date.ts
+++ src/framework/validators/date.ts
@@ -4,60 +4,60 @@ declare var _;
 
 export function isDate(value: any, key: string) {
     "use strict";
-    return function (c: Control): {[key:string]: boolean} {
+    return function (c: Control): any {
         if (_.isDate(c.value)) {
             return null;
         }
-        return {
-            key: true
-        };
+        let error = {};
+        error[key] = true;
+        return error;
     };
 }
 
 export function isDateGreaterThan(value: any, key: string) {
     "use strict";
-    return function (c: Control): {[key: string]: boolean} {
+    return function (c: Control): any {
         if (_.isDate(c.value) && c.value.getTime() > value.getTime()) {
             return null;
         }
-        return {
-            key: true
-        };
+        let error = {};
+        error[key] = true;
+        return error;
     };
 }
 
 export function isDateGreaterThanEqual(value: any, key: string) {
     "use strict";
-    return function (c: Control): {[key: string]: boolean} {
+    return function (c: Control): any {
         if (_.isDate(c.value) && c.value.getTime() >= value.getTime()) {
             return null;
         }
-        return {
-            key: true
-        };
+        let error = {};
+        error[key] = true;
+        return error;
     };
 }
 
 export function isDateLowerThan(value: any, key: string) {
     "use strict";
-    return function (c: Control): {[key: string]: boolean} {
+    return function (c: Control): any {
         if (_.isDate(c.value) && c.value.getTime() < value.getTime()) {
             return null;
         }
-        return {
-            key: true
-        };
+        let error = {};
+        error[key] = true;
+        return error;
     };
 }
 
 export function isDateLowerThanEqual(value: any, key: string) {
     "use strict";
-    return function (c: Control): {[key: string]: boolean} {
+    return function (c: Control): any {
         if (_.isDate(c.value) && c.value.getTime() <= value.getTime()) {
             return null;
         }
-        return {
-            key: true
-        };
+        let error = {};
+        error[key] = true;
+        return error;
     };
 }
