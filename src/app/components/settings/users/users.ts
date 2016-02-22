b6bff615ba11f7652c879f95afa2a7f721379bcc
Jørgen Kristiansen <j.kristiansen19@gmail.com>
2016-02-22 15:27:18 +0100

    Rewrite of users, from unitable to list. New Pipe, to sort list

--- src/app/components/settings/users/users.ts
+++ src/app/components/settings/users/users.ts
@@ -1,12 +1,41 @@
-﻿import {Component, ViewChild} from 'angular2/core';
+﻿import {Component, ViewChild, Pipe} from 'angular2/core';
 import {NgFor, NgIf} from 'angular2/common';
 import {UniTable, UniTableBuilder, UniTableColumn} from '../../../../framework/uniTable';
 import {UniHttpService} from '../../../../framework/data/uniHttpService';
 
 declare var jQuery;
 
+@Pipe({
+    name: 'sort'
+})
+
+export class OrderByPipe {
+    transform(value, property) {
+        return value.sort(this.dynamicSort(property[0]));
+        
+    }
+
+    dynamicSort(property) {
+        var sortOrder = 1;
+
+        if (property !== undefined) {
+            if (property.charAt(0) === '-') {
+                sortOrder = -1;
+                property = property.substr(1);
+            }
+        }
+        
+
+        return function (a, b) {
+            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortOrder;  
+        }
+    }
+}
+
 @Component({
     selector: 'uni-users',
+    pipes: [OrderByPipe],
     templateUrl: 'app/components/settings/users/users.html',
     directives: [NgFor, NgIf, UniTable]
 })
@@ -20,6 +49,7 @@ export class Users {
     errorMessage: string;
     invalidInviteInfo: boolean = false;
     isPostUserActive: boolean = false;
+    sortProperty: string;
 
     constructor(private http: UniHttpService) {
         this.newUser = {};
@@ -29,22 +59,36 @@ export class Users {
 
     createUserTable() {
 
-        var nameCol = new UniTableColumn('DisplayName', 'Navn', 'string');
+        var nameCol = new UniTableColumn('DisplayName', 'Navn', 'string').setEditable(true);
         var emailCol = new UniTableColumn('Email', 'Epost', 'string');
         var statusCol = new UniTableColumn('Status', 'Status', 'string');
-        var roleCol = new UniTableColumn('Role', 'Rolle', 'string');
-        var commandCol = new UniTableColumn('', '', '')
-            .setCommand(
-            [
-                { name: "destroy" } // built-in "destroy" command
-            ]
-            );
+        //var roleCol = new UniTableColumn('Role', 'Rolle', 'string');
+        var self = this;
            
-        
         this.usersTable = new UniTableBuilder(this.users, false)
             .setPageSize(2)
             .setPageable(false)
-            .addColumns(nameCol, emailCol, statusCol, roleCol, commandCol)
+            .addColumns(nameCol, emailCol, statusCol)
+            .addCommands(
+            {
+                name: 'Resend',
+                click: function (e) {
+                    e.preventDefault();
+                    var tr = jQuery(e.target).closest("tr");
+                    self.resendInvite(this.dataItem(tr));
+                },
+                className: 'btn_resend'
+            },
+            {
+                name: 'Revoke',
+                click: function (e) {
+                    e.preventDefault();
+                    var tr = jQuery(e.target).closest("tr");
+                    self.revokeInvite(this.dataItem(tr));
+                },
+                className: 'btn_revoke'
+            }
+            )
             .setFilterable(false)
     }
 
@@ -53,24 +97,22 @@ export class Users {
     setUserTableData() {
 
         var status = this.statusDummy();
-        var role = this.roleDummy();
         this.http.get({ resource: 'users' })
             .subscribe(
                 (users) => {
-                    this.users = users;
+                    
 
                     users.forEach((user) => {
-                        user.Status = status[status.map((st) => { return st.ID }).indexOf(user.StatusID)].Name;
-                        if (user.Status === 'Invited') {
-                            user.Role = role[Math.floor(Math.random() * 6)].Name;
-                        }
+                        user.Status = status[status.map((st) => { return st.ID }).indexOf(user.StatusID + (Math.floor(Math.random()* 3)))].Name;
                     })
 
-                    if (this.usersTable) {
-                        this.userTable.refresh(users);
-                    } else {
-                        this.createUserTable();
-                    }
+                    //if (this.usersTable) {
+                    //    this.userTable.refresh(users);
+                    //} else {
+                    //    this.createUserTable();
+                    //}
+
+                    this.users = users;
                 },
                 (error) => {
                     //Error handling
@@ -105,39 +147,54 @@ export class Users {
         }
     }
 
+    resendInvite(user) {
+        this.newUser.DisplayName = user.DisplayName;
+        this.newUser.Email = user.Email;
+        this.inviteNewUser();
+    }
+
+    /*
+        Should cancel current active user-verification invite..
+        Should set status to Invoked/Removed/Invactive?
+     */
+    revokeInvite(user) {
+        var revoke = confirm('Er du sikker på at du ønsker å fjerne rettighetene til ' + user.DisplayName + '?');
+
+        if (revoke) {
+            console.log(user.DisplayName + '\'s rights have been revoked..');
+        }
+    }
+
     validateEmail(email) {
         return /[^\s@]+@[^\s@]+\.[^\s@]+/.test(email);
     }
 
+    sortList(sortValue) {
+        if (sortValue === this.sortProperty) {
+            var tempstring = '-' + this.sortProperty;
+            this.sortProperty = tempstring;
+        } else {
+            this.sortProperty = sortValue;
+        }
+    }
+
     statusDummy() {
         return [
-            {
-                Name: 'Invited',
-                ID: 17
-            },
-            {
-                Name: 'Active',
-                ID: 18
-            },
-            {
-                Name: 'Inactive',
-                ID: 19
-            },
-            {
-                Name: 'Revoked',
-                ID: 20
-            }
+            { Name: 'Invitert', ID: 17 },
+            { Name: 'Aktiv', ID: 18 },
+            { Name: 'Inaktiv', ID: 19 },
+            { Name: 'Trukket godkjenning', ID: 20 }
         ]
     }
 
     roleDummy() {
         return [
             { Name: 'Admin' },
-            { Name: 'User(#845)' },
-            { Name: 'User(#323)' },
-            { Name: 'User(#523)' },
-            { Name: 'General User' },
-            { Name: 'Temporary' },
+            { Name: 'Bruker(#845)' },
+            { Name: 'Bruker(#323)' },
+            { Name: 'Bruker(#523)' },
+            { Name: 'Generell bruker' },
+            { Name: 'Midlertidig' },
         ]
     }
 }
