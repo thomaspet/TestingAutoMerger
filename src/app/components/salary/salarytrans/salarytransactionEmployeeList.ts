893e8b6490f9cd4d2c549221aafc92b6dfd00433
Arild Soltvedt <arild@unimicro.no>
2016-02-22 12:27:57 +0100

    fix(salarytransemployee) update trans when employeeNumber change

--- src/app/components/salary/salarytrans/salarytransactionEmployeeList.ts
+++ src/app/components/salary/salarytrans/salarytransactionEmployeeList.ts
@@ -1,39 +1,59 @@
-import {Component} from "angular2/core";
+import {Component, Input} from "angular2/core";
 import {Router, RouteConfig} from "angular2/router";
 import {UniTable, UniTableBuilder, UniTableColumn} from "../../../../framework/uniTable";
 
 @Component({
     selector: "salary-transactions-employee",
     templateUrl: "app/components/salary/salarytrans/salarytransactionEmployeeList.html",
-    directives: [UniTable],
-    inputs: ["ansatt"]
+    directives: [UniTable]
+    //,inputs: ["ansattID"]
 })
 
 export class SalaryTransactionEmployeeList {
     salarytransEmployeeTableConfig;
+    salarytransFilterString: string;
+    @Input() ansattID: number = 0;
     
     constructor(router: Router) {
         
-        var idCol = new UniTableColumn("ID","ID","number");
-        var wagetypeidCol = new UniTableColumn("wagetype.wagetypeid","Lønnsart","number");
-        var wagetypenameCol = new UniTableColumn("text","Tekst","string");
-        var fromdateCol = new UniTableColumn("","Dato fra-til","datetime");
-        var rateCol = new UniTableColumn("rate","Sats","number");
-        var amountCol = new UniTableColumn("amount","Antall","number");
-        var sumCol = new UniTableColumn("sum","Beløp","number");
-        var employmentidCol = new UniTableColumn("employmentID","Arb.forhold ID","number");
-        var accountCol = new UniTableColumn("account","Konto","string");
-        var payoutCol = new UniTableColumn("wagetype.base_payment","Utbetales","bool");
-        var transtypeCol = new UniTableColumn("","Fast/Variabel post","string");
-        
-        this.salarytransEmployeeTableConfig = new UniTableBuilder("salarytrans",false)
-        .setExpand("wagetype")
-        .addColumns(idCol, wagetypeidCol, wagetypenameCol, 
-            fromdateCol, 
-            rateCol, amountCol, sumCol, employmentidCol, 
-            accountCol, payoutCol, transtypeCol);
     }
     
+    ngOnInit() {
+        this.createTableConfig();
+    }
     
+    ngOnChanges() {
+        console.log("onChange: ansattID",this.ansattID);
+        //update filter in unitable
+        this.salarytransFilterString = "EmployeeNumber eq " + this.ansattID;
+    }
+    
+    createTableConfig() {
+        
+        //this.ansattID = ansattID;
+        
+        var idCol = new UniTableColumn("ID","ID","number");
+        //var wagetypeidCol = new UniTableColumn("wagetype.wagetypeid","Lønnsart","number");
+        //var wagetypenameCol = new UniTableColumn("text","Tekst","string");
+        //var fromdateCol = new UniTableColumn("","Dato fra-til","datetime");
+        var rateCol = new UniTableColumn("Rate","Sats","number");
+        // var amountCol = new UniTableColumn("amount","Antall","number");
+        // var sumCol = new UniTableColumn("sum","Beløp","number");
+        // var employmentidCol = new UniTableColumn("employmentID","Arb.forhold ID","number");
+        // var accountCol = new UniTableColumn("account","Konto","string");
+        // var payoutCol = new UniTableColumn("wagetype.base_payment","Utbetales","bool");
+        // var transtypeCol = new UniTableColumn("","Fast/Variabel post","string");
+        
+        this.salarytransEmployeeTableConfig = new UniTableBuilder("employees",false)
+        //.setExpand("wagetype")
+        //.setFilter("EmployeeNumber eq " + this.ansattID)
+        .addColumns(idCol
+            //, wagetypeidCol, wagetypenameCol, 
+            //fromdateCol, 
+            ,rateCol 
+            //, amountCol, sumCol, employmentidCol, 
+            //accountCol, payoutCol, transtypeCol
+            );
+    }
     
 }
--- src/app/components/salary/salarytrans/salarytransactionSelectionList.ts
+++ src/app/components/salary/salarytrans/salarytransactionSelectionList.ts
@@ -10,8 +10,8 @@ import {SalaryTransactionEmployeeList} from "./salarytransactionEmployeeList";
 
 export class SalaryTransactionSelectionList {
     salarytransSelectionTableConfig;
-    allEmployees;
-    selectedEmployee;
+    //allEmployees;
+    selectedEmployeeID:number;
     
     constructor(router: Router) {
         this.createTableConfig();
@@ -20,29 +20,32 @@ export class SalaryTransactionSelectionList {
     createTableConfig() {
         var idCol = new UniTableColumn("ID","ID","number");
         var employeenumberCol = new UniTableColumn("EmployeeNumber","Ansattnr.","number");
-        //var nameCol = new UniTableColumn("BusinessRelationInfo.Name","Navn","string");
+        var nameCol = new UniTableColumn("BusinessRelationInfo.Name","Navn","string");
         //var bankaccountCol = new UniTableColumn("BankAccounts[0].AccountNumber","Bankkonto","string");
         var taxcardCol = new UniTableColumn("TaxTable","Skattekort","bool");
         var forpayoutCol = new UniTableColumn("Pay","Beløp til utbetaling","number");
         //var localizationCol = new UniTableColumn("Localization.BusinessRelationInfo.Name","Lokasjon","string");
         var leaveCol = new UniTableColumn("Leave","Permisjon","datetime");
         
-        this.allEmployees = [
-          {ID: 1, Employee: 1, Name: "Ben", Account: "1234.56.78910", Tax: true, Pay: 90, Location: "Stadt", Leave: ""},
-          {ID: 2, Employee: 20, Name: "Arild", Account: "1111.22.33333", Tax: true, Pay: 1000000, Location: "Bergen", Leave: "01.01.2016 - 31.03.2016"},
-          {ID: 3, Employee: 33, Name: "Sondre", Account: "0198.76.54321", Tax: false, Pay: 150, Location: "Modalen", Leave: ""},
-          {ID: 4, Employee: 49, Name: "Vibeke", Account: "4444.55.66666", Tax: false, Pay: 6879, Location: "Modalen", Leave: ""},  
-        ];
+        // this.allEmployees = [
+        //   {ID: 1, Employee: 1, Name: "Ben", Account: "1234.56.78910", Tax: true, Pay: 90, Location: "Stadt", Leave: ""},
+        //   {ID: 2, Employee: 20, Name: "Arild", Account: "1111.22.33333", Tax: true, Pay: 1000000, Location: "Bergen", Leave: "01.01.2016 - 31.03.2016"},
+        //   {ID: 3, Employee: 33, Name: "Sondre", Account: "0198.76.54321", Tax: false, Pay: 150, Location: "Modalen", Leave: ""},
+        //   {ID: 4, Employee: 49, Name: "Vibeke", Account: "4444.55.66666", Tax: false, Pay: 6879, Location: "Modalen", Leave: ""},  
+        // ];
         
         //this.salarytransSelectionTableConfig = new UniTableBuilder(this.allEmployees,false)
         this.salarytransSelectionTableConfig = new UniTableBuilder("employees",false)
         .setExpand("BusinessRelationInfo")
+        .setFilter("BusinessRelationID gt 0")
         .setSelectCallback((selEmp) => {
-            this.selectedEmployee = selEmp;
+            this.selectedEmployeeID = selEmp.EmployeeNumber;
+            console.log("valgt ansattID", this.selectedEmployeeID);
             console.log("ansatt valgt",selEmp);
         })
         .addColumns(idCol, employeenumberCol, 
-            //nameCol, bankaccountCol, 
+            nameCol, 
+            //bankaccountCol, 
             taxcardCol, forpayoutCol, 
             //localizationCol, 
             leaveCol);
