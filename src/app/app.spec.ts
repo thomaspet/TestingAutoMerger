653534c7fee8f5763007b6ab8f5ab2a3f481c8cc
jorge <jorge@unimicro.no>
2016-03-04 16:22:41 +0100

    feat(testing): LoadComponentAsync works in test environment
    if window.TEST_MODE is true in the escope ComponentProxy try to load the ts file instead of the dist js file
    
    ref: #202

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -3,22 +3,25 @@ import { describe, it, expect, beforeEachProviders, beforeEach, inject} from 'an
 import { MockBackend, MockConnection} from 'angular2/http/testing';
 import {provide} from 'angular2/core';
 import {Http, Response, BaseRequestOptions, ResponseOptions} from 'angular2/http';
-import {Router, RouterOutlet, RouterLink, RouteParams, RouteData, Location, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
+import {Router, Location, ROUTER_PRIMARY_COMPONENT} from 'angular2/router';
 
 import {App} from './app';
 import {AuthService} from '../framework/authentication/authService';
 
+//NOT PUBLIC ANGULAR PACKAGES
 import {RouteRegistry} from 'angular2/src/router/route_registry';
 import { SpyLocation } from 'angular2/src/mock/location_mock';
 import {RootRouter} from 'angular2/src/router/router';
 
 describe('App', () => {
     beforeEachProviders(() => [
+        //HOW TO INITIALIZE ROUTER
         RouteRegistry,
         provide(Location, {useClass: SpyLocation}),
         provide(ROUTER_PRIMARY_COMPONENT, {useValue: App}),
         provide(Router, {useClass: RootRouter}),
 
+        //HOW TO INITIALIZE HTTP SERVICE
         AuthService,
         BaseRequestOptions,
         MockBackend,
@@ -30,11 +33,13 @@ describe('App', () => {
         })
     ]);
 
+    //HOW TO MOCK HTTP RESPONSE
     beforeEach(inject([MockBackend], (backend: MockBackend) => {
         const baseResponse = new Response(new ResponseOptions({body: 'got response'}));
         backend.connections.subscribe((c: MockConnection) => c.mockRespond(baseResponse));
     }));
 
+    //TEST
     it('is initialized', inject([AuthService, Router], (authService: AuthService, router: Router) => {
         var app: App = new App(authService, router);
         expect(app.routes).toBeDefined();
--- src/framework/core/componentProxy.ts
+++ src/framework/core/componentProxy.ts
@@ -2,7 +2,13 @@
 export class ComponentProxy {
 
     static LoadComponentAsync(name,path){
-        return System.import(path).then(c => c[name]);
+        if (!window.TEST_MODE) {
+            return System.import(path)
+                .then(c => c[name]);
+        } else {
+            var newpath = path.replace('.',"./src");
+            return System.import(newpath).then(c => c[name]);
+        }
     }
 
 }
