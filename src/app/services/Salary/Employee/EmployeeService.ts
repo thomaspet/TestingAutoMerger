c39aa1473f75a13bd21370e6998833946b62dcde
jorge <jorgeparent@gmail.com>
2016-03-07 15:11:26 +0100

    refactor(EmployeeService): remove Injectable decorator. ref: #199

--- src/app/services/Salary/Employee/EmployeeService.ts
+++ src/app/services/Salary/Employee/EmployeeService.ts
@@ -1,9 +1,7 @@
-import {Injectable} from 'angular2/core';
 import {BizHttp} from '../../../../framework/core/http/BizHttp';
 import {UniHttp} from '../../../../framework/core/http/http';
 import {IEmployee} from '../../../../framework/interfaces/interfaces';
 
-@Injectable()
 export class EmployeeService extends BizHttp<IEmployee> {
 
     constructor(http:UniHttp) {
