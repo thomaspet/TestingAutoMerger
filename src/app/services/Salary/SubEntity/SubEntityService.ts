0e42887a0773ba37a5a61588803f0f37e574d2b1
Sondre Eide Vik <sondre.vik@unimicro.no>
2016-03-15 14:32:29 +0100

    Feature(SubEntity): add SubEntity service
    ref #246

---
+++ src/app/services/Salary/SubEntity/SubEntityService.ts
@@ -0,0 +1,7 @@
+import {BizHttp} from '../../../../framework/core/http/BizHttp';
+import {UniHttp} from '../../../../framework/core/http/http';
+import {SubEntity} from '../../../unientities';
+
+export class SubEntityService extends BizHttp<SubEntity> {
+    
+}
