648034b29a30cff40f662cef36e4fe9cb10295b1
Arild <Arild@unimicro.no>
2016-03-22 09:26:50 +0100

    feat(staticRegister): json stringify and parse
    ref #191

--- src/app/components/salary/employee/employments/employments.ts
+++ src/app/components/salary/employee/employments/employments.ts
@@ -1,170 +1,176 @@
-import {RouteParams} from "angular2/router";
-import {Component, Injector} from "angular2/core";
-import {EmployeeDS} from "../../../../data/employee";
-import {STYRKCodesDS} from "../../../../data/styrkCodes";
-import {UNI_CONTROL_DIRECTIVES} from "../../../../../framework/controls";
-import {FieldType} from "../../../../unientities";
+import {RouteParams} from 'angular2/router';
+import {Component, Injector} from 'angular2/core';
+import {EmployeeDS} from '../../../../data/employee';
+import {STYRKCodesDS} from '../../../../data/styrkCodes';
+import {UNI_CONTROL_DIRECTIVES} from '../../../../../framework/controls';
+import {FieldType} from '../../../../unientities';
 import {
     UniForm, UniFormBuilder, UniFieldBuilder, UniSectionBuilder, UniFieldsetBuilder
-} from "../../../../../framework/forms";
-import {Observable} from "rxjs/Observable";
-import {Employment} from "../../../../unientities";
-import {UniElementFinder} from "../../../../../framework/forms/shared/UniElementFinder";
+} from '../../../../../framework/forms';
+import {Observable} from 'rxjs/Observable';
+import {Employment} from '../../../../unientities';
+import {UniElementFinder} from '../../../../../framework/forms/shared/UniElementFinder';
 import {StaticRegisterService} from '../../../../services/staticregisterservice';
+import {Employee} from '../../../../unientities';
 
 declare var jQuery;
 
 @Component({
-    selector: "employee-employment",
+    selector: 'employee-employment',
     directives: [UniForm],
-    templateUrl: "app/components/salary/employee/employments/employments.html",
+    templateUrl: 'app/components/salary/employee/employments/employments.html',
     providers: [StaticRegisterService]
     
 })
+
 export class EmployeeEmployment {
-    currentEmployee;
-    formConfigs: UniFormBuilder[];
-    styrkCodes;
-    staticRegisterService: StaticRegisterService;
-    styrk;
+    private currentEmployee: Employee;
+    private formConfigs: UniFormBuilder[];
+    // private styrkCodes: Array<any>;
+    // private staticRegisterService: StaticRegisterService;
+    private styrks: any;
 
-    typeOfEmployment: Array<any> = [
-        {ID: 0, Navn: "Ikke satt"},
-        {ID: 1, Navn: "1 - Ordinært arbeidsforhold"},
-        {ID: 2, Navn: "2 - Maritimt arbeidsforhold"},
-        {ID: 3, Navn: "3 - Frilanser, oppdragstager, honorar"},
-        {ID: 4, Navn: "4 - Pensjon og annet uten ansettelse"}
+    private typeOfEmployment: Array<any> = [
+        {ID: 0, Navn: 'Ikke satt'},
+        {ID: 1, Navn: '1 - Ordinært arbeidsforhold'},
+        {ID: 2, Navn: '2 - Maritimt arbeidsforhold'},
+        {ID: 3, Navn: '3 - Frilanser, oppdragstager, honorar'},
+        {ID: 4, Navn: '4 - Pensjon og annet uten ansettelse'}
     ];
 
-    renumerationType: Array<any> = [
-        {ID: 0, Navn: "Udefinert"},
-        {ID: 1, Navn: "1 - Fast lønnet"},
-        {ID: 2, Navn: "2 - Timelønnet"},
-        {ID: 3, Navn: "3 - Provisjonslønnet"},
-        {ID: 4, Navn: "4 - Honorar"},
-        {ID: 5, Navn: "5 - Akkord"}
+    private renumerationType: Array<any> = [
+        {ID: 0, Navn: 'Udefinert'},
+        {ID: 1, Navn: '1 - Fast lønnet'},
+        {ID: 2, Navn: '2 - Timelønnet'},
+        {ID: 3, Navn: '3 - Provisjonslønnet'},
+        {ID: 4, Navn: '4 - Honorar'},
+        {ID: 5, Navn: '5 - Akkord'}
     ];
 
-    workingHoursScheme: Array<any> = [
-        {ID: 0, Navn: "Udefinert"},
-        {ID: 1, Navn: "1 - Ikke skiftarbeid"},
-        {ID: 2, Navn: "2 - Arbeid offshore"},
-        {ID: 3, Navn: "3 - Helkontinuerlig skiftarbeid"},
-        {ID: 4, Navn: "4 - Døgnkontinuerlig skiftarbeid"},
-        {ID: 5, Navn: "5 - 2 skiftarbeid"}
+    private workingHoursScheme: Array<any> = [
+        {ID: 0, Navn: 'Udefinert'},
+        {ID: 1, Navn: '1 - Ikke skiftarbeid'},
+        {ID: 2, Navn: '2 - Arbeid offshore'},
+        {ID: 3, Navn: '3 - Helkontinuerlig skiftarbeid'},
+        {ID: 4, Navn: '4 - Døgnkontinuerlig skiftarbeid'},
+        {ID: 5, Navn: '5 - 2 skiftarbeid'}
     ];
 
-    shipType: Array<any> = [
-        {ID: 0, Navn: "Udefinert"},
-        {ID: 1, Navn: "1 - Annet"},
-        {ID: 2, Navn: "2 - Boreplattform"},
-        {ID: 3, Navn: "3 - Turist"}
-    ];
+    // private shipType: Array<any> = [
+    //     {ID: 0, Navn: 'Udefinert'},
+    //     {ID: 1, Navn: '1 - Annet'},
+    //     {ID: 2, Navn: '2 - Boreplattform'},
+    //     {ID: 3, Navn: '3 - Turist'}
+    // ];
 
-    shipReg: Array<any> = [
-        {ID: 0, Navn: "Udefinert"},
-        {ID: 1, Navn: "1 - Norsk Internasjonalt skipsregister"},
-        {ID: 2, Navn: "2 - Norsk ordinært skipsregister"},
-        {ID: 3, Navn: "3 - Utenlandsk skipsregister"}
-    ];
+    // private shipReg: Array<any> = [
+    //     {ID: 0, Navn: 'Udefinert'},
+    //     {ID: 1, Navn: '1 - Norsk Internasjonalt skipsregister'},
+    //     {ID: 2, Navn: '2 - Norsk ordinært skipsregister'},
+    //     {ID: 3, Navn: '3 - Utenlandsk skipsregister'}
+    // ];
 
-    tradeArea: Array<any> = [
-        {ID: 0, Navn: "Udefinert"},
-        {ID: 1, Navn: "1 - Innenriks"},
-        {ID: 2, Navn: "2 - Utenriks"}
-    ];
+    // private tradeArea: Array<any> = [
+    //     {ID: 0, Navn: 'Udefinert'},
+    //     {ID: 1, Navn: '1 - Innenriks'},
+    //     {ID: 2, Navn: '2 - Utenriks'}
+    // ];
 
-    subEntities: Array<any> = [
-        {ID: 0, Navn: "Modalen"},
-        {ID: 1, Navn: "Haugesund"},
-        {ID: 2, Navn: "Bergen"},
-        {ID: 3, Navn: "Oslo"},
-        {ID: 4, Navn: "Trondheim"},
-        {ID: 5, Navn: "Stavanger"},
-        {ID: 6, Navn: "Bodø"}
-    ];
+    private subEntities: Array<any> = [];
 
-    constructor(private Injector: Injector, public employeeDS: EmployeeDS, public styrkcodesDS: STYRKCodesDS, public statReg: StaticRegisterService) {
+    constructor(private injector: Injector, 
+                public employeeDS: EmployeeDS, 
+                public styrkcodesDS: STYRKCodesDS, 
+                public statReg: StaticRegisterService) {
         
-        this.statReg.checkForStaticRegisterUpdate();
+        this.styrks = this.statReg.getStaticRegisterDataset('styrk');
+        let params = injector.parent.parent.get(RouteParams);
         
-        let params = Injector.parent.parent.get(RouteParams);
         Observable.forkJoin(
-            employeeDS.get(params.get("id")),
-            styrkcodesDS.getCodes(),
+            employeeDS.get(params.get('id')),
             employeeDS.getSubEntities()
         ).subscribe((response: any) => {
-            let [employee, codes, subEnt] = response;
-            console.log("SubEntities from constructor", subEnt);
+            let [employee, subEnt] = response;
             this.currentEmployee = employee;
-            this.styrkCodes = codes;
             this.subEntities = subEnt;
             this.buildFormConfigs();
         }, (error: any) => console.log(error));
     }
 
-    buildFormConfigs() {
+    private buildFormConfigs(): void {
         this.formConfigs = [];
         
-        //this.statReg.checkForStaticRegisterUpdate();
-
         this.currentEmployee.Employments.forEach((employment: Employment) => {
             var formbuilder = new UniFormBuilder();
-            var AddSubEntity = false;
+            var addSubEntity = false;
 
             var jobCode = this
-                .buildField("Stillingskode", employment, "JobCode", FieldType.AUTOCOMPLETE)
+                .buildField('Stillingskode', employment, 'JobCode', FieldType.AUTOCOMPLETE)
                 .setKendoOptions({
-                    //dataSource: this.styrkCodes,
-                    dataSource: this.statReg.getStaticRegisterDataset("styrk"),
-                    dataTextField: "styrk",
-                    dataValueField: "styrk"
+                    dataSource: this.styrks,
+                    dataTextField: 'styrk',
+                    dataValueField: 'styrk'
                 });
             jobCode.onSelect = (event: kendo.ui.AutoCompleteSelectEvent) => {
                 var item: any = event.item;
                 var dataItem = event.sender.dataItem(item.index());
-                var fjn = <UniFieldBuilder>UniElementFinder.findUniFieldByPropertyName("JobName", formbuilder.config());
+                var fjn = <UniFieldBuilder>UniElementFinder
+                .findUniFieldByPropertyName('JobName', formbuilder.config());
                 fjn.control.updateValue(dataItem.tittel, {});
             };
 
-            var jobName = this.buildField("Navn", employment, "JobName", FieldType.AUTOCOMPLETE)
+            var jobName = this.buildField('Navn', employment, 'JobName', FieldType.AUTOCOMPLETE)
                 .setKendoOptions({
-                    dataSource: this.styrkCodes,
-                    dataTextField: "tittel",
-                    dataValueField: "tittel"
+                    dataSource: this.styrks,
+                    dataTextField: 'tittel',
+                    dataValueField: 'tittel'
                 });
             jobName.onSelect = (event: kendo.ui.AutoCompleteSelectEvent) => {
                 var item: any = event.item;
                 var dataItem = event.sender.dataItem(item.index());
-                var fjc = <UniFieldBuilder>UniElementFinder.findUniFieldByPropertyName("JobCode", formbuilder.config());
+                var fjc = <UniFieldBuilder>UniElementFinder
+                .findUniFieldByPropertyName('JobCode', formbuilder.config());
                 fjc.control.updateValue(dataItem.styrk, {});
             };
 
-            var startDate = this.buildField("Startdato", employment, "StartDate", FieldType.DATEPICKER);
-            var endDate = this.buildField("Sluttdato", employment, "EndDate", FieldType.DATEPICKER);
-            var monthRate = this.buildField("Månedlønn", employment, "MonthRate", FieldType.NUMERIC);
-            var hourRate = this.buildField("Timelønn", employment, "HourRate", FieldType.NUMERIC);
-            var workPercent = this.buildField("Stillingprosent", employment, "WorkPercent", FieldType.NUMERIC);
+            var startDate = this.buildField('Startdato', employment, 'StartDate'
+                , FieldType.DATEPICKER);
+            var endDate = this.buildField('Sluttdato', employment, 'EndDate'
+                , FieldType.DATEPICKER);
+            var monthRate = this.buildField('Månedlønn', employment, 'MonthRate'
+                , FieldType.NUMERIC);
+            var hourRate = this.buildField('Timelønn', employment, 'HourRate', FieldType.NUMERIC);
+            var workPercent = this.buildField('Stillingprosent', employment, 'WorkPercent'
+                , FieldType.NUMERIC);
 
             if (typeof employment.SubEntity) {
                 if (typeof employment.SubEntity.BusinessRelationInfo) {
                     var subEntity = this
-                        .buildField("Lokasjon", employment.SubEntity.BusinessRelationInfo, "Name", FieldType.COMBOBOX);
+                        .buildField('Lokasjon', employment.SubEntity.BusinessRelationInfo, 'Name', 
+                            FieldType.COMBOBOX);
+                            
                     subEntity.setKendoOptions({
                         dataSource: this.subEntities,
-                        dataTextField: "BusinessRelationInfo.Name",
-                        dataValueField: "ID"
+                        dataTextField: 'BusinessRelationInfo.Name',
+                        dataValueField: 'ID'
                     });
-                    AddSubEntity = true;
+                    
+                    addSubEntity = true;
                 }
             }
 
             var readgroup = this.buildGroupForm(employment);
 
-            if (AddSubEntity) {
-                formbuilder.addUniElements(jobCode, jobName, startDate, endDate, monthRate, hourRate, workPercent, subEntity, readgroup);
+            if (addSubEntity) {
+                formbuilder.addUniElements(
+                    jobCode, jobName, startDate, endDate, monthRate
+                    , hourRate, workPercent, subEntity, readgroup
+                    );
             } else {
-                formbuilder.addUniElements(jobCode, jobName, startDate, endDate, monthRate, hourRate, workPercent, readgroup);
+                formbuilder.addUniElements(
+                    jobCode, jobName, startDate, endDate, monthRate
+                    , hourRate, workPercent, readgroup
+                    );
             }
 
 
@@ -174,46 +180,54 @@ export class EmployeeEmployment {
 
     }
 
-    buildGroupForm(employment: Employment) {
-        var groupBuilder = new UniSectionBuilder("Vis mer");
+    private buildGroupForm(employment: Employment) {
+        var groupBuilder = new UniSectionBuilder('Vis mer');
         if (employment.Standard === true) {
             groupBuilder.openByDefault(true);
         }
 
         // a-meldingsinfo
         var ameldingSet = new UniFieldsetBuilder();
-        var tOfEmplnt = this.buildField("Arbeidsforhold", employment, "TypeOfEmployment", FieldType.COMBOBOX);
+        var tOfEmplnt = this.buildField('Arbeidsforhold', employment, 'TypeOfEmployment'
+            , FieldType.COMBOBOX);
         tOfEmplnt.setKendoOptions({
             dataSource: this.typeOfEmployment,
-            dataTextField: "Navn",
-            dataValueField: "ID"
+            dataTextField: 'Navn',
+            dataValueField: 'ID'
         });
-        var renum = this.buildField("Avlønning", employment, "RenumerationType", FieldType.COMBOBOX);
+        var renum = this.buildField('Avlønning', employment, 'RenumerationType'
+            , FieldType.COMBOBOX);
         renum.setKendoOptions({
             dataSource: this.renumerationType,
-            dataTextField: "Navn",
-            dataValueField: "ID"
+            dataTextField: 'Navn',
+            dataValueField: 'ID'
         });
-        var work = this.buildField("Arbeidstid", employment, "WorkingHoursScheme", FieldType.COMBOBOX);
+        var work = this.buildField('Arbeidstid', employment, 'WorkingHoursScheme'
+            , FieldType.COMBOBOX);
         work.setKendoOptions({
             dataSource: this.workingHoursScheme,
-            dataTextField: "Navn",
-            dataValueField: "ID"
+            dataTextField: 'Navn',
+            dataValueField: 'ID'
         });
-        var hours = this.buildField("Standardtimer", employment, "HoursPerWeek", FieldType.NUMERIC);
+        var hours = this.buildField('Standardtimer', employment, 'HoursPerWeek', FieldType.NUMERIC);
         ameldingSet.addUniElements(hours, tOfEmplnt, renum, work);
 
         // dates
         var dateSet = new UniFieldsetBuilder();
-        var salary = this.buildField("Lønnsjustering", employment, "LastSalaryChangeDate", FieldType.DATEPICKER);
-        var percent = this.buildField("Endret stillingprosent", employment, "LastWorkPercentChangeDate", FieldType.DATEPICKER);
-        var senority = this.buildField("Ansiennitet", employment, "SenorityDate", FieldType.DATEPICKER);
+        var salary = this.buildField('Lønnsjustering', employment, 'LastSalaryChangeDate'
+            , FieldType.DATEPICKER);
+        var percent = this.buildField('Endret stillingprosent', employment
+            , 'LastWorkPercentChangeDate', FieldType.DATEPICKER);
+        var senority = this.buildField('Ansiennitet', employment, 'SenorityDate'
+            , FieldType.DATEPICKER);
         dateSet.addUniElements(salary, percent, senority);
 
         // annen lønnsinfo
         var infoSet = new UniFieldsetBuilder();
-        var freerate = this.buildField("Fri sats", employment, "UserdefinedRate", FieldType.NUMERIC);
-        var ledger = this.buildField("Hovedbokskonto", employment, "LedgerAccount", FieldType.TEXT);
+        var freerate = this.buildField('Fri sats', employment, 'UserdefinedRate'
+            , FieldType.NUMERIC);
+        var ledger = this.buildField('Hovedbokskonto', employment, 'LedgerAccount'
+            , FieldType.TEXT);
         infoSet.addUniElements(freerate, ledger);
 
         // dimensjoner
@@ -225,7 +239,7 @@ export class EmployeeEmployment {
         return groupBuilder;
     }
 
-    buildField(label: string, model: any, modelfield: string, type: number, index = null) {
+    private buildField(label: string, model: any, modelfield: string, type: number, index = null) {
         return new UniFieldBuilder()
             .setLabel(label)
             .setModel(model)
@@ -233,20 +247,22 @@ export class EmployeeEmployment {
             .setType(UNI_CONTROL_DIRECTIVES[type]);
     }
 
-    changeDefault(event, index) {
-        console.log("Index when changing default: " + index);
+    private changeDefault(event, index) {
+        console.log('Index when changing default: ' + index);
     }
 
-    onFormSubmit(event, index) {
+    public onFormSubmit(event, index) {
         jQuery.merge(this.currentEmployee.Employments[index], event.value);
     }
 
     // @fixme: Proper date formatting here…
-    formatDate(date) {
-        if (!date)return "";
-
+    private formatDate(date) {
+        if (!date) {
+            return '';
+        }
+        
         date = new Date(date);
-        return date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+        return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear();
     }
 
 }
--- src/app/services/StaticRegisterService.ts
+++ src/app/services/StaticRegisterService.ts
@@ -1,58 +1,29 @@
 import {BizHttp} from '../../framework/core/http/BizHttp';
 import {StaticRegister} from '../unientities';
 import {UniHttp} from '../../framework/core/http/http';
-import {Injectable, Inject} from "angular2/core";
+import {Injectable} from 'angular2/core';
 
 @Injectable()
 export class StaticRegisterService extends BizHttp<StaticRegister> {
     
-    constructor(http:UniHttp) {
+    constructor(http: UniHttp) {
         super(http);
     }
     
     public checkForStaticRegisterUpdate() {
-        console.log("Logget inn, klar for sjekk av staticRegs");
         this.http.asGET()
         .usingBusinessDomain()
-        .withEndPoint("staticregister")
+        .withEndPoint('staticregister')
         .send()
         .subscribe((response) => {
-            console.log("staticregisters", response);
             response.forEach(entity => {
-                console.log("statreg entity",entity);
-                var localstorageStamp = localStorage.getItem(entity.Registry + "Stamp");
-                console.log("localstoragestamp", localstorageStamp);
-                console.log("entity", entity);
-                entity.stamp = this.getTodayAsDate();
-                console.log("entity med ny dato", entity);
-                //if((localstorageStamp === undefined) || (localstorageStamp < entity.Stamp)) {
-                //if(!localstorageStamp) {
-                    console.log("ready for post to localstorage");
+                var localstorageStamp = localStorage.getItem(entity.Registry + 'Stamp');
+                if ((!localstorageStamp) || (localstorageStamp < entity.stamp)) {
                     this.postStaticRegisterDataset(entity);
-                //}
-                console.log("STYRK dataset", localStorage.getItem("styrkData"));
+                }
             });
         });
          
-    }
-    
-    private getTodayAsDate() {
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1;
-        var yyyy = today.getFullYear();
-        
-        if (dd < 10) {
-            dd = '0' + dd;
-        }
-        
-        if(mm < 10) {
-            mm = '0' + mm;
-        }
-        
-        //today = dd + "." + mm + "." + yyyy;
-        
-        return today;
     }
     
     public postStaticRegisterDataset(entity) {
@@ -61,18 +32,13 @@ export class StaticRegisterService extends BizHttp<StaticRegister> {
         .withEndPoint(entity.Registry)
         .send()
         .subscribe((response) => {
-            console.log("response fra get",response);
-            console.log("set localstorage");
-            console.log("entity.Stamp", entity.stamp);
-            localStorage.setItem(entity.Registry + "Data", response);
-            localStorage.setItem(entity.Registry + "Stamp", entity.stamp);
-            console.log("item set", localStorage.getItem(entity.Registry + "Data"));
+            localStorage.setItem(entity.Registry + 'Data', JSON.stringify(response));
+            localStorage.setItem(entity.Registry + 'Stamp', entity.stamp);
         }); 
     }
     
-    public getStaticRegisterDataset(registry:string) {
-        console.log("dataset for " + registry, localStorage.getItem(registry + "Data"));
-        return localStorage.getItem(registry + "Data");
+    public getStaticRegisterDataset(registry: string) {
+        return JSON.parse(localStorage.getItem(registry + 'Data'));
     }
     
 }
