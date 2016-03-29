88585543a4bbec0bb132eb574e60eae7e24c9667
jorge <jorge@unimicro.no>
2016-03-29 15:53:04 +0200

    refactor(routes): salary and settings
    ref: #254

--- src/app/components/salary/employee/employeeDetails.ts
+++ src/app/components/salary/employee/employeeDetails.ts
@@ -1,14 +1,7 @@
 import {Component, provide} from "angular2/core";
-import {RouteConfig, RouteDefinition, RouteParams, ROUTER_DIRECTIVES} from "angular2/router";
+import {RouteConfig, RouteDefinition, RouteParams, ROUTER_DIRECTIVES, AsyncRoute} from "angular2/router";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
-
-import {PersonalDetails} from "./personalDetails/personalDetails";
-import {EmployeeEmployment} from "./employments/employments";
-import {Hours} from "./hours/hours";
-import {Travel} from "./travel/travel";
-import {SalaryTransactions} from "./salaryTransactions/salaryTransactions";
-import {EmployeeLeave} from "./employeeLeave/employeeLeave";
 
 import {UniTabs} from "../../layout/uniTabs/uniTabs";
 import {WidgetPoster} from "../../../../framework/widgetPoster/widgetPoster";
@@ -16,15 +9,40 @@ import {WidgetPoster} from "../../../../framework/widgetPoster/widgetPoster";
 
 import {EmployeeDS} from "../../../data/employee";
 import {STYRKCodesDS} from "../../../data/styrkCodes";
-import {SalaryTransactionEmployeeList} from "../salarytrans/salarytransList";
+import {ComponentProxy} from "../../../../framework/core/componentProxy";
 
 const CHILD_ROUTES = [
-    {path: "/", component: PersonalDetails, as: "PersonalDetails", useAsDefault:true},
-    {path: "/employment", component: EmployeeEmployment, as: "Employment"},
-    {path: "/salarytrans", component: SalaryTransactionEmployeeList, as: "Lønnstranser"},
-    {path: "/hours", component: Hours, as: "Hours"},
-    {path: "/travel", component: Travel, as: "Travel"},
-    {path: "/employeeleave", component: EmployeeLeave, as: "EmployeeLeave"}
+    new AsyncRoute({
+        useAsDefault: true,
+        path: "/personal-details",
+        name: "Detaljer",
+        loader: () => ComponentProxy.LoadComponentAsync("PersonalDetails", "./app/components/salary/employee/personalDetails/personalDetails")
+    }),
+    new AsyncRoute({
+        path: "/employment",
+        name: "Employment",
+        loader: () => ComponentProxy.LoadComponentAsync("EmployeeEmployment", "./app/components/salary/employee/employments/employments")
+    }),
+    new AsyncRoute({
+        path: "/salarytrans",
+        name: "Lønnstranser",
+        loader: () => ComponentProxy.LoadComponentAsync("SalaryTransactionEmployeeList", "./app/components/salary/salarytrans/salarytransList")
+    }),
+    new AsyncRoute({
+        path: "/hours",
+        name: "Hours",
+        loader: () => ComponentProxy.LoadComponentAsync("Hours", "./app/components/salary/employee/hours/hours")
+    }),
+    new AsyncRoute({
+        path: "/travel",
+        name: "Travel",
+        loader: () => ComponentProxy.LoadComponentAsync("Travel", "./app/components/salary/employee/travel/travel")
+    }),
+    new AsyncRoute({
+        path: "/employeeleave",
+        name: "EmployeeLeave",
+        loader: () => ComponentProxy.LoadComponentAsync("EmployeeLeave", "./app/components/salary/employee/employeeLeave/employeeLeave")
+    })
 ];
 
 @Component({
