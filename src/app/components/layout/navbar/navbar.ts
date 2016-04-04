3f7ba68ef3a2602a24acef4cbcd4fbd233eae039
Anders Urrang <anders.urrang@unimicro.no>
2016-04-04 16:40:58 +0200

    feat(navbar): Close hamburger menu on clicks outside
    ref #119

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -1,5 +1,5 @@
-import {Component, AfterViewInit} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {Component} from 'angular2/core';
+import {AsyncRoute, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Routes} from '../../../route.config';
 
 import {HamburgerMenu} from './hamburgerMenu/hamburgerMenu';
@@ -8,16 +8,20 @@ import {NavbarSearch} from './search/search';
 import {UniUserDropdown} from './userInfo/userDropdown/userDropdown';
 import {UniCompanyDropdown} from './userInfo/companyDropdown/companyDropdown';
 
+declare var jQuery;
+
 @Component({
-	selector: 'uni-navbar',
-	templateUrl: 'app/components/layout/navbar/navbar.html',
-	directives: [ROUTER_DIRECTIVES, HamburgerMenu, UniTabStrip, NavbarSearch, UniUserDropdown, UniCompanyDropdown]
+    selector: 'uni-navbar',
+    templateUrl: 'app/components/layout/navbar/navbar.html',
+    directives: [
+        ROUTER_DIRECTIVES,
+        HamburgerMenu,
+        UniTabStrip,
+        NavbarSearch,
+        UniUserDropdown,
+        UniCompanyDropdown
+    ]
 })
-export class UniNavbar implements AfterViewInit {
-	public routes = Routes;
-	
-	constructor() {
-	}
-	
-	ngAfterViewInit() {}
+export class UniNavbar {
+    public routes: AsyncRoute[] = Routes;
 }
