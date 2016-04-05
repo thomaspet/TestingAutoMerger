cf189e09b91d88320047eac0e7f857e2d0fc5889
Arild <Arild@unimicro.no>
2016-04-05 14:49:35 +0200

    feat(employee):add category
    added categories for employee
    
    ref #249

--- src/app/components/salary/employee/employeeCategoryButtons.ts
+++ src/app/components/salary/employee/employeeCategoryButtons.ts
@@ -11,17 +11,20 @@ declare var jQuery;
         <section class="poster_tags">
 
             <ul class="poster_tags_list">
-                <li *ngFor="#category of categories">{{category.Name}} <button class="remove" (click)="removeTag(category)">Remove</button></li>
+                <li *ngFor="#category of selectedEmployee.EmployeeCategories">{{category.Name}} 
+                <button class="remove" (click)="removeTag(category)">Remove</button></li>
             </ul>
 
             <button class="poster_tags_addBtn" (click)="addingTags = !addingTags" [ngClass]="{'-is-active': addingTags}">Legg til&hellip;</button>
             <div class="poster_tags_addDropdown" [ngClass]="{'-is-active': addingTags}">
 
-                <input type="text" placeholder="Søk kategorierier…" [(ngModel)]="newTag" (keyup)="presentResults(newTag)" autofocus/>
+                <input type="text" placeholder="Søk kategorierier…" 
+                [(ngModel)]="newTag" (keyup)="presentResults(newTag)" autofocus/>
 
                 <ul (click)="addingTags = false; newTag = ''" *ngIf="newTag">
-                    <li *ngFor="#result of results" (click)="addCategory(result)">{{result}}</li>
-                    <li class="poster_tags_addNew" (click)="addCategory(newTag)">Legg til <strong>‘{{newTag}}’</strong>…</li>
+                    <li *ngFor="#result of results" (click)="addCategory(result.Name)">{{result.Name}}</li>
+                    <li class="poster_tags_addNew" (click)="addCategory(newTag)">
+                    Legg til <strong>‘{{newTag}}’</strong>…</li>
                 </ul>
             </div>
 
@@ -34,10 +37,10 @@ declare var jQuery;
                     <button (click)="removeCategory(category)">{{category.Name}}</button>
                 </li>
             </ul>
-            <button (click)="addCategories()">Legg til</button>
+            <!--<button (click)="addCategories()">Legg til</button>
             <button (click)="saveCategories()">Lagre</button>
             <select id="tags"></select>
-            <input id="selectedCategories">
+            <input id="selectedCategories">-->
         </article>
     `
 })
@@ -46,310 +49,63 @@ export class EmployeeCategoryButtons implements OnInit {
     private categories: Array<EmployeeCategory>;
     @Input()
     private selectedEmployee: Employee;
-    private categoriesFromEmployee: Array<EmployeeCategoryLink>;
-    //private multiselect: any;
-    
-    //private newItemText: any;
     
     constructor(private employeeService: EmployeeService, 
                 private employeeCategoryService: EmployeeCategoryService) {
         
-    }
-    
-    // http://docs.telerik.com/kendo-ui/api/javascript/ui/multiselect
-    // http://jsfiddle.net/OnaBai/LQx2z/
-    // http://stackoverflow.com/questions/16113029/how-to-set-data-values-on-kendo-multi-select
-    // http://dojo.telerik.com/@diondirza/esEqo/4
-    // http://output.jsbin.com/ucimay/7
-    
-    
-    
-    // public onDataBound(e: kendo.ui.MultiSelectDataBoundEvent) {
-    //     // if (this.multiselection) {
-    //         console.log('inside');
-    //         if ((this.newItem || e.sender._prev) && 
-    //         this.newItem !== e.sender._prev) {
-    //             var ds = e.sender.dataSource,
-    //                 datas = ds.data(),
-    //                 lastItem = datas[datas.length - 1];
-
-    //             this.newItem = e.sender._prev;
-
-    //             if (/\(Add New\)$/i.test(lastItem.text)) {
-    //                 ds.remove(lastItem);
-    //             }
-                
-    //             var newEntryFound = e.sender._.findWhere(datas, 
-    //             { ID: this.newItem }) !== null;
-
-    //             if (this.newItem.length > 2 && !newEntryFound) {
-    //             ds.add({ Name: this.newItem + ' (Lag ny)', ID: this.newItem });
-    //             e.sender.open();
-    //             }
-    //         }
-    //     // }
-    // }
-    
-    // public onSelect(e: kendo.ui.MultiSelectEvent) {
-    //     // e.sender.dataSource.view()[e.sender.]
-    //     var dataItem = e.sender.dataSource.view()[e.item.index],
-    //         datas = e.sender.dataSource.data(),
-    //         lastData = datas[datas.length - 1];
-        
-    //     if (parseInt(dataItem.value) > 0) {
-    //         e.sender.dataSource.remove(lastData);
-    //     } else {
-    //         dataItem.Name = dataItem.Name.replace(' (Lag ny)', '');
-    //     }
-    // }
-    
-    private setCategories() {
-        // this.selectedEmployee.EmployeeCategoryLinks.forEach(categoryLinkID => {
-        //     var cat = new EmployeeCategory();
-        //     console.log('kategorilink', categoryLinkID);
-        //     cat.EmployeeCategoryLinkID = categoryLinkID.EmployeeCategoryID;
-        // });
-        
-        // this.categoriesFromEmployee = this.selectedEmployee.EmployeeCategoryLinks;
-    }
-    
-    public ngOnInit() {
-        var tags = [
-            { text: "Tag1", value:"Tag1" },
-            { text: "Tag2", value:"Tag2" },
-            { text: "Tag3", value:"Tag3" }
-        ];
-
-        // this.setCategories();
-		
-        var newitemtext;
-        
-        var multiselect = jQuery('#tags').kendoMultiSelect({
-			// change: function() {
-			// 	var value = this.value().slice(0);
-			// 	var dataitems = this.dataSource.data();
-
-			// 	var newtag = "";
-
-			// 	for (var i = 0; i < dataitems.length; i++) {
-			// 	    var dataItem = dataitems[i];
-
-			// 		if(dataItem.text.substring(0, "Add new tag: ".length) === "Add new tag: ") {
-			// 			newtag = dataItem.text.replace("Add new tag: ", "");
-			// 			this.dataSource.remove(dataItem);
-			// 		}
-			// 	}
-
-			// 	this.dataSource.add({value: newtag, text: newtag});
-
-            //   if (newtag) {
-            //     this.dataSource.filter({});
-			// 	if(this.value().length == 1)
-			// 	{
-			// 		this.value(newtag);
-			// 	}
-			// 	else
-			// 	{
-            //         value.push(newtag);
-            //         console.log(value);
-			// 		this.value(value);
-			// 	}
-            //   }
-			// },
-			// dataBound: function() {
-			// 	if((newitemtext || this._prev) && newitemtext != this._prev)
-			// 	{
-			// 		newitemtext = this._prev;
-
-			// 		var dataitems = this.dataSource.data();
-
-			// 		var isfound = false;
-			// 		for (var i = 0; i < dataitems.length; i++) {
-			// 		    var dataItem = dataitems[i];
-
-			// 			if(dataItem.value != dataItem.text) {
-			// 				dataItem.text = "Add new tag: " + newitemtext;
-			// 				this.refresh();
-			// 				isfound = true;
-			// 			}
-			// 		}
-			// 		if(!isfound)
-			// 		{
-			// 			this.dataSource.add({text: "Add new tag: " + newitemtext, value: newitemtext});
-			// 			this.refresh();
-			// 		}
-			// 		this.search();
-			// 		this.open();
-			// 	}
-			// },
-			dataSource: tags,
-			dataTextField: 'text',
-			dataValueField: 'value',
-			filter: 'contains',
-			animation: false
-		});
-        
-        console.log('multiselect', multiselect);
-        // multiselect.dataSource.filter({});
-        // multiselect.value = ('Tag1', 'Tag3');
-        // multiselect.trigger('change');
-        
-        
-        
-        this.employeeService.getEmployeeCategories(this.selectedEmployee.EmployeeNumber)
+        this.employeeCategoryService.GetAll('')
         .subscribe((response: any) => {
             this.categories = response;
-            console.log('response kategorier', response);
-            
-            this.selectedEmployee.EmployeeCategories = response;
-            
-            var data: any = [
-                { text : 'name1', value : 'name1' },
-                { text : 'name2', value : 'name2' },
-                { text : 'name3', value : 'name3' },
-                { text : 'name4', value : 'name4' },
-                { text : 'name5', value : 'name5' },
-                { text : 'name6', value : 'name6' }
-            ];
-            
-            var newItemText;
-            
-            var multiselect2 = jQuery('#selectedCategories').kendoMultiSelect({
-                animation: false,
-                dataSource: data,
-                // dataSource: this.data,
-                dataTextField: 'text',
-                dataValueField: 'value',
-                placeholder: 'Legg til',
-                filter: 'contains',
-                // dataBound: (event) => this.onDataBound(event),
-                
-                // select: this.onSelect,
-                // change: function() {
-                //     // var dataItem = this.dataSource.view()[event.item.index()],
-                //     // datas = this.dataSource.data(),
-                //     // lastData = datas[datas.length - 1];
-                    
-                //     var dataitems = this.dataSource.data();
-                    
-                //     for (var i = 0; i < dataitems.length; i++) {
-                //         var dataItem = dataitems[i];
-                        
-                //         console.log('dataItem', dataItem);
-                        
-                //         if (parseInt(dataItem.value) > 0) {
-                //             this.dataSource.remove(dataItem);            
-                //         } else {
-                //             dataItem.text = dataItem.text.replace('Legg til ny: ', '');
-                //         }
-                //     }
-                    
-                //     console.log('dataSource', this.dataSource.data());
-                // },
-                change: function() {
-                    var value = this.value().slice(0);
-                    console.log('value', value);
-                    
-                    var dataitems = this.dataSource.data();
-
-                    var newtag = '';
-
-                    for (var i = 0; i < dataitems.length; i++) {
-                        var dataItem = dataitems[i];
-                        console.log('dataitem', dataItem);
-                        if (dataItem.text.substring(0, 'Legg til ny: '.length) === 'Legg til ny: ') {
-                            
-                            newtag = dataItem.text.replace('Legg til ny: ', '');
-                            console.log('newtag', newtag);
-                            this.dataSource.remove(dataItem);
-                        }
-                    }
-
-                    this.dataSource.add({value: newtag, text: newtag});
-
-                    if (newtag) {
-                        this.dataSource.filter({});
-                        if (this.value().length === 1) {
-                            this.value(newtag);
-                        } else {
-                            value.push(newtag);
-                            console.log(value);
-                            this.value(value);
-                        }
-                    }
-                    
-                    console.log('dataSource', this.dataSource.data());
-                },
-                dataBound: function() {
-                    console.log('newItemText', newItemText);
-                    if ((newItemText || this._prev) && newItemText !== this._prev) {
-                        newItemText = this._prev;
-                        console.log('this.prev', newItemText);
-
-                        var dataitems = this.dataSource.data();
-
-                        var isfound = false;
-                        for (var i = 0; i < dataitems.length; i++) {
-                            var dataItem = dataitems[i];
-                            
-                            console.log('dataItem for sjekk om likhet mellom value og text', dataItem);
-                            if (dataItem.value !== dataItem.text) {
-                                dataItem.text = 'Legg til ny: ' + newItemText;
-                                this.refresh();
-                                isfound = true;
-                            }
-                        }
-                        if (!isfound) {
-                            console.log('adds', newItemText);
-                            this.dataSource.add({   text: 'Legg til ny: ' + newItemText, 
-                                                    value: newItemText});
-                            this.refresh();
-                        }
-                        this.search();
-                        this.open();
-                    }
-                }
-            }).data('kendoMultiSelect');
         });
     }
     
-    public addCategory(category) {
-        console.log('Add category', category);
+    public ngOnInit() {        
+        this.employeeService.getEmployeeCategories(this.selectedEmployee.EmployeeNumber)
+        .subscribe((response: any) => {
+            this.selectedEmployee.EmployeeCategories = response;
+        });
+    }
+    
+    public addCategory(categoryName) {
+        console.log('Add category', categoryName);
+        console.log('categories', this.categories);
+        
+        var category = new EmployeeCategory();
+        category.Name = categoryName;
+        
         this.selectedEmployee.EmployeeCategories.push(category);
-        this.selectedEmployee.EmployeeCategories.splice(this.selectedEmployee.EmployeeCategories.indexOf(category.Name),1);
-        // this.categories.push(category);
+        this.categories.splice(this.categories.indexOf(categoryName), 1);
+        
+        console.log('categories', this.categories);
     }
     
     public removeCategory(removeCategory: EmployeeCategory) {
         console.log('Remove category');
-        var item: number = 0;
-        this.selectedEmployee.EmployeeCategories.forEach(category => {
-            if (category.Name === removeCategory.Name) {
-                this.selectedEmployee.EmployeeCategories.splice(item, 1);
-                // Here we must also delete categorylink on server
-                // OR
-                // we must flag categorylink to be deleted when employee is saved ?
-            }
-            item += 1;
-        });
+        this.selectedEmployee.EmployeeCategories.splice(this.selectedEmployee.EmployeeCategories.indexOf(removeCategory), 1);
+        this.categories.unshift(removeCategory);
+        
+        // var item: number = 0;
+        // this.selectedEmployee.EmployeeCategories.forEach(category => {
+        //     if (category.Name === removeCategory.Name) {
+        //         this.selectedEmployee.EmployeeCategories.splice(item, 1);
+        //         // Here we must also delete categorylink on server
+        //         // OR
+        //         // we must flag categorylink to be deleted when employee is saved ?
+        //     }
+        //     item += 1;
+        // });
     }
     
-    // lagrer kategorier basert på arrayen av kategorilinkID'er som ligg på den ansatte
-    // ... dvs dersom det ligg linker i den arrayen - uvisst om den skal brukes no.
-    // Avklaring på dette er rett rundt hjørnet (04.04.2016)
     public saveCategories() {
         this.employeeCategoryService.saveCategoriesOnEmployee(this.selectedEmployee);
     }
     
-    private tags: string[] = ['Aktiv', 'Sjømenn', 'Pensjon'];
+    // private tags: string[] = ['Aktiv', 'Sjømenn', 'Pensjon'];
 
     private results: Array<EmployeeCategory> = [];
-
-
+    
     private filterTags = function(tag: string) {
-        console.log('str', tag);
         let containsString = function(str: EmployeeCategory) {
-            console.log('str2', str);
             return str.Name.toLowerCase().indexOf(tag.toLowerCase()) >= 0;
         };
 
