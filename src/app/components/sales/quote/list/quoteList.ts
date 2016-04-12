64e4aee19d7316afd217da6e66b2e94f10d7fb27
Tor Kårstad <tor.kaarstad@unimicro.no>
2016-04-12 14:40:03 +0200

    feature/Quote_search_#322: Move status method to quoteService

--- src/app/components/sales/quote/list/quoteList.ts
+++ src/app/components/sales/quote/list/quoteList.ts
@@ -19,32 +19,12 @@ export class QuoteList {
     private quoteTable: UniTableBuilder;
     private selectedquote: CustomerQuote;
 
-    // TODO: To be retrieved from database schema shared.Status instead?
-    private statusTypes: Array<any> = [
-        { Code: '40002', Text: 'Registrert' },
-        { Code: '40003', Text: 'Sendt til kunde' },
-        { Code: '40004', Text: 'Kunde har godkjent' },
-        { Code: '40005', Text: 'Overført til ordre' },
-        { Code: '40006', Text: 'Overført til faktura' },
-        { Code: '40007', Text: 'Avsluttet' },
-        { Code: '40008', Text: 'Kladd' },
-
-    ];
-
+   
+   
     constructor(private uniHttpService: UniHttp, private router: Router, private customerQouteService: CustomerQuoteService) {
         this.setupQuoteTable();
     }
 
-    private getStatusText = (statusCode: string) => {
-        var text = 'Udefinert';
-        this.statusTypes.forEach((status) => {
-            if (status.Code == statusCode) {
-                text = status.Text;
-                return;
-            }
-        });
-        return text;
-    };
 
 
     public createQuote() {        
@@ -103,7 +83,7 @@ export class QuoteList {
 
         var statusCol = new UniTableColumn('StatusCode', 'Status', 'number').setWidth('15%');
         statusCol.setTemplate((dataItem) => {
-            return this.getStatusText(dataItem.StatusCode);
+            return this.customerQouteService.getStatusText(dataItem.StatusCode);
         });
 
         // Define callback function for row clicks
