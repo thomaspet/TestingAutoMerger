ccff8a9d103b9f5c98883bebcc4714edd34ae8ab
Kjetil Ek <kjetil@unimicro.no>
2016-04-21 07:55:00 +0200

    feat (sales): add status to orderitem
    ref #381

--- src/app/services/Sales/CustomerOrderItemService.ts
+++ src/app/services/Sales/CustomerOrderItemService.ts
@@ -10,4 +10,21 @@ export class CustomerOrderItemService extends BizHttp<CustomerOrderItem> {
         this.relativeURL = "orderitems";// CustomerOrderItem.relativeUrl;
         this.DefaultOrderBy = null;
     }
+    
+    // TODO: To be retrieved from database schema shared.Status instead?
+    // TODO: Sett opp gyldige statuser her
+    private statusTypes: Array<any> = [
+       
+    ];
+
+    public getStatusText = (statusCode: string) => {
+        var text = 'Udefinert';
+        this.statusTypes.forEach((status) => {
+            if (status.Code == statusCode) {
+                text = status.Text;
+                return;
+            }
+        });
+        return text;
+    };
 }
