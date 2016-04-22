4118bc71bca3c8c04d9db3f094f0cab5d5423f91
jorge <jorge@unimicro.no>
2016-04-22 15:50:58 +0200

    fixed client header for unientities

--- gulpfile.js
+++ gulpfile.js
@@ -119,9 +119,9 @@ gulp.task('clean' , function (done) {
 
 gulp.task('entities' , function (done) {
     var options = {
-        url: 'http://devapi.unieconomy.no/api/metadata/typescriptentities' ,
+        url: 'https://devapi-unieconomy.azurewebsites.net/api/metadata/typescriptentities' ,
         headers: {
-            'client': 'economytestas'
+            'client': 'andersas'
         }
     };
     var callback = function (error , response , body) {
