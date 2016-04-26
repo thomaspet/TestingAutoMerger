ae9cf3b54d75cee781742cc6371499b90d6e920e
Anders Urrang <anders.urrang@unimicro.no>
2016-04-26 15:04:04 +0200

    chore: Update comments in AppConfig.ts

--- src/app/AppConfig.ts
+++ src/app/AppConfig.ts
@@ -1,12 +1,15 @@
 import {CONST_EXPR} from 'angular2/src/facade/lang';
 
 export const AppConfig = CONST_EXPR({
-    // Hacky solution for making frontend work with localhost backend.
-    // Used in authService. Remember to revert to BASE_URL there when this is removed!
+    // Allow developers to work with localhost backend.
+    // Let this stay as long we want to support local backend.
+    // When removed, remember to change BASE_URL_INIT to BASE_URL in authService.ts.
     BASE_URL_INIT: 'https://devapi-unieconomy.azurewebsites.net',
     
-    BASE_URL: 'http://localhost:27831',
-    // BASE_URL: 'https://devapi-unieconomy.azurewebsites.net',
+    // Swap between these BASE_URL's depending on whether you want to use local og remote backend
+    // BASE_URL: 'http://localhost:27831',
+    BASE_URL: 'https://devapi-unieconomy.azurewebsites.net',
+    
     API_DOMAINS: {
         INIT : '/api/init/',
         BUSINESS: '/api/biz/',
