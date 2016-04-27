9e785fd7304a7e3ba7607ffd4ff82ad23e4766a7
Terje Pedersen <terje@senikk.com>
2016-04-27 10:11:34 +0200

    fix(quote): fixed some bugs after review

--- gulpfile.js
+++ gulpfile.js
@@ -119,8 +119,8 @@ gulp.task('clean' , function (done) {
 
 gulp.task('entities' , function (done) {
     var options = {
-        url: 'http://localhost:27831/api/metadata/typescriptentities',
-        //url: 'https://devapi-unieconomy.azurewebsites.net/api/metadata/typescriptentities' ,
+        //url: 'http://localhost:27831/api/metadata/typescriptentities',
+        url: 'https://devapi-unieconomy.azurewebsites.net/api/metadata/typescriptentities' ,
         headers: {
             'client': 'andersas'
         }
--- src/app/AppConfig.ts
+++ src/app/AppConfig.ts
@@ -1,8 +1,8 @@
 import {CONST_EXPR} from 'angular2/src/facade/lang';
 
 export const AppConfig = CONST_EXPR({
-    BASE_URL: 'http://localhost:27831',
-    //BASE_URL: 'https://devapi-unieconomy.azurewebsites.net',
+    //BASE_URL: 'http://localhost:27831',
+    BASE_URL: 'https://devapi-unieconomy.azurewebsites.net',
     API_DOMAINS: {
         INIT : '/api/init/',
         BUSINESS: '/api/biz/',
