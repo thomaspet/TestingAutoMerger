1ee2555cd7fe3e97e999a6ea6ff006d134c1cd76
Attila Torjak <attila@unimicro.no>
2016-04-28 16:07:08 +0200

    POC report: use live data

--- src/app/components/reports/modals/preview/previewModal.ts
+++ src/app/components/reports/modals/preview/previewModal.ts
@@ -1,8 +1,11 @@
 import {Component, ViewChild, Type, Input} from "angular2/core";
 import {NgIf, NgModel, NgFor, NgClass} from "angular2/common";
+import {Http, Headers} from 'angular2/http';
 import {UniModal} from "../../../../../framework/modals/modal";
 import {UniComponentLoader} from "../../../../../framework/core/componentLoader";
 import {StimulsoftReportWrapper} from "../../../../../framework/wrappers/reporting/reportWrapper";
+import {CustomerInvoiceService, CustomerInvoiceItemService, CustomerService, SupplierService, ProjectService, DepartementService, AddressService} from '../../../../services/services';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: "report-preview-modal-type",
@@ -31,7 +34,7 @@ export class ReportPreviewModalType {
     template: `
         <uni-modal [type]="type" [config]="modalConfig"></uni-modal>
     `,
-    providers: [StimulsoftReportWrapper]
+    providers: [StimulsoftReportWrapper, CustomerInvoiceService]
 })
 export class PreviewModal {
     @ViewChild(UniModal)
@@ -40,7 +43,9 @@ export class PreviewModal {
     public modalConfig: any = {};
     type: Type = ReportPreviewModalType;
 
-    constructor(public reportWrapper: StimulsoftReportWrapper)
+    constructor(public reportWrapper: StimulsoftReportWrapper,
+                private customerInvoiceService: CustomerInvoiceService,
+                private http: Http)
     {
         var self = this;
         this.modalConfig = {
@@ -68,7 +73,25 @@ export class PreviewModal {
     
     public open()
     {
-        this.reportWrapper.start(this.modalConfig);
-        this.modal.open();
+        // for test purpose only
+        // @TODO: implement and use API resource
+        
+        this.http.get('/assets/DemoData/Demo.mrt') 
+            .map(res => res.text())
+            .subscribe(template => this.onTemplateLoaded(template),
+            err => this.onError("Cannot load report template."));
+    }
+   
+    private onTemplateLoaded(template : string) {
+        // for test purpose only:
+        // hardcoded invoice id
+        this.customerInvoiceService.Get(1).subscribe(response => { 
+            this.reportWrapper.showReport(template, [response], this.modalConfig);
+            this.modal.open();        
+        });
+    }
+   
+    private onError(err : string) {
+        alert(err)
     }
 }
--- src/framework/wrappers/reporting/reportWrapper.ts
+++ src/framework/wrappers/reporting/reportWrapper.ts
@@ -1,116 +1,69 @@
 import {Injectable} from 'angular2/core';
-import {Http, Headers} from 'angular2/http';
 
 declare var Stimulsoft;
 
 @Injectable()
 export class StimulsoftReportWrapper {
-    private caller : any;
-    constructor(private http : Http) {
+
+    constructor() {
         
     }
-    
-    public start(caller: any)
+            
+    public showReport(template : string, reportData : string[], caller : any)
     {
-        this.caller = caller;
-        this.http.get('/assets/DemoData/Demo.json') 
-            .map(res => res.text())
-            .subscribe(data => this.onReportDataLoaded( [data] ),
-                    err => this.onError("Cannot load test data from json file."));
-    }
-
-    private onReportDataLoaded(reportData : string[])
-    {
-        this.showReport("Demo", reportData, "reportContainer")
-        //this.printReport("Demo", reportData, true);
-    }
-    // report interface
-    public getReportTypes()
-    {
-        // hard coded for test purpose
-        // @TODO: implement and use API resource
-        var reportTypes = ["Demo"];
-
-        return reportTypes
-    }
-
-    public showReport(reportType : string, reportData : string[], targetDivId : string)
-    {
-        if (reportType && targetDivId && reportData)
+        if (template && reportData && caller)
         {
-            this.generateReport(reportType, reportData, (report) => {
-                var container = document.getElementById(targetDivId);
+            var report = this.generateReport(template, reportData);
+            
+            if (report) {
+                // Create a text writer objects.
+                var textWriter = new Stimulsoft.System.IO.TextWriter();
+                var htmlTextWriter = new Stimulsoft.Report.Export.StiHtmlTextWriter(textWriter);
 
-                if (container) {
-                    // Create a text writer objects.
-                    var textWriter = new Stimulsoft.System.IO.TextWriter();
-                    var htmlTextWriter = new Stimulsoft.Report.Export.StiHtmlTextWriter(textWriter);
+                // Export HTML using text writer.
+                var settings = new Stimulsoft.Report.Export.StiHtmlExportSettings();
+                var service = new Stimulsoft.Report.Export.StiHtmlExportService();
 
-                    // Export HTML using text writer.
-                    var settings = new Stimulsoft.Report.Export.StiHtmlExportSettings();
-                    var service = new Stimulsoft.Report.Export.StiHtmlExportService();
+                service.exportTo(report, htmlTextWriter, settings);
 
-                    service.exportTo(report, htmlTextWriter, settings);
-
-                    // Write HTML text to DIV element.
-                    //container.innerHTML = textWriter.getStringBuilder().toString();
-                    this.caller.report = textWriter.getStringBuilder().toString();
-                }
-            });
+                // Write HTML text to DIV element.
+                //container.innerHTML = textWriter.getStringBuilder().toString();
+                caller.report = textWriter.getStringBuilder().toString();
+            }
         }
     }
 
-    public printReport(reportType : string, reportData : string[], showPreview : boolean)
-    {
-        if (reportType, reportData)
-        {
-            this.generateReport(reportType, reportData, (report) => {
-                if (report) {
-                    report.print();
-                }
-            });
+    public printReport(template : string, reportData : string[], showPreview : boolean) {
+        
+        if (template && reportData) {
+            var report = this.generateReport(template, reportData);
+            
+            if (report) {
+                report.print(showPreview);
+            }
         }
     }
 
     // these functions should be private in typescript
-    private generateReport(reportType : string, reportData : string[], callback : any)
-    {
-        this.getReportTemplate(reportType, function (template) {
-            // load template
-            var report = new Stimulsoft.Report.StiReport();
-            report.load(template);
+    private generateReport(template : string, reportData : string[]) : any {
+        // load template
+        var report = new Stimulsoft.Report.StiReport();
+        report.load(template);
 
-            // remove connections specified in the template file
-            report.dictionary.databases.clear();
+        // remove connections specified in the template file
+        report.dictionary.databases.clear();
 
-            // load report data
-            var dataSet : any;
+        // load report data
+        var dataSet : any;
 
-            for (var i = 0; i < reportData.length; ++i) {
-                dataSet = new Stimulsoft.System.Data.DataSet("Data" + i);
+        for (var i = 0; i < reportData.length; ++i) {
+            dataSet = new Stimulsoft.System.Data.DataSet("Data" + i);
 
-                dataSet.readJson(reportData[i]);
-                report.regData("Data" + i, "Data" + i, dataSet);
-            }
-            // render
-            report.render();
-            callback(report);
-        });
-    }
-
-    private getReportTemplate(reportType, onDone)
-    {
-        // for test purpose only
-        // @TODO: implement and use API resource
-        
-        this.http.get('/assets/DemoData/Demo.mrt') 
-            .map(res => res.text())
-            .subscribe(data => onDone(data),
-            err => this.onError("Cannot load report template."));
-    }
-    
-    private onError(err : string)
-    {
-        alert(err)
+            dataSet.readJson(reportData[i]);
+            report.regData("Data" + i, "Data" + i, dataSet);
+        }
+        // render
+        report.render();
+        return report;
     }
 }
