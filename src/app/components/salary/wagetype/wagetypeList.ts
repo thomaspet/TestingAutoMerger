c8ebc54702b617406d27b9aa6b72c5e00428dbe3
Arild <Arild@unimicro.no>
2016-05-03 12:51:52 +0200

    feat(wagetype): save
    and next/previous
    
    ref #457

--- src/app/components/salary/wagetype/wagetypeList.ts
+++ src/app/components/salary/wagetype/wagetypeList.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from 'angular2/core';
 import {Router } from 'angular2/router';
 
-import {UniTable, UniTableBuilder, UniTableColumn} from "../../../../framework/uniTable";
-import {WageType} from "../../../unientities";
+import {UniTable, UniTableBuilder, UniTableColumn} from '../../../../framework/uniTable';
+import {WageType} from '../../../unientities';
 import {TabService} from '../../layout/navbar/tabstrip/tabService';
 
 @Component({
@@ -12,33 +12,31 @@ import {TabService} from '../../layout/navbar/tabstrip/tabService';
 })
 export class WagetypeList implements OnInit {
 
-    wagetypelistConfig;
+    private wagetypelistConfig: any;
 
-    constructor(private _router: Router,
-        private tabSer: TabService) {
-        
+    constructor(private _router: Router, private tabSer: TabService) {
         this.tabSer.addTab({name: 'Lønnsarter', url: 'salary/wagetypes' });
     }
 
-    ngOnInit() {
+    public ngOnInit() {
 
-        var idCol = new UniTableColumn("WageTypeId", "Nr", "number");
-        idCol.setWidth("10rem");
+        var idCol = new UniTableColumn('WageTypeId', 'Nr', 'number');
+        idCol.setWidth('10rem');
 
-        var nameCol = new UniTableColumn("WageTypeName", "Navn", "string");
+        var nameCol = new UniTableColumn('WageTypeName', 'Navn', 'string');
 
-        var descCol = new UniTableColumn("Description", "Beskrivelse", "string");
+        var descCol = new UniTableColumn('Description', 'Beskrivelse', 'string');
 
-        this.wagetypelistConfig = new UniTableBuilder("wagetypes", false)
+        this.wagetypelistConfig = new UniTableBuilder('wagetypes', false)
             .setSelectCallback((selectedWagetype: WageType) => {
 
-                this._router.navigateByUrl("/salary/wagetypes/" + selectedWagetype.ID);
+                this._router.navigateByUrl('/salary/wagetypes/' + selectedWagetype.ID);
             })
             .setOrderBy('WageTypeId', 'asc')
             .addColumns(idCol, nameCol, descCol);
     }
     
-    newWageType(){
-        this._router.navigateByUrl("/salary/wagetypes/" + 0);
+    public createWageType() {
+        this._router.navigateByUrl('/salary/wagetypes/0');
     }
 }
--- src/app/services/Salary/WageType/WageTypeService.ts
+++ src/app/services/Salary/WageType/WageTypeService.ts
@@ -1,305 +1,296 @@
 import {BizHttp} from '../../../../framework/core/http/BizHttp';
 import {UniHttp} from '../../../../framework/core/http/http';
 import {WageType} from '../../../unientities';
-import {TaxType} from "../../../unientities";
-import {RateTypeColumn} from "../../../unientities";
-import {LimitType} from "../../../unientities";
-import {StdWageType} from "../../../unientities";
-import {FieldType} from "../../../unientities";
-import {Observable} from "rxjs/Observable";
-import "rxjs/Rx";
+import {TaxType} from '../../../unientities';
+// import {RateTypeColumn} from '../../../unientities';
+// import {LimitType} from '../../../unientities';
+import {StdWageType} from '../../../unientities';
+import {FieldType} from '../../../unientities';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 let taxType: Array<any> = [
-    {ID: TaxType.Tax_None, Name: "Ingen"},
-    {ID: TaxType.Tax_Table, Name: "Tabelltrekk"},
-    {ID: TaxType.Tax_Percent, Name: "Prosenttrekk"},
-    {ID: TaxType.Tax_0, Name: "..."}
+    {ID: TaxType.Tax_None, Name: 'Ingen'},
+    {ID: TaxType.Tax_Table, Name: 'Tabelltrekk'},
+    {ID: TaxType.Tax_Percent, Name: 'Prosenttrekk'},
+    {ID: TaxType.Tax_0, Name: '...'}
 ];
 
-let rateType: Array<any> = [
-    {ID: RateTypeColumn.none, Name: "Ingen"},
-    {ID: RateTypeColumn.Employment, Name: "Arbeidsforhold"},
-    {ID: RateTypeColumn.Employee, Name: "Ansatt"},
-    {ID: RateTypeColumn.Salary_scale, Name: "Regulativ"}
-];
+// let rateType: Array<any> = [
+//     {ID: RateTypeColumn.none, Name: 'Ingen'},
+//     {ID: RateTypeColumn.Employment, Name: 'Arbeidsforhold'},
+//     {ID: RateTypeColumn.Employee, Name: 'Ansatt'},
+//     {ID: RateTypeColumn.Salary_scale, Name: 'Regulativ'}
+// ];
 
-let limitType: Array<any> = [
-    {ID: LimitType.None, Name: "Ingen"},
-    {ID: LimitType.Amount, Name: "Antall"},
-    {ID: LimitType.Sum, Name: "Sum"}
-];
+// let limitType: Array<any> = [
+//     {ID: LimitType.None, Name: 'Ingen'},
+//     {ID: LimitType.Amount, Name: 'Antall'},
+//     {ID: LimitType.Sum, Name: 'Sum'}
+// ];
 
 let stdWageType: Array<any> = [
-    {ID: StdWageType.None, Name: "Ingen"},
-    {ID: StdWageType.TaxDrawTable, Name: "Tabelltrekk"},
-    {ID: StdWageType.TaxDrawPercent, Name: "Prosenttrekk"},
-    {ID: StdWageType.HolidayPayWithTaxDeduction, Name: "Feriepenger med skattetrekk"},
-    {ID: StdWageType.HolidayPayThisYear, Name: "Feriepenger i år"},
-    {ID: StdWageType.HolidayPayLastYear, Name: "Feriepenger forrige år"},
+    {ID: StdWageType.None, Name: 'Ingen'},
+    {ID: StdWageType.TaxDrawTable, Name: 'Tabelltrekk'},
+    {ID: StdWageType.TaxDrawPercent, Name: 'Prosenttrekk'},
+    {ID: StdWageType.HolidayPayWithTaxDeduction, Name: 'Feriepenger med skattetrekk'},
+    {ID: StdWageType.HolidayPayThisYear, Name: 'Feriepenger i år'},
+    {ID: StdWageType.HolidayPayLastYear, Name: 'Feriepenger forrige år'},
 ];
 
 let layout = {
-    Name: "MockWagTypeLayout",
-    BaseEntity: "wagetype",
+    Name: 'MockWagTypeLayout',
+    BaseEntity: 'wagetype',
     Fields: [
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "WageTypeId",
+            EntityType: 'wagetype',
+            Property: 'WageTypeId',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.TEXT,
             ReadOnly: true,
             LookupField: false,
-            Label: "Nummer",
+            Label: 'Nummer',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 0,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         },
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "WageTypeName",
+            EntityType: 'wagetype',
+            Property: 'WageTypeName',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.TEXT,
             ReadOnly: false,
             LookupField: false,
-            Label: "Navn",
+            Label: 'Navn',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 0,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         },
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "Description",
+            EntityType: 'wagetype',
+            Property: 'Description',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.TEXT,
             ReadOnly: false,
             LookupField: false,
-            Label: "Beskrivelse",
+            Label: 'Beskrivelse',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 0,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false
         },
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "Base_EmploymentTax",
+            EntityType: 'wagetype',
+            Property: 'Base_EmploymentTax',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.CHECKBOX,
             ReadOnly: false,
             LookupField: false,
-            Label: "Skatt (tabell)",
+            Label: 'Skatt (tabell)',
             Description: null,
             HelpText: null,
             openByDefault: true,
             FieldSet: 0,
             Section: 1,
-            Legend: "Med i grunnlag for",
+            Legend: 'Med i grunnlag for',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         },
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "Base_Payment",
+            EntityType: 'wagetype',
+            Property: 'Base_Payment',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.CHECKBOX,
             ReadOnly: false,
             LookupField: false,
-            Label: "Utbetaling",
+            Label: 'Utbetaling',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 1,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         }
         ,
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "HideFromPaycheck",
+            EntityType: 'wagetype',
+            Property: 'HideFromPaycheck',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.CHECKBOX,
             ReadOnly: false,
             LookupField: false,
-            Label: "Skjul på lønnslipp",
+            Label: 'Skjul på lønnslipp',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 1,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         },
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "Base_Vacation",
+            EntityType: 'wagetype',
+            Property: 'Base_Vacation',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.CHECKBOX,
             ReadOnly: false,
             LookupField: false,
-            Label: "Feriepenger",
+            Label: 'Feriepenger',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 1,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         },
         {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "StandardWageTypeFor",
+            EntityType: 'wagetype',
+            Property: 'StandardWageTypeFor',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.COMBOBOX,
             ReadOnly: false,
-            LookupField: "Name",
+            LookupField: 'Name',
             kendoOptions: {
                 dataSource: taxType,
-                dataTextField: "Name",
-                dataValueField: "ID"
+                dataTextField: 'Name',
+                dataValueField: 'ID'
             },
-            /*
-             fromArray: [
-             {ID: 0, Name:"None" },
-             {ID: 1, Name:"TableTax" },
-             {ID: 2, Name:"PercentTax" },
-             {ID: 3, Name:"Holidaypay with tax" },
-             {ID: 4, Name:"Holidaypay this year" },
-             {ID: 5, Name:"Holidaypay last year" },
-             ],*/
-            Label: "Standard lønnsart for",
+            Label: 'Standard lønnsart for',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 1,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         }
         , {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "taxtype",
+            EntityType: 'wagetype',
+            Property: 'taxtype',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.COMBOBOX,
             ReadOnly: false,
-            LookupField: "Name",
-            Label: "Type",
+            LookupField: 'Name',
+            Label: 'Type',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 1,
-            Legend: "",
+            Legend: '',
             kendoOptions: {
                 dataSource: stdWageType,
-                dataTextField: "Name",
-                dataValueField: "ID"
+                dataTextField: 'Name',
+                dataValueField: 'ID'
             },
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         }
         , {
             ComponentLayoutID: 1,
-            EntityType: "wagetype",
-            Property: "AccountNumber",
+            EntityType: 'wagetype',
+            Property: 'AccountNumber',
             Placement: 1,
             Hidden: false,
             FieldType: FieldType.TEXT,
             ReadOnly: false,
             LookupField: false,
-            Label: "Hovedbokskonto",
+            Label: 'Hovedbokskonto',
             Description: null,
             HelpText: null,
             FieldSet: 0,
             Section: 1,
-            Legend: "",
+            Legend: '',
             hasLineBreak: false,
             Validations: [
                 {
-                    ErrorMessage: "Required field",
+                    ErrorMessage: 'Required field',
                     Level: 3,
-                    Operator: "REQUIRED"
+                    Operator: 'REQUIRED'
                 }
             ]
         }
 
     ]
 
-}
+};
 
 export class WageTypeService extends BizHttp<WageType> {
 
@@ -308,7 +299,7 @@ export class WageTypeService extends BizHttp<WageType> {
         this.relativeURL = WageType.relativeUrl;
     }
 
-    getSubEntities() {
+    public getSubEntities() {
         return this.http
             .asGET()
             .usingBusinessDomain()
@@ -316,7 +307,7 @@ export class WageTypeService extends BizHttp<WageType> {
             .send();
     }
 
-    getTypes() {
+    public getTypes() {
         return this.http
             .usingBusinessDomain()
             .asGET()
@@ -324,26 +315,34 @@ export class WageTypeService extends BizHttp<WageType> {
             .send();
     }
     
-    getWageType(id): Observable<any> {
-        if(id === 0){
+    public getWageType(id): Observable<any> {
+        if (id === 0) {
             return this.GetNewEntity();
-        }else{
+        } else {
             return this.Get(id);
         }
     }
-
-    //mocks layout request
+    
+    public getPrevious(ID: number) {
+        return super.GetAction(ID, 'previous');
+    }
+    
+    public getNext(ID: number) {
+        return super.GetAction(ID, 'next');
+    }
+    
+    // mocks layout request
     public getLayout(ID: string) {
         return Observable.of(layout);
     }
 
-    //mocked with layout request
-    public getLayoutAndEntity(LayoutID: string, EntityID: number) {
+    // mocked with layout request
+    public getLayoutAndEntity(layoutID: string, entityID: number) {
         var layout, self = this;
-        return this.GetLayout(LayoutID)
+        return this.GetLayout(layoutID)
             .concatMap((data: any) => {
                 layout = data;
-                return self.Get(EntityID, data.Expands);
+                return self.Get(entityID, data.Expands);
             })
             .map((entity: any) => {
                 return [layout, entity];
