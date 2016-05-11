b9d5f04d1c24ae415e77258f9e2a4b8ca1b37b9c
Sveinung Farestveit <sveinung@unimicro.no>
2016-05-11 20:03:49 +0200

    feature(timetracking): get user and workerdetails from server

--- src/app/components/timetracking/timeentry/timeentry.ts
+++ src/app/components/timetracking/timeentry/timeentry.ts
@@ -1,6 +1,9 @@
-import {Component} from "@angular/core";
+import {Component, AfterViewInit} from "@angular/core";
 import {TabService} from '../../layout/navbar/tabstrip/tabService';
 import {View} from '../../../models/view/view';
+import {AuthService} from '../../../../framework/core/authService';
+import {UniHttp} from '../../../../framework/core/http/http';
+import {Worker} from '../../../unientities';
 
 export var view = new View('timeentry', 'Registrere timer', 'TimeEntry');
 
@@ -9,9 +12,47 @@ export var view = new View('timeentry', 'Registrere timer', 'TimeEntry');
     templateUrl: 'app/components/timetracking/timeentry/timeentry.html', 
 })
 export class TimeEntry {    
-    public view = view;
+    view = view;
+    user = {
+        id: 0,
+        name: '',
+        email: ''
+    };
+    worker: Worker = new Worker();
 
-    constructor(private tabService: TabService) {
+    constructor(private tabService: TabService, authService: AuthService, private http:UniHttp) {
         this.tabService.addTab({ name: view.label, url: view.route });
+        
+        this.user.name = authService.jwtDecoded.unique_name;
+        this.user.id = authService.jwtDecoded.userId;
+        this.initUser();
     }
+    
+    ngAfterViewInit() {               
+    }
+    
+    initUser() {
+        if (this.user.id) {
+            this.initWorker(this.user.id);
+            return;
+        }
+        var route = "users/?select=id,email&filter=displayname eq '" + this.user.name + "'";
+        this.http.asGET().usingBusinessDomain()
+        .withEndPoint(route).send().subscribe((result)=> {
+            if (result.length>0) {
+                var item = result[0];
+                this.user.id = item.ID;
+                this.user.email = item.Email;
+            } 
+        });          
+    }
+    
+    initWorker(userid:number) {
+        var route = "workers?action=create-worker-from-user&userid=" + userid;
+        this.http.asGET().usingBusinessDomain()
+        .withEndPoint(route).send().subscribe((result:Worker)=> {
+            this.worker = result;
+        });
+    }
+    
 }
