de44b9d3dc43e8805ef47b6f6b818f2ca6f954d3
Sveinung Farestveit <sveinung@unimicro.no>
2016-05-11 09:58:28 +0200

    feature(timetracking): create subview for timeentries

--- src/app/components/timetracking/timeentry/timeentry.ts
+++ src/app/components/timetracking/timeentry/timeentry.ts
@@ -6,6 +6,6 @@ import {Component} from "@angular/core";
 })
 export class TimeEntry {    
     constructor() {
-  
+        console.log("TimeEntry (sub)view constructor");
     }
 }
--- src/app/components/timetracking/timetracking.ts
+++ src/app/components/timetracking/timetracking.ts
@@ -4,11 +4,26 @@ import {ComponentProxy} from "../../../framework/core/componentProxy";
 import {AsyncRoute} from "@angular/router-deprecated";
 import {UniRouterOutlet} from "../../uniRouterOutlet";
 
+const $BASE_FOLDER = "./app/components/timetracking/";
+
+const TIMETRACKING_ROUTES = [
+    new AsyncRoute({
+        useAsDefault: true,
+        path: '/timeentry',
+        name: 'TimeEntry',
+        loader: () => ComponentProxy.LoadComponentAsync('TimeEntry', $BASE_FOLDER + 'timeentry/timeentry')
+    }),
+];
 
 @Component({
-    //selector: 'uni-sales',
-    template: '<h3>Timetracking</h3>',
-    //directives: [ROUTER_DIRECTIVES, UniRouterOutlet]
+    selector: 'uni-timetracking',
+    template: `<h3>Timetracking</h3>
+        <uni-router-outlet></uni-router-outlet>`,
+    directives: [ROUTER_DIRECTIVES, UniRouterOutlet]
 })
-//@RouteConfig(TIMETRACKING_ROUTES)
-export class UniTimetracking {}
+@RouteConfig(TIMETRACKING_ROUTES)
+export class UniTimetracking {
+    constructor() {
+        console.log("UniTimetracking view constructor");
+    }
+}
