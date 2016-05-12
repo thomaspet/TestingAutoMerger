331991c3c37d0075a5e091763f1d44305657dd6a
Sveinung Farestveit <sveinung@unimicro.no>
2016-05-12 13:04:54 +0200

    feature(timetracking): initialize worker/profile and workrelation from user

--- src/app/components/timetracking/timeentry/timeentry.ts
+++ src/app/components/timetracking/timeentry/timeentry.ts
@@ -3,7 +3,9 @@ import {TabService} from '../../layout/navbar/tabstrip/tabService';
 import {View} from '../../../models/view/view';
 import {AuthService} from '../../../../framework/core/authService';
 import {UniHttp} from '../../../../framework/core/http/http';
-import {Worker} from '../../../unientities';
+import {Worker, WorkRelation, WorkProfile, WorkItem} from '../../../unientities';
+import {Observable} from "rxjs/Observable";
+declare var moment;
 
 export var view = new View('timeentry', 'Registrere timer', 'TimeEntry');
 
@@ -17,9 +19,12 @@ export class TimeEntry {
         id: 0,
         guid: '',
         name: '',
-        email: ''
+        email: '',
+        company: ''
     };
     worker: Worker = new Worker();
+    positions: Array<WorkRelation> = [];
+    profiles: Array<WorkProfile> = [];
 
     constructor(private tabService: TabService, authService: AuthService, private http:UniHttp) {
         this.tabService.addTab({ name: view.label, url: view.route });
@@ -27,11 +32,12 @@ export class TimeEntry {
         this.user.name = authService.jwtDecoded.unique_name;
         this.user.id = authService.jwtDecoded.userId;
         this.user.guid = authService.jwtDecoded.nameid;
+        this.user.company = authService.jwtDecoded.company;
         this.initUser();
     }
     
     ngAfterViewInit() {
-                       
+
     }
     
     initUser() {
@@ -42,8 +48,7 @@ export class TimeEntry {
         // todo: when authService includes userid this can be removed:
         var route = "users";
         var guid = this.user.guid;
-        this.http.asGET().usingBusinessDomain()
-        .withEndPoint(route).send().subscribe((result)=> {
+        this.GET(route).subscribe((result)=> {
             if (result.length>0) {
                 for (var i=0; i<result.length; i++) {
                     var item = result[i];
@@ -60,14 +65,66 @@ export class TimeEntry {
     
     initWorker(userid:number) {
         var route = "workers/?action=create-worker-from-user&userid=" + userid;
-        this.http.asPOST().usingBusinessDomain()
-        .withEndPoint(route).send().subscribe((result:Worker)=> {
+        this.POST(route).subscribe((result:Worker)=> {
             this.worker = result;
+            this.initWorkRelations(this.worker.ID);
         });
     }
     
-    initWorkRelations() {
-        
+    initWorkRelations(workerId:number, autoCreate = true) {
+        var route = "workrelations";
+        this.GET(route, { filter:'workerid eq ' + workerId, expand: 'workprofile' } ).subscribe((result:Array<WorkRelation>) => {
+            if (result.length>0) {
+                this.positions = result;
+            } else {
+                if (autoCreate) {
+                    this.GET('workprofiles').subscribe((result: Array<WorkProfile>)=> {
+                        if (result.length>0) {
+                            this.createInitialWorkRelation(workerId, result[0]);
+                        }    
+                    });
+                }                
+            }
+        });
     }
+    
+    initWorkProfiles() {
+        this.GET('workprofiles').subscribe((result: Array<WorkProfile>)=> {
+            this.profiles = result;
+        });
+    }
+
+    
+    createInitialWorkRelation(workerId: number, workProfile: WorkProfile) {
+        var route = "workrelations";
+        var rel = {
+            WorkerID: workerId,
+            CompanyName: this.user.company,
+            WorkPercentage: 100,
+            Description: "Ansatt",
+            StartDate: moment([moment().year(), moment().month, 1]).date(),
+            IsActive: true,
+            WorkProfileID: workProfile.ID
+        };        
+        this.POST(route, undefined, rel).subscribe((result:WorkRelation) => {
+            this.initWorkRelations(workerId, false);
+        });
+    }
+    
+    // Internal helpers (less verbose)
+    
+    private GET(route: string, cargo?:any ):Observable<any> {
+        return this.http.asGET().usingBusinessDomain()
+        .withEndPoint(route).send(cargo);
+    }
+    private POST(route: string, cargo?:any, body?:any ):Observable<any> {
+        if (body) {
+            return this.http.asPOST().usingBusinessDomain().withBody(body)
+            .withEndPoint(route).send(cargo);            
+        } else {
+            return this.http.asPOST().usingBusinessDomain()
+            .withEndPoint(route).send(cargo);
+        }        
+    }    
     
 }
