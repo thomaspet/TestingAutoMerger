78397973c2eaec5275dff4925aa3c06d83c5a6bb
Sondre Eide Vik <sondre.vik@unimicro.no>
2016-05-18 13:02:41 +0200

    Refactor(Altinn): Changes after CR
    found a shorter url to Altinn
    
    ref #459

--- src/app/components/settings/altinnSettings/altinnSettings.ts
+++ src/app/components/settings/altinnSettings/altinnSettings.ts
@@ -1,10 +1,8 @@
-import {Component, OnInit, ChangeDetectionStrategy, ViewChild, ComponentRef} from '@angular/core';
-import {UniFieldBuilder, UniFormBuilder, UniForm, UniFormLayoutBuilder} from '../../../../framework/forms';
-import {UNI_CONTROL_DIRECTIVES} from '../../../../framework/controls';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UniFormBuilder, UniForm, UniFormLayoutBuilder} from '../../../../framework/forms';
 import {AltinnService} from '../../../services/services';
-import { FieldType, Altinn, ComponentLayout} from '../../../unientities';
-import {UniComponentLoader} from '../../../../framework/core';
-import { Observable } from 'rxjs/Observable';
+import {Altinn} from '../../../unientities';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: 'altinn-settings',
@@ -15,10 +13,7 @@ import { Observable } from 'rxjs/Observable';
 export class AltinnSettings implements OnInit {
     private formConfig: UniFormBuilder = null;
     private altinn: Altinn;
-    //private whenFormInstance: Promise<UniForm>;
-    //private formInstance: UniForm;
-
-    //@ViewChild(UniComponentLoader) private uniCompLoader: UniComponentLoader;
+    
     @ViewChild(UniForm) private formInstance: UniForm;
 
     constructor(private _altinnService: AltinnService) {
@@ -27,11 +22,6 @@ export class AltinnSettings implements OnInit {
 
     public ngOnInit() {
         this.getData();
-        /*this.formInstance.ready.subscribe(() => {
-            this.formInstance.submit.subscribe((event) => {
-                this.saveAltinn(event);
-            });
-        });*/
     }
 
     private getData() {
@@ -64,12 +54,10 @@ export class AltinnSettings implements OnInit {
     public saveAltinn(event) {
         this.formInstance.sync();
         if (this.altinn.ID) {
-            console.log('put');
             this._altinnService.Put(this.altinn.ID, this.altinn).subscribe((response: Altinn) => {
                 this.altinn = response;
             }, error => console.log(error));
         } else {
-            console.log('Post');
             this._altinnService.Post(this.altinn).subscribe((response: Altinn) => {
                 this.altinn = response;
             }, error => console.log(error));
