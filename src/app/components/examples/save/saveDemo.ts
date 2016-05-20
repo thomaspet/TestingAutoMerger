4eefbea78dc805bd1d3d81c9b14062ecab302bf2
Jørgen Arnor G. Lom <git@lom.me>
2016-05-20 10:11:23 +0200

    Removes unused code
    Signed-off-by: Jørgen Arnor G. Lom <git@lom.me>

--- src/app/components/examples/save/saveDemo.ts
+++ src/app/components/examples/save/saveDemo.ts
@@ -5,11 +5,9 @@ import {UniSave, IUniSaveAction} from '../../../../framework/save/save';
     selector: 'uni-save-demo',
     template: `
         <h1>Save demo</h1>
-        
-        <uni-save [actions]="myActions" [status]="status" (save)="onSave($event)"></uni-save> 
-        
-        {{status}}
-        
+
+        <uni-save [actions]="myActions" [status]="status" (save)="onSave($event)"></uni-save>
+
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     `,
     directives: [UniSave]
@@ -52,14 +50,6 @@ export class UniSaveDemo {
         action.callback(action, this);
     }
 
-    public ngOnInit(){
-        window.setTimeout(() => {
-            console.log(this);
-            this.myActions[0].disabled = false;
-            this.myActions = [this.myActions[0], ...this.myActions.splice(0,1)]; 
-        }, 3000);
-    }
-
     // You provide your own save functions and queueings, the save component
     // is simply responsible for UI and event emission.
     private save(event, self) {
@@ -68,18 +58,10 @@ export class UniSaveDemo {
         event.busy = true;
         window.setTimeout(() => {
             event.busy = false;
-            self.status = 'Du lagret, just nu'; 
-            
-            // let _actions = this.myActions;
-            // _actions.push({
-            //     verb: 'hello', 
-            //     callback: this.saveAndBank
-            // });
-            // this.myActions = _actions;
-            
+            self.status = 'Du lagret, just nu';
         }, 2000);
     }
-    
+
     private saveAndBank() {
         console.log('Hello there');
     }
