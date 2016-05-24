db3da60070fcf58964e4c85f535847f62b465356
Sveinung Farestveit <sveinung@unimicro.no>
2016-05-24 10:24:57 +0200

    feature(timetracking): initial data

--- src/app/components/timetracking/regtime/regtime.ts
+++ src/app/components/timetracking/regtime/regtime.ts
@@ -8,6 +8,8 @@ import {WorkerService} from '../../../services/timetracking/workerservice';
 
 export var view = new View('regtime', 'Timeregistrering', 'RegisterTime');
 
+declare var moment;
+
 @Component({
     selector: view.name,
     templateUrl: 'app/components/timetracking/regtime/regtime.html',
@@ -19,9 +21,8 @@ export class RegisterTime {
     public view = view;
     
     private tableConfig: UniTableConfig;
-    private tableData:Array<WorkItem> = [];
+    private tableData:Array<any> = [];
     private worktypes:Array<WorkType> = [];
-    private types:Observable<Array<WorkType>>;
     
     tabs = [ { name: 'timeentry', label: 'Timer', isSelected: true },
             { name: 'totals', label: 'Totaler' },
@@ -33,61 +34,63 @@ export class RegisterTime {
 
     constructor(private tabService: TabService, private service:WorkerService) {
         this.tabService.addTab({ name: view.label, url: view.route });
-        //this.addNewRow();
+        this.addNewRow();
         this.tableConfig = this.createTableConfig();
         
         this.initServiceValues();        
     }
     
+    defaultType():WorkType {
+        var t = new WorkType();
+        t.ID = 1;
+        t.Name = 'Diverse..';
+        return t;
+    }
+    
     initServiceValues() {
-        this.types = this.service.getWorkTypes();
-        this.types.subscribe((result:Array<WorkType>)=>{
+        this.service.getWorkTypes().subscribe((result:Array<WorkType>)=>{
             this.worktypes = result;
             console.log('list returned from service', this.worktypes.length);
         });        
     }
     
 	addNewRow(text = 'Fyll inn beskrivelse') {
-        var row = new WorkItem();
-        row.Description = text;
-        row.Date = new Date();
-        row.StartTime = new Date();
-        row.EndTime = new Date();
-        row.WorkTypeID = 1;
-        row.Worktype = this.getWorkType(row.WorkTypeID);
+        var workType = this.defaultType();
+        var row = {
+            Description: text,
+            Date: new Date(),
+            StartTime: moment().hours(8).toDate(),
+            EndTime: new Date(),
+            WorkTypeID: workType.ID,
+            Worktype: workType
+        };
+        debugger;
         this.tableData.push(row);
 	}    
     
     getWorkType(id:number): WorkType {
-        return this.worktypes.find((item:WorkType)=> {
+        var tp = this.worktypes.find((item:WorkType)=> {
            return item.ID === id;  
-        });        
+        });     
+        return tp || this.defaultType();
     }
     
-    findWorkType(searchValue:string):Observable<any> {
-        return this.types;
-//        debugger;
-//        var list = this.worktypes || [this.getWorkType(1)]; 
-//        console.log('list with ' + list.length + ' items');
-//        return Observable.from(list);
+    findWorkType(txt:string):Observable<any> {
+        var list = this.worktypes || [this.getWorkType(1)]; 
+        var lcaseText = txt.toLowerCase();
+        var sublist = list.filter((item:WorkType)=>
+            { return (item.ID.toString() == txt || item.Name.toLowerCase().indexOf(lcaseText)>=0); } );
+        return Observable.from([sublist]);
     }
     
     createTableConfig():UniTableConfig {        
         
         var cols = [
             new UniTableColumn('Description', 'Beskrivelse', UniTableColumnType.Text, true).setWidth('40vw'),
-            new UniTableColumn('StartTime', 'Fra kl.', UniTableColumnType.Text, true),
-            new UniTableColumn('EndTime', 'Til kl.', UniTableColumnType.Text, true),
-            new UniTableColumn('Date', 'Dato', UniTableColumnType.Date, true),
-            
-            this.createLookupColumn('WorkType', 'Type #', 'WorkType', 'ID', 'Name', (txt)=>{
-                //return this.types;
-                return Observable.from([this.worktypes]);
-                //return this.service.getWorkTypes();
-                //var x = this.findWorkType(txt);
-                //console.log('workitems:', x); 
-                //return x; 
-            })                      
+            this.createTimeColumn('StartTime', 'Fra kl.'),
+            this.createTimeColumn('EndTime', 'Til kl.'),
+            new UniTableColumn('Date', 'Dato', UniTableColumnType.Date, true),            
+            this.createLookupColumn('Worktype', 'Type arbeid', 'Worktype', (txt) => this.findWorkType(txt))                      
         ];
 
         return new UniTableConfig(true, true, 25).setColumns(cols).setChangeCallback((event)=>this.onEditChange(event));
@@ -96,22 +99,40 @@ export class RegisterTime {
     onEditChange(event) {
         var newRow = event.rowModel;
 
-        if (event.field === 'WorkType') {
-           
+        if (event.field === 'Worktype') {
+            debugger;
+            newRow.WorkTypeID = newRow.WorkType ? newRow.WorkType.ID : 0;            
+            console.log(event.fielt, event);
         }
+        
+        if (event.field)
 
         return newRow; 
     }
     
-    createLookupColumn(name:string, label: string, expandCol:string, expandKey = 'ID', expandLabel = 'Name', lookupFn?: any): UniTableColumn {
+    // UniTable helperes:
+    
+    createTimeColumn(name:string, label:string): UniTableColumn {
+        return new UniTableColumn(name, label, UniTableColumnType.Text)
+            .setTemplate((item:any)=>{
+                var value =item[name];
+                if (value) {
+                    return moment(value).format("HH:mm");
+                }
+                return '';                    
+            });        
+    }
+    
+    createLookupColumn(name:string, label: string, expandCol:string, lookupFn?: any, expandKey = 'ID', expandLabel = 'Name'): UniTableColumn {
+        console.log(`${expandCol}.${expandLabel}`);
         var col = new UniTableColumn(name, label, UniTableColumnType.Lookup)
-                .setDisplayField(`${expandCol}.${expandLabel}`)
-                .setEditorOptions({
-                    itemTemplate: (item)=> {
-                        return item[expandKey] + ' - ' + item[expandLabel];
-                    },
-                    lookupFunction: lookupFn
-                });
+            .setDisplayField(`${expandCol}.${expandLabel}`)
+            .setEditorOptions({
+                itemTemplate: (item)=> {
+                    return item[expandKey] + ' - ' + item[expandLabel];
+                },
+                lookupFunction: lookupFn
+            });
         return col;
     }
 
