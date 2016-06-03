8015341fa7f779d3d92174850f9e0bdf5941b5ad
jorge <jorge@unimicro.no>
2016-06-03 10:36:13 +0200

    add correct interfaces

--- src/framework/uniform/index.ts
+++ src/framework/uniform/index.ts
@@ -3,7 +3,6 @@ export * from './unisection';
 export * from './unifieldset';
 export * from './unicombo';
 export * from './unifield';
-export * from './unifieldlayout';
 export * from './validators';
 export * from './showError';
 export * from './interfaces';
