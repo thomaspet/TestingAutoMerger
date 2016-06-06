ba1c91128a2d266314f05c92d6f2420906300e22
Frank Atle Rød <frank@unimicro.no>
2016-06-06 11:05:40 +0200

    remove sourcemaps from production build

--- gulp/ts2js.js
+++ gulp/ts2js.js
@@ -10,11 +10,9 @@ var tsproject = plugins.typescript.createProject('tsconfig.json');
 gulp.task('ts2js', function() {
     return tsproject.src()
         .pipe(plugins.plumber())
-        .pipe(plugins.sourcemaps.init())
         .pipe(plugins.typescript(tsproject))
         .pipe(plugins.concat('app.bundle.js'))
         .pipe(plugins.uglify())
-        .pipe(plugins.sourcemaps.write('.'))
         .pipe(gulp.dest(DIST));
 });
 
