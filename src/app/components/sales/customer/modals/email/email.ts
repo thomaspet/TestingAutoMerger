d87ee72ebb045a6ac46a52b3cb80ca44d8270df8
Kjetil Ek <kjetil@unimicro.no>
2016-06-15 10:48:43 +0200

    refactor (sales): refactor modals to use new uniform
    closes #672
    ref #664
    ref #665

--- src/app/components/sales/customer/modals/email/email.ts
+++ src/app/components/sales/customer/modals/email/email.ts
@@ -1,154 +1,117 @@
-import {Component, ViewChildren, Type, Input, Output, QueryList, ViewChild, ComponentRef, EventEmitter} from "@angular/core";
-import {NgIf, NgModel, NgFor, NgClass} from "@angular/common";
-import {UniModal} from "../../../../../../framework/modals/modal";
-import {UniComponentLoader} from "../../../../../../framework/core/componentLoader";
-import {UniFormBuilder} from "../../../../../../framework/forms/builders/uniFormBuilder";
-import {UniForm} from "../../../../../../framework/forms/uniForm";
-import {UNI_CONTROL_DIRECTIVES} from "../../../../../../framework/controls";
-import {UniFieldBuilder} from "../../../../../../framework/forms/builders/uniFieldBuilder";
-import {FieldType, ComponentLayout, Email} from "../../../../../unientities";
-import {UniFormLayoutBuilder} from "../../../../../../framework/forms/builders/uniFormLayoutBuilder";
-import {EmailService} from "../../../../../services/services";
+import {Component, Type, Input, Output, ViewChild, EventEmitter} from '@angular/core';
+import {UniModal} from '../../../../../../framework/modals/modal';
+import {UniForm} from '../../../../../../framework/uniform';
+import {Email} from '../../../../../unientities';
+import {EmailService} from '../../../../../services/services';
 
 // Reusable email form
 @Component({
     selector: 'email-form',
-    directives: [UniForm,NgIf],
+    directives: [UniForm],
     template: `
-        <uni-form *ngIf="config" [config]="config">
+        <uni-form *ngIf="config" [config]="config" [fields]="fields" [model]="model">
         </uni-form>
     `
 })
-export class EmailForm {
-    config: UniFormBuilder;
-
-    @ViewChild(UniForm)
-    form: UniForm;
-
-    model: Email;
+export class EmailForm {    
+    @Input() public model: Email;
+    @ViewChild(UniForm) public form: UniForm;
+    private config: any = {};
+    private fields: any[] = [];
        
-    ngOnInit()
-    {
-        this.createFormConfig();      
+    public ngOnInit() {
+        this.setupForm();      
     }
  
-    createFormConfig() {   
+    private setupForm() {   
         // TODO get it from the API and move these to backend migrations
         // TODO: turn to 'ComponentLayout when the object respects the interface
-        var view: any = {
-            Name: "Email",
-            BaseEntity: "Email",
-            StatusCode: 0,
-            Deleted: false,
-            CreatedAt: null,
-            UpdatedAt: null,
-            CreatedBy: null,
-            UpdatedBy: null,
-            ID: 3,
-            CustomFields: null,
-            Fields: [
-                {
-                    ComponentLayoutID: 1,
-                    EntityType: "Email",
-                    Property: "EmailAddress",
-                    Placement: 1,
-                    Hidden: false,
-                    FieldType: 11,
-                    ReadOnly: false,
-                    LookupField: false,
-                    Label: "Epostadresse",
-                    Description: "",
-                    HelpText: "",
-                    FieldSet: 0,
-                    Section: 0,
-                    Placeholder: null,
-                    Options: null,
-                    LineBreak: null,
-                    Combo: null,
-                    Legend: "",
-                    StatusCode: 0,
-                    ID: 1,
-                    Deleted: false,
-                    CreatedAt: null,
-                    UpdatedAt: null,
-                    CreatedBy: null,
-                    UpdatedBy: null,
-                    CustomFields: null 
-                },
-                {
-                    ComponentLayoutID: 1,
-                    EntityType: "Email",
-                    Property: "Description",
-                    Placement: 1,
-                    Hidden: false,
-                    FieldType: 10,
-                    ReadOnly: false,
-                    LookupField: false,
-                    Label: "Beskrivelse",
-                    Description: "",
-                    HelpText: "",
-                    FieldSet: 0,
-                    Section: 0,
-                    Placeholder: null,
-                    Options: null,
-                    LineBreak: null,
-                    Combo: null,
-                    Legend: "",
-                    StatusCode: 0,
-                    ID: 2,
-                    Deleted: false,
-                    CreatedAt: null,
-                    UpdatedAt: null,
-                    CreatedBy: null,
-                    UpdatedBy: null,
-                    CustomFields: null 
-                } 
-            ]               
-        };   
-        
-        this.config = new UniFormLayoutBuilder().build(view, this.model);
-        this.config.hideSubmitButton();
+        this.fields = [
+            {
+                ComponentLayoutID: 1,
+                EntityType: 'Email',
+                Property: 'EmailAddress',
+                Placement: 1,
+                Hidden: false,
+                FieldType: 11,
+                ReadOnly: false,
+                LookupField: false,
+                Label: 'Epostadresse',
+                Description: '',
+                HelpText: '',
+                FieldSet: 0,
+                Section: 0,
+                Placeholder: null,
+                Options: null,
+                LineBreak: null,
+                Combo: null,
+                Legend: '',
+                StatusCode: 0,
+                ID: 1,
+                Deleted: false,
+                CreatedAt: null,
+                UpdatedAt: null,
+                CreatedBy: null,
+                UpdatedBy: null,
+                CustomFields: null 
+            },
+            {
+                ComponentLayoutID: 1,
+                EntityType: 'Email',
+                Property: 'Description',
+                Placement: 1,
+                Hidden: false,
+                FieldType: 10,
+                ReadOnly: false,
+                LookupField: false,
+                Label: 'Beskrivelse',
+                Description: '',
+                HelpText: '',
+                FieldSet: 0,
+                Section: 0,
+                Placeholder: null,
+                Options: null,
+                LineBreak: null,
+                Combo: null,
+                Legend: '',
+                StatusCode: 0,
+                ID: 2,
+                Deleted: false,
+                CreatedAt: null,
+                UpdatedAt: null,
+                CreatedBy: null,
+                UpdatedBy: null,
+                CustomFields: null 
+            } 
+        ];
     }
 
 }
 
 // email modal type
 @Component({
-    selector: "email-modal-type",
-    directives: [NgIf, NgModel, NgFor, NgClass, UniComponentLoader],
+    selector: 'email-modal-type',
+    directives: [EmailForm],
     template: `
         <article class="modal-content email-modal">
             <h1 *ngIf="config.title">{{config.title}}</h1>
-            <uni-component-loader></uni-component-loader>
+            <email-form [model]="config.model"></email-form>
             <footer>
                 <button *ngFor="let action of config.actions; let i=index" (click)="action.method()" [ngClass]="action.class">
                     {{action.text}}
-                </button>
+                </button>                
             </footer>
         </article>
     `
 })
 export class EmailModalType {
-    @Input('config')
-    config;
-    @ViewChild(UniComponentLoader)
-    ucl: UniComponentLoader;
-    instance: Promise<EmailForm>;
-            
-    ngAfterViewInit() {
-        var self = this;
-        this.ucl.load(EmailForm).then((cmp: ComponentRef<any>)=> {
-            cmp.instance.model = self.config.model;
-            self.instance = new Promise((resolve)=> {
-                resolve(cmp.instance);
-            });
-        });
-    }
+    @Input() public config: any;
+    @ViewChild(EmailForm) public form: EmailForm;    
 }
 
 // email modal
 @Component({
-    selector: "email-modal",
+    selector: 'email-modal',
     template: `
         <uni-modal [type]="type" [config]="modalConfig"></uni-modal>
     `,
@@ -156,53 +119,38 @@ export class EmailModalType {
     providers: [EmailService]
 })
 export class EmailModal {
-    @ViewChild(UniModal)
-    modal: UniModal;
+    @Input() public email: Email;    
+    @ViewChild(UniModal) public modal: UniModal;
     
-    @Output() Changed = new EventEmitter<Email>();
-    @Output() Canceled = new EventEmitter<boolean>();
+    @Output() public Changed = new EventEmitter<Email>();
+    @Output() public Canceled = new EventEmitter<boolean>();
 
-    modalConfig: any = {};
-    
+    private modalConfig: any = {};    
 
-    type: Type = EmailModalType;
+    private type: Type = EmailModalType;
 
     constructor(private emailService: EmailService) {
-        var self = this;
+    }
+    
+    public ngOnInit() {    
         this.modalConfig = {
-            title: "Epost",
+            model: this.email,            
+            title: 'Epost',
             actions: [
                 {
-                    text: "Lagre epost",
-                    class: "good",
-                    method: () => {
-                        self.modal.getContent().then((content: EmailModalType)=> {
-                            content.instance.then((form: EmailForm)=> {
-                                form.form.sync();
-                                self.modal.close();       
-                                
-                                // store
-                                //if(form.model.ID) {
-                                //    emailService.Put(form.model.ID, form.model).subscribe(null, (error: Error) => console.log('error in updating phone from modal - Put: ' + error));
-                                //} else {
-                                //    emailService.Post(form.model).subscribe(null, (error: Error) => console.error('error in posting phone from modal - Post: ', error));
-                                //}
-                                                        
-                                self.Changed.emit(form.model);
-                            });
-                        });
-                        
+                    text: 'Lagre epost',
+                    class: 'good',
+                    method: () => {                        
+                        this.modal.close();
+                        this.Changed.emit(this.modalConfig.model);
                         return false;
                     }
                 },
                 {
-                    text: "Angre",
+                    text: 'Angre',
                     method: () => {
-                        self.modal.getContent().then(() => {
-                            self.modal.close();
-                            self.Canceled.emit(true);
-                        });
-                        
+                        this.modal.close();
+                        this.Canceled.emit(true);
                         return false;
                     }
                 }
@@ -210,7 +158,8 @@ export class EmailModal {
         };
     }
 
-    openModal() {
+    public openModal(email: Email) {  
+        this.modalConfig.model = email;    
         this.modal.open();
     }
 }
