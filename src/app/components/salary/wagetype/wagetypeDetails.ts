cebde49000b28b46b337190f5b4a7ca547ba61ab
Sondre Eide Vik <sondre.vik@unimicro.no>
2016-06-28 09:04:22 +0200

    Feature(WageType): fix section toggle
    section is now always open
    Made the tab show Wag type number instead of id and "new wage type" when making a new wage type
    fixed issue with wageTypeNumber being readonly when making a new wage type
    added login
    
    ref #675

--- src/app/components/salary/wagetype/wagetypeDetails.ts
+++ src/app/components/salary/wagetype/wagetypeDetails.ts
@@ -29,7 +29,7 @@ export class WagetypeDetail {
     public config: any = {};
     public fields: any[] = [];
     @ViewChild(UniForm) public uniform: UniForm;
-    
+
     constructor(private routeparams: RouteParams, private router: Router, private wageService: WageTypeService, private tabService: TabService) {
 
         this.config = {
@@ -37,10 +37,8 @@ export class WagetypeDetail {
         };
         this.wagetypeID = +this.routeparams.get('id');
         this.getLayoutAndData();
-        //WageTypeID is not correct!! Needs to be sorted out
-        this.tabService.addTab({ name: 'Lønnsartnr. ' + this.wagetypeID, url: 'salary/wagetypes/' + this.wagetypeID, moduleID: 13, active: true });
     }
-    
+
     private getLayoutAndData() {
         Observable.forkJoin(
             this.wageService.getWageType(this.wagetypeID),
@@ -48,22 +46,29 @@ export class WagetypeDetail {
         ).subscribe(
             (response: any) => {
                 let [wagetype, layout] = response;
-                
+
                 this.wageType = wagetype;
 
                 if (this.wageType.ID === 0) {
                     this.wageType.WageTypeId = null;
                     this.wageType.AccountNumber = null;
+                    this.tabService.addTab({ name: 'Ny lønnsart', url: 'salary/wagetypes/' + this.wagetypeID, moduleID: 13, active: true });
+                } else {
+                    this.tabService.addTab({ name: 'Lønnsartnr. ' + this.wageType.WageTypeId, url: 'salary/wagetypes/' + this.wagetypeID, moduleID: 13, active: true });
                 }
-                
+
                 this.fields = layout.Fields;
+
+                let wageTypeNumber: UniFieldLayout = this.findByProperty(this.fields, 'WageTypeId');
+                wageTypeNumber.ReadOnly = this.wagetypeID > 0;
+                this.fields = _.cloneDeep(this.fields);
 
                 this.config = {
                     submitText: ''
                 };
                 this.toggleAccountNumberBalanceHidden();
             }
-        );
+            );
     }
 
     private toggleAccountNumberBalanceHidden() {
@@ -71,68 +76,81 @@ export class WagetypeDetail {
         if (accountNumberBalance.Hidden !== this.wageType.Base_Payment) {
             accountNumberBalance.Hidden = this.wageType.Base_Payment;
             this.fields = _.cloneDeep(this.fields);
-            setTimeout(() => {
-                this.uniform.section(1).toggle();
-            }, 100);
         }
+        setTimeout(() => {
+            if (!this.uniform.section(1).isOpen) {
+                this.uniform.section(1).toggle();
+            }
+        }, 100);
     }
 
     public ready(value) {
-        
+        console.log('form ready');
     }
-    
+
     public change(value) {
         this.toggleAccountNumberBalanceHidden();
         this.saveactions[0].disabled = false;
     }
-    
+
     public saveWagetype(done) {
         done('Lagrer lønnsart');
         if (this.wageType.ID > 0) {
             this.wageService.Put(this.wageType.ID, this.wageType)
-            .subscribe((wagetype) => {
-                this.wageType = wagetype;
-                done('Sist lagret: ');
-                this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
-            },
-            (err) => {
-                console.log('Feil ved oppdatering av lønnsart', err);
-            });
+                .subscribe((wagetype) => {
+                    this.wageType = wagetype;
+                    done('Sist lagret: ');
+                    this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
+                },
+                (err) => {
+                    this.log(err);
+                    console.log('Feil ved oppdatering av lønnsart', err);
+                });
         } else {
             this.wageService.Post(this.wageType)
-            .subscribe((wagetype) => {
-                this.wageType = wagetype;
-                done('Sist lagret: ');
-                this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
-            },
-            (err) => {
-                console.log('Feil ved lagring av lønnsart', err);
-            });
+                .subscribe((wagetype) => {
+                    this.wageType = wagetype;
+                    done('Sist lagret: ');
+                    this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
+                },
+                (err) => {
+                    this.log(err);
+                    console.log('Feil ved lagring av lønnsart', err);
+                });
         }
     }
 
     private findByProperty(fields, name) {
         var field = fields.find((fld) => fld.Property === name);
-        return field; 
+        return field;
     }
-    
+
     public previousWagetype() {
         this.wageService.getPrevious(this.wageType.ID)
-        .subscribe((response) => {
-            if (response) {
-                this.wageType = response;
-                this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
-            }
-        });
+            .subscribe((response) => {
+                if (response) {
+                    this.wageType = response;
+                    this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
+                }
+            });
     }
-    
+
     public nextWagetype() {
         this.wageService.getNext(this.wageType.ID)
-        .subscribe((response) => {
-            if (response) {
-                this.wageType = response;
-                this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
-            }
-        });
+            .subscribe((response) => {
+                if (response) {
+                    this.wageType = response;
+                    this.router.navigateByUrl('/salary/wagetypes/' + this.wageType.ID);
+                }
+            });
+    }
+
+    public log(err) {
+        if (err._body) {
+            alert(err._body);
+        } else {
+            alert(JSON.stringify(err));
+        }
+        
     }
 }
