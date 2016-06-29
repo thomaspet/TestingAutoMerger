f47a1783945d0abacad75908029bd6089106d24b
Jørgen Arnor G. Lom <j@lom.me>
2016-06-29 21:54:10 +0200

    Adds poster placeholder

--- src/framework/widgetPoster/widgetPoster.ts
+++ src/framework/widgetPoster/widgetPoster.ts
@@ -1,31 +1,64 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ChangeDetectorRef} from '@angular/core';
 declare var Chart;
 
 @Component({
     selector: 'uni-widget-poster',
-    templateUrl: 'framework/widgetPoster/widgetPoster.html'
+    templateUrl: 'framework/widgetPoster/widgetPoster.html',
 })
 export class WidgetPoster {
     @Input() public model: any;
 
+    private rngIteration: number = 0;
+    public randomNumber: string = '42%';
+    private cdr: any;
+
+    constructor(cdr: ChangeDetectorRef) {
+        this.cdr = cdr;
+    }
+
     public ngAfterViewInit() {
-        let chartElem0 = document.getElementById('widgetGraph0');
-        this.lineChartGenerator(chartElem0);
+        let chartElem = document.getElementById('widgetGraph0');
+        this.lineChartGenerator(chartElem);
+        this.randomNumberGenerator();
+    }
+
+    private randomNumberGenerator() {
+        
+        let iterations = 50;
+
+        if (this.rngIteration < iterations) {
+            
+            this.rngIteration++;
+            let rando = Math.floor((Math.random() * 94) + 5);
+            this.randomNumber = rando.toString() + '%';
+            this.cdr.detectChanges();
+
+            window.requestAnimationFrame(() => {
+                this.randomNumberGenerator();
+            });
+
+        } else if (this.rngIteration === iterations) {
+            let rando = Math.floor((Math.random() * 50) + 50);
+            this.randomNumber = rando.toString() + '%';
+            this.cdr.detectChanges();
+        }
+
     }
 
     private lineChartGenerator(elem: any) {
-        console.log(elem);
         let chart = new Chart(elem, {
             type: 'line',
             data: {
+                labels: this.labelGenerator(10),
                 datasets: [{
-                    data: this.generateRandomGraphData(8, 175000, 10000, 750),
-                    lineTension: 0,
-                    backgroundColor: 'transparent',
-                    borderColor: 'rgba(0,130,192,.1)'
+                    data: this.generateRandomGraphData(10, 175000, 10000, 750),
+                    backgroundColor: 'rgba(0,130,192,.05)',
+                    borderColor: 'rgba(0,130,192,.1)',
+                    borderWidth: 5
                 }]
             },
             options: {
+                maintainAspectRatio: false,
                 scaleShowLabels : false,
                 tooltips: {
                     enabled: false
@@ -38,7 +71,6 @@ export class WidgetPoster {
                         display: false
                     }],
                     xAxes: [{
-                        type: 'time',
                         display: false
                     }]
                 }
@@ -46,39 +78,30 @@ export class WidgetPoster {
         });
     }
 
-    public generateRandomDateTime(start, end){
-        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    public labelGenerator(numberOfDataPoints){
+        let _data = [];
+        for (var index = 0; index < numberOfDataPoints; index++) {
+            _data.push('');
+        }
+        return _data;
     }
 
     public generateRandomGraphData(
         numberOfDataPoints = 10,
         startValue = 175000,
         maxValueVariance = 1000,
-        trend = 0,
-        startDate = new Date(2016, 1, 1),
-        endDate = new Date()
+        trend = 0
     ) {
 
         let _data = [];
-
-        // Add random dates
         for (var index = 0; index < numberOfDataPoints; index++) {
-            _data.push({x: this.generateRandomDateTime(startDate, endDate)});
-        }
-
-        // Sort the dates
-        _data.sort((a, b) => {
-            return a.x - b.x;
-        });
-
-        // Add values
-        _data.forEach((datapoint, index) => {
             let _currentMin = startValue - maxValueVariance + (trend * index);
             let _currentMax = startValue + maxValueVariance + (trend * index);
             let _currentVal = Math.floor(Math.random() * (_currentMax - _currentMin + 1)) + _currentMin;
-            datapoint.y = _currentVal;
-        });
 
+            _data.push(_currentVal);
+
+        }
         return _data;
     }
 }
