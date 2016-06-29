f19577b4f82d0c44ea0efd20b067dc5e89e1827f
Terje Pedersen <terje@senikk.com>
2016-06-29 07:14:16 +0200

    fix(companydropdown): initial attempt

--- src/app/components/layout/navbar/userinfo/companyDropdown/companyDropdown.ts
+++ src/app/components/layout/navbar/userinfo/companyDropdown/companyDropdown.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router-deprecated';
 import {Observable} from 'rxjs/Observable';
 import {AuthService} from '../../../../../../framework/core/authService';
 import {UniHttp} from '../../../../../../framework/core/http/http';
+import {CompanySettingsService} from '../../../../../services/services';
 
 import 'rxjs/add/observable/fromEvent';
 declare var jQuery;
@@ -17,7 +18,10 @@ export class UniCompanyDropdown implements AfterViewInit, OnDestroy {
     private companyDropdownActive: Boolean;
     private dropdownConfig: kendo.ui.DropDownListOptions;
 
-    constructor(private _router: Router, private _authService: AuthService, private http: UniHttp) {
+    constructor(private _router: Router, 
+                private _authService: AuthService, 
+                private http: UniHttp,
+                private companySettingsService: CompanySettingsService) {
         this.companyDropdownActive = false;
         this.activeCompany = JSON.parse(localStorage.getItem('activeCompany'));
 
