c282246daf08d64ada69a132c6b28643cec3da25
Anders Urrang <anders.urrang@unimicro.no>
2016-07-04 15:54:06 +0200

    feat(toast): first iteration of toast messages
    with example under /#/examples/toast
    
    ref: #232 #233 #234

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,21 +1,22 @@
 /// <reference path="../../typings/browser/ambient/es6-shim/es6-shim.d.ts"/>
 ///<reference path="../../node_modules/immutable/dist/immutable.d.ts"/>
 
-import {provide, enableProdMode} from "@angular/core";
+import {provide, enableProdMode} from '@angular/core';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
-import {bootstrap} from "@angular/platform-browser-dynamic";
-import {ROUTER_PROVIDERS} from "@angular/router-deprecated";
-import {HTTP_PROVIDERS} from "@angular/http";
-import {App} from "./app";
-import {UniHttp} from "../framework/core/http/http";
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {HTTP_PROVIDERS} from '@angular/http';
+import {App} from './app';
+import {UniHttp} from '../framework/core/http/http';
 import {UniState} from '../framework/core/UniState';
-import {REPORT_PROVIDERS} from "./services/reports/index";
+import {REPORT_PROVIDERS} from './services/reports/index';
 import {IntegrationServerCaller} from './services/common/IntegrationServerCaller';
 import {AuthService} from './../framework/core/authService';
 import {AccountService, VatTypeService, CurrencyService, CompanySettingsService} from './services/services';
+import {ToastService} from '../framework/uniToast/toastService';
 
 declare var window;
-if(window.ENV === 'production') {
+if (window.ENV === 'production') {
     enableProdMode();
 }
 
@@ -33,6 +34,7 @@ bootstrap(App, [
     provide(UniState, {useClass: UniState}),
     
     // Services
+    provide(ToastService, {useClass: ToastService}),
     provide(AccountService, {useClass: AccountService}),
     provide(VatTypeService, {useClass: VatTypeService}),
     provide(CurrencyService, {useClass: CurrencyService}),
