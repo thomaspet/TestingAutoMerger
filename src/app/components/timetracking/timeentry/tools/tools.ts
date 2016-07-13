f8d99a1af0742d789c1b722cf5dd1c1c12d7abec
Sveinung Farestveit <sveinung@unimicro.no>
2016-07-13 16:12:25 +0200

    feature(timetracking): timeliste view (for eksport m.m.)

---
+++ src/app/components/timetracking/timeentry/tools/tools.ts
@@ -0,0 +1,84 @@
+import {Component, AfterViewInit} from "@angular/core";
+import {TimeSheet, TimesheetService} from '../../../../services/timetracking/timesheetservice';
+import {WorkerService, ItemInterval} from '../../../../services/timetracking/workerservice';
+import {MinutesToHoursPipe} from '../../utils/pipes';
+import {ICol, Column, ColumnType} from '../../utils/editable/interfaces';
+import {IFilter} from '../timeentry';
+
+@Component({
+    selector: 'regtimetools',
+    templateUrl: 'app/components/timetracking/timeentry/tools/tools.html',
+    pipes: [MinutesToHoursPipe]
+})
+export class RegtimeTools {
+    private timesheet: TimeSheet;
+    private config: {
+        title: string, 
+        items: Array<any>
+    }
+    private filters: Array<IFilter> = [
+        { name: 'today', label: 'I dag', interval: ItemInterval.today },
+        { name: 'week', label: 'Denne uke', interval: ItemInterval.thisWeek},
+        { name: 'month', label: 'Denne måned', interval: ItemInterval.thisMonth },
+        { name: 'months', label: 'Siste 2 måneder', interval: ItemInterval.lastTwoMonths},
+        { name: 'year', label: 'Dette år', interval: ItemInterval.thisYear},
+        { name: 'all', label: 'Alt', interval: ItemInterval.all}
+    ];
+    private currentFilter: IFilter;
+    private busy = true;
+
+    constructor(private workerService:WorkerService, private timesheetService: TimesheetService) {  }
+
+    public activate(ts:TimeSheet, filter?: IFilter) {
+        if (!this.timesheet) { 
+            this.currentFilter = this.filters[2]; 
+        }
+        this.timesheet = ts;
+        this.onFilterClick( this.currentFilter );
+    } 
+
+    onFilterClick(filter: IFilter) {
+        var f:IFilter;
+        this.filters.forEach((value:any) => {
+            if (value.name === filter.name) {
+                f = value;
+                f.isSelected = true;
+            } else {
+                value.isSelected = false
+            }
+        });        
+        this.currentFilter = f;
+        this.queryTotals();
+    }
+    
+
+    showData(items:Array<any>) {
+        this.config = {
+            title: items[0].Name,
+            items: items
+        }
+    }
+
+    private queryTotals() {
+        this.busy = true;
+        var query = "model=workitem";
+        var filter = this.workerService.getIntervalFilter(this.currentFilter.interval);
+        query += this.createArg('select', 'workerid,businessrelation.name,workrelation.description,date,min(starttime),max(endtime),sum(minutes),sum(lunchinminutes)');
+        query += this.createArg('filter', 'deleted eq \'false\'' + (filter ? ' and ( ' +  filter + ' )' : ''));
+        query += this.createArg('join', 'workitem.worktypeid eq worktype.id and workitem.workrelationid eq workrelation.id and workrelation.workerid eq worker.id and worker.businessrelationid eq businessrelation.id');
+        this.workerService.getStatistics(query).subscribe((items:Array<any>)=>{
+            this.busy = false;
+            if (items && items.length>0) {
+                if (items[0].Success) {
+                    this.showData(items[0].Data);
+                } else {
+                    this.showData([{'label':items[0].Message}]);
+                }
+            } 
+        });
+    }
+
+    private createArg(name:string, value:string):string {
+        return '&' + name + '=' + value;
+    }
+}   
