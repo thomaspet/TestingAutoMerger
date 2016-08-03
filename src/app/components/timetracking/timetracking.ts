9c2173604c41ce442f9cc463730519f4c1c1fba5
Sveinung Farestveit <sveinung@unimicro.no>
2016-08-03 12:49:19 +0200

    feature(worker): edit workrelation for worker ref: #1012

--- src/app/components/timetracking/timetracking.ts
+++ src/app/components/timetracking/timetracking.ts
@@ -1,5 +1,5 @@
-import {Component} from "@angular/core";
-import {ROUTER_DIRECTIVES} from "@angular/router";
+import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
 import {View} from '../../models/view/view';
 import {view as workerView} from './worker/workers';
