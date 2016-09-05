475bfa50af39c2e3de3ad4806f9df5dd6bad8c24
Anders Urrang <anders.urrang@unimicro.no>
2016-09-05 14:55:13 +0200

    fix(signup): always update busy/success states in success callback
    closes: #1011

--- src/app/components/init/signup/signup.ts
+++ src/app/components/init/signup/signup.ts
@@ -13,12 +13,12 @@ export class Signup {
     private emailForm: FormGroup = undefined;
     private detailsForm: FormGroup = undefined;
     private isTest: boolean = false;
-    
+
     private passwordsMatching: boolean = false;
-    
+
     private working: boolean = false;
     private existingUser: Object;
-    private success: boolean = false;    
+    private success: boolean = false;
     private errorMessage: string = '';
 
 
@@ -27,20 +27,20 @@ export class Signup {
             Email: new FormControl('', Validators.required)
         });
     }
-    
+
     private usernameValidator(control) {
         const valid = /^[a-z0-9]+$/i.test(control.value);
         console.log('valid: ' + valid);
-        return valid ? null : { 
+        return valid ? null : {
             'validUsername': valid
         };
     }
 
     private checkEmail() {
         this.working = true;
-        
+
         let uriEncoded = encodeURIComponent(this.emailForm.controls['Email'].value);
-        
+
         let passwordValidators = Validators.compose([
             passwordValidator,
             Validators.required,
@@ -52,23 +52,23 @@ export class Signup {
             Validators.required,
             this.usernameValidator
         ]);
-     
+
         this._http.asGET()
             .usingInitDomain()
             .withEndPoint('users?email=' + uriEncoded)
             .send()
             .map(response => response.json())
             .subscribe(
-                (response) => {                                        
-                    if (response['UserName'] && response['FullName']) {                        
+                (response) => {
+                    if (response['UserName'] && response['FullName']) {
                         this.detailsForm = new FormGroup({
                             CompanyName: new FormControl('', Validators.required)
                         });
-                        
+
                         this.existingUser = response;
                         this.working = false;
                     }
-                }, 
+                },
                 (error) => {
                     this.detailsForm = new FormGroup({
                         Name: new FormControl('', Validators.required),
@@ -77,39 +77,39 @@ export class Signup {
                         Password: new FormControl('', passwordValidators),
                         ConfirmPassword: new FormControl('', passwordValidators),
                     });
-                    
+
                     this.detailsForm.controls['Password'].valueChanges.subscribe((value) => {
                         this.passwordsMatching = (value === this.detailsForm.controls['ConfirmPassword'].value);
                     });
-                    
+
                     this.detailsForm.controls['ConfirmPassword'].valueChanges.subscribe((value) => {
                         this.passwordsMatching = (value === this.detailsForm.controls['Password'].value);
                     });
-                    
+
                     this.existingUser = false;
                     this.working = false;
                 }
             );
     }
-    
+
     private submitDetails() {
         this.errorMessage = '';
         this.working = true;
 
-        let controls = this.detailsForm.controls;        
+        let controls = this.detailsForm.controls;
         let body = this.existingUser || {};
-        
+
         body['Email'] = this.emailForm.controls['Email'].value;
         body['CompanyName'] = controls['CompanyName'].value;
         body['IsTest'] = this.isTest;
-        
+
         if (!this.existingUser) {
             body['Name'] = controls['Name'].value;
             body['UserName'] = controls['UserName'].value;
             body['Password'] = controls['Password'].value;
             body['Secret'] = 'uni2016';
         }
-        
+
         this._http.asPOST()
             .usingInitDomain()
             .withEndPoint('sign-up')
@@ -117,10 +117,8 @@ export class Signup {
             .send()
             .subscribe(
                 (response) => {
-                    if (response.status === 200) {
-                        this.success = true;
-                        this.working = false;
-                    }
+                    this.success = true;
+                    this.working = false;
                 },
                 (error) => {
                     this.working = false;
