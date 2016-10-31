e1a5a19b90b5fb8b01379173bcb40ee525f9125f
Anders Urrang <urrang@users.noreply.github.com>
2016-10-31 10:03:46 +0100

    feat(uniform date): improve datepicker in uniform (#1581)
    * feat(uniform date): improve datepicker in uniform
    
    - Init calendar to selected date, not todays date (if there is a selected date)
    - Set min-width to calendar, to prevent it from collapsing on small inputs
    - Import autocompleteDate from unitable repo, to get autocomplete on more input formats
    - case 1 in switch now falls through to case 2 as their logic is the same
    - use moment for checking if inputed day is greater day max day of month, for readability

--- src/framework/uniform/controls/date/calendar.ts
+++ src/framework/uniform/controls/date/calendar.ts
@@ -7,7 +7,7 @@ import moment from 'moment';
         <table>
             <caption>
                 <a class="prev" (click)="prevMonth()"></a>
-                <label>{{selectedDate?.format('MMMM YYYY')}}</label>
+                <label>{{calendarDate?.format('MMMM YYYY')}}</label>
                 <a class="next" (click)="nextMonth()"></a>
             </caption>
 
@@ -22,8 +22,9 @@ import moment from 'moment';
                     <td *ngFor="let weekday of week"
                         (click)="dateSelected(weekday)"
                         [ngClass]="{
-                            'other-month': weekday.month !== selectedDate.month(),
-                            'selected': weekday.day === selectedDate.date() && weekday.month === selectedDate.month()
+                            'other-month': weekday.month !== calendarDate.month(),
+                            'selected': weekday.day === selectedDate.date()
+                                        && weekday.month === selectedDate.month()
                         }">
                         {{weekday.day}}
                     </td>
@@ -41,6 +42,7 @@ export class UniCalendar {
 
     private weekdays: string[];
     private selectedDate: moment.Moment;
+    private calendarDate: moment.Moment;
     private calendarWeeks: any[] = [];
 
     constructor() {
@@ -48,13 +50,14 @@ export class UniCalendar {
     }
 
     public ngOnChanges() {
-        this.selectedDate = moment(this.date);
+        this.selectedDate = moment(this.date || new Date());
+        this.calendarDate = this.selectedDate.clone();
         this.generateCalendarPage(this.selectedDate);
     }
 
     private generateCalendarPage(date: moment.Moment) {
         const daysInMonth = date.daysInMonth();
-        const firstDayOfMonth = date.startOf('month').weekday();
+        const firstDayOfMonth = date.clone().startOf('month').weekday();
         let days = [];
 
         for (let i = 1; i <= daysInMonth; i++) {
@@ -80,25 +83,23 @@ export class UniCalendar {
         for (let i = 0; i <= 5; i++) {
             this.calendarWeeks[i] = days.splice(0, 7);
         }
-
     }
-
 
     private dateSelected(date) {
         this.selectedDate.month(date.month);
         this.selectedDate.date(date.day);
+        this.calendarDate = this.selectedDate.clone();
         this.dateChange.emit(this.selectedDate.toDate());
     }
 
     private prevMonth() {
-        this.selectedDate.subtract(1, 'month');
-        this.generateCalendarPage(this.selectedDate);
+        this.calendarDate.subtract(1, 'month');
+        this.generateCalendarPage(this.calendarDate);
     }
 
     private nextMonth() {
-        this.selectedDate.add(1, 'month');
-        this.generateCalendarPage(this.selectedDate);
+        this.calendarDate.add(1, 'month');
+        this.generateCalendarPage(this.calendarDate);
     }
-
 
 }
--- src/framework/uniform/shared/autocompleteDate.ts
+++ src/framework/uniform/shared/autocompleteDate.ts
@@ -1,65 +1,77 @@
+import moment from 'moment';
 export function autocompleteDate(inputValue: string): Date {
     'use strict';
+    let day, month, year;
+    let date = new Date();
 
-    var input = inputValue.replace(/[^0-9]/g, '');
+    const split = inputValue.split(/\/|\.|-|,/);
 
-    var day, month, year;
-    var date = new Date();
-
-    switch (input.length) {
-        case 0:
-            return date;
-        case 1:
-            day = parseInt(input, 10);
-            month = date.getMonth();
-            year = date.getFullYear();
-            break;
-        case 2:
-            day = parseInt(input, 10);
-            month = date.getMonth();
-            year = date.getFullYear();
-            break;
-        case 3:
-            day = parseInt(input.slice(0, 2), 10);
-            month = parseInt(input[2], 10) - 1;
-            year = date.getFullYear();
-            if (day > new Date(0, month, year).getDate()) {
-                day = parseInt(input[0], 10);
-                month = parseInt(input.slice(1), 10) - 1;
-            }
-            break;
-        case 4:
-            day = parseInt(input.slice(0, 2), 10);
-            month = parseInt(input.slice(2), 10) - 1;
-            year = date.getFullYear();
-            break;
-        case 6:
-            day = parseInt(input.slice(0, 2), 10);
-            month = parseInt(input.slice(2, 4), 10) - 1;
-            year = parseInt(date.getFullYear().toString().substr(0, 2) + input.slice(4), 10);
-            break;
-        case 8:
-            day = parseInt(input.slice(0, 2), 10);
-            month = parseInt(input.slice(2, 4), 10) - 1;
-            year = parseInt(input.slice(4), 10);
-            break;
-        default:
-            return null;
+    if (split.length > 1) {
+        day = split[0];
+        month = +split[1] - 1;
+        year = split[2] || date.getFullYear();
+        if (year.length === 2) {
+            year = date.getFullYear().toString().substr(0, 2) + year;
+        }
+    } else {
+        let input = inputValue.replace(/[^0-9]/g, '');
+        switch (input.length) {
+            case 1:
+            case 2:
+                day = parseInt(input, 10);
+                month = date.getMonth();
+                year = date.getFullYear();
+                break;
+            case 3:
+                day = parseInt(input.slice(0, 2), 10);
+                month = parseInt(input[2], 10) - 1; // - 1 because js month is zero based
+                year = date.getFullYear();
+                if (day > new Date(year, month, 0).getDate()) {
+                    day = parseInt(input[0], 10);
+                    month = parseInt(input.slice(1), 10) - 1;
+                }
+                break;
+            case 4:
+                day = parseInt(input.slice(0, 2), 10);
+                month = parseInt(input.slice(2), 10) - 1;
+                year = date.getFullYear();
+                break;
+            case 6:
+                if (input.indexOf('20') >= 2) {
+                    // input format: DMYYYY
+                    day = parseInt(input[0], 10);
+                    month = parseInt(input[1], 10) - 1;
+                    year = parseInt(input.slice(2));
+                } else {
+                    // input format: DDMMYY
+                    day = parseInt(input.slice(0, 2), 10);
+                    month = parseInt(input.slice(2, 4), 10) - 1;
+                    year = parseInt(date.getFullYear().toString().substr(0, 2) + input.slice(4), 10);
+                }
+                break;
+            case 8:
+                day = parseInt(input.slice(0, 2), 10);
+                month = parseInt(input.slice(2, 4), 10) - 1;
+                year = parseInt(input.slice(4), 10);
+                break;
+            default:
+                return null;
+        }
     }
 
     if (year < 1900) {
         return null;
     }
-    if (month < 0 || month > 12) {
+
+    if (month < 0 || month > 11) {
         return null;
     }
 
-    // Check if day > max day of month
-    // Need to +1 month here to get last day of current month
-    // (js dates ftw)
-    if (day > new Date(year, (month + 1), 0).getDate()) {
+    const autocompleted = new Date(year, month);
+    if (day > moment(autocompleted).daysInMonth()) {
         return null;
     }
 
-    return new Date(year, month, day);
+    autocompleted.setDate(day);
+    return autocompleted;
 }
