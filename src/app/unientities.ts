4bb8ff97e16c5f4a14a522634fd04996c7c42810
Karl Gustav Roksund <KarlGustav@unimicro.no>
2016-12-29 12:48:13 +0100

    fix(LocalDate): Fix LocalDate implementation and update uni-form/table
    Update uniform and unitable because their versions of LocalDate was
    lagging behind.
    
    Update LocalDate because we want to be able to easily create a new
    moment date based on a LocalDate. Now that can be done as easily as
    `moment(new LocalDate('2016-04-04'))`. This functionality already worked
    for date: `new Date(new LocalDate('2016-04-04'))`

--- src/app/unientities.ts
+++ src/app/unientities.ts
@@ -2,23 +2,27 @@
 
 export class LocalDate {
     private value: Date;
+    public year: number; // <-- For moment constructor
+    public month: number; // <-- For moment constructor
+    public day: number; // <-- For moment constructor
     constructor(value: string | Date = new Date()) {
         if (typeof value === 'string') {
             this.value = new Date(value);
         } else if (Object.prototype.toString.call(value) === '[object Date]') {
             this.value = value
         } else {
-            throw new Error(`Can only be initialized with a date or a sting! Was: ${value} (${typeof value})`);
+            throw new Error(`Can only be initialized with a date or a string! Was: ${value} (${typeof value})`);
         }
         if (isNaN(this.value.getTime())) {
             throw new Error(`The date you supplied is not valid: ${value}`);
         }
+        this.year = this.value.getFullYear();
+        this.month = this.value.getMonth();
+        this.day = this.value.getDate();
     }
     public toJSON(): string {
-        const y = this.value.getFullYear();
-        const m = this.value.getMonth() + 1;
-        const d = this.value.getDate();
-        return y + '-' + ('00'+m).slice(-2) + '-' + ('00'+d).slice(-2);
+        const pad = function(x) { return ('00' + x).slice(-2); };
+        return this.year + '-' + pad(this.month + 1) + '-' + pad(this.day);
     }
     public toDate(): Date {
         return new Date(this.value.getTime());
