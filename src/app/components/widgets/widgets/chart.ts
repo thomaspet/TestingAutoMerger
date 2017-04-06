7c8b42d25c29a65c9a707bdda0b892a35920378e
Anders <anders.urrang@unimicro.no>
2017-04-06 15:07:23 +0200

    chore: refactor chart and move labels to class variable

--- src/app/components/widgets/widgets/chart.ts
+++ src/app/components/widgets/widgets/chart.ts
@@ -1,4 +1,4 @@
-﻿import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+﻿import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IUniWidget } from '../uniWidget';
 import { WidgetDatasetBuilder, ChartColorEnum } from '../widgetDatasetBuilder';
 import { WidgetDataService } from '../widgetDataService';
@@ -7,24 +7,27 @@ import * as Chart from 'chart.js';
 
 @Component({
     selector: 'uni-chart',
-    template: ` <figure style="margin: 0; color: white; text-align: center; background-color: #fff; max-height: 100%; height: 100%;">
-                    <div class="uni-dashboard-chart-header"> {{ widget.config.header }}</div>
-                    <div style="padding: 20px;">
-                        <canvas #chartElement> </canvas>
-                    </div>
-                </figure>`
+    template: `
+        <figure style="margin: 0; color: white; text-align: center; background-color: #fff; max-height: 100%; height: 100%;">
+            <div class="uni-dashboard-chart-header"> {{ widget.config.header }}</div>
+            <div style="padding: 20px;">
+                <canvas #chartElement> </canvas>
+            </div>
+        </figure>`
 })
 
 export class UniChartWidget {
     @ViewChild('chartElement')
     private chartElement: ElementRef;
+
     private builder: WidgetDatasetBuilder = new WidgetDatasetBuilder();
     public widget: IUniWidget;
     private myChart: any;
 
+    private labels: string[];
     private datasets: any[] = [];
 
-    constructor(private widgetDataService: WidgetDataService) { }
+    constructor(private widgetDataService: WidgetDataService) {}
 
     public ngAfterViewInit() {
         if (this.widget) {
@@ -43,36 +46,50 @@ export class UniChartWidget {
         if (this.myChart) {
             this.myChart.destroy();
         }
+
         let obs = [];
         this.widget.config.dataEndpoint.forEach((endpoint) => {
-            obs.push(this.widgetDataService.getData(endpoint))
-        })
+            obs.push(this.widgetDataService.getData(endpoint));
+        });
 
-        Observable.forkJoin(
-            obs
-        ).subscribe(
+        Observable.forkJoin(obs).subscribe(
             (data: any) => {
-                //Failcheck in case http call fails.. needed???
+                // Failcheck in case http call fails.. needed???
                 if (data[0].Success) {
                     data.forEach((item: any, i) => {
                         if (this.widget.config.labels.length < 1) {
-                            var data = this.builder.buildWithDynamicLabels(item.Data, this.widget.config.dataKey, this.widget.config.useIf, this.widget.config.maxNumberOfLabels);
-                            this.datasets.push(data.dataset);
-                            this.widget.config.labels = data.labels;
+                            const builderResult = this.builder.buildWithDynamicLabels(
+                                item.Data,
+                                this.widget.config.dataKey,
+                                this.widget.config.useIf,
+                                this.widget.config.maxNumberOfLabels
+                            );
+
+                            this.datasets.push(builderResult.dataset);
+                            this.labels = builderResult.labels;
 
                         } else {
-                            this.datasets.push(this.builder.buildSingleColorDataset(item.Data, ChartColorEnum.White, this.widget.config.colors[i], this.widget.config.title[i], this.widget.config.dataKey[i], this.widget.config.chartType, this.widget.config.multiplyValue | 1));
+                            this.datasets.push(this.builder.buildSingleColorDataset(
+                                item.Data,
+                                ChartColorEnum.White,
+                                this.widget.config.colors[i],
+                                this.widget.config.title[i],
+                                this.widget.config.dataKey[i],
+                                this.widget.config.chartType,
+                                this.widget.config.multiplyValue || 1
+                            ));
+
+                            this.labels = this.widget.config.labels;
                         }
-                    })
+                    });
                 }
                 this.drawChart();
             },
-            err => console.log(err)
-            )
+                err => console.log(err)
+            );
     }
 
     private drawChart() {
-
         let myElement = this.chartElement.nativeElement;
 
         // 64px is 2 * 20px padding in parent + 24px in the header.. Canvas hack! Jørgen Lom fix it..
@@ -83,12 +100,11 @@ export class UniChartWidget {
         this.myChart = new Chart(<any>myElement, {
             type: this.widget.config.chartType,
             data: {
-                labels: this.widget.config.labels,
+                labels: this.labels,
                 datasets: this.datasets
             },
             options: this.widget.config.options
         });
     }
-
 }
 
