176daf9f0fdd339a52bb8cd370120ad2b8b996d6
Sondre Eide Vik <sondre_vik@hotmail.com>
2017-04-07 08:40:20 +0200

    feature(TaxCard): add checkbox for changed employees (#2920)
    because we wan't to be able to choose if we want chosen emps + changed or just chosen emps
    could be valuable when we make the first request in a year
    
    ref #2919

--- src/app/components/salary/employee/modals/taxCardRequest.ts
+++ src/app/components/salary/employee/modals/taxCardRequest.ts
@@ -5,6 +5,12 @@ import {AltinnIntegrationService, ErrorService} from '../../../../../app/service
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 declare const _;
 
+type TaxRequestModel = {
+    singleEmpChoice: number,
+    multiEmpChoice: number,
+    empsAndChanged: boolean
+};
+
 @Component({
     selector: 'tax-card-request',
     templateUrl: './taxCardRequest.html',
@@ -23,10 +29,8 @@ export class TaxCardRequest {
 
     @Output() public newReceipt: EventEmitter<boolean> = new EventEmitter<boolean>(true);
 
-    public model$: BehaviorSubject<{
-        singleEmpChoice: number,
-        multiEmpChoice: number
-    }> = new BehaviorSubject({ singleEmpChoice: 1, multiEmpChoice: 1 });
+    public model$: BehaviorSubject<TaxRequestModel> = new BehaviorSubject<TaxRequestModel>(
+        { singleEmpChoice: 1, multiEmpChoice: 1, empsAndChanged: false });
     public fields$: BehaviorSubject<FieldLayout[]> = new BehaviorSubject([]);
     public formConfig$: BehaviorSubject<any> = new BehaviorSubject({});
 
@@ -55,8 +59,8 @@ export class TaxCardRequest {
         this.title = 'Send forespørsel om skattekort';
         this.exitButton = 'Avbryt';
         this.error = '';
-        this.model$.next({ singleEmpChoice: 1, multiEmpChoice: 1 });
-        var singleChoice: any = {
+        this.model$.next({ singleEmpChoice: 1, multiEmpChoice: 1, empsAndChanged: false });
+        let singleChoice: any = {
             FieldSet: 0,
             Section: 0,
             Combo: 0,
@@ -74,7 +78,7 @@ export class TaxCardRequest {
             },
 
         };
-        var multipleChoice: any = {
+        let multipleChoice: any = {
             FieldSet: 0,
             Section: 0,
             Combo: 0,
@@ -93,31 +97,47 @@ export class TaxCardRequest {
                 valueProperty: 'id'
             }
         };
-        this.fields$.next([singleChoice, multipleChoice]);
+        let empsAndChangedField: any = {
+            FieldSet: 0,
+            Section: 0,
+            Combo: 0,
+            FieldType: FieldType.CHECKBOX,
+            Hidden: false,
+            Property: 'empsAndChanged',
+            Label: 'Be om endrede skattekort',
+            ReadOnly: false,
+            Placeholder: 'Select',
+        };
+        this.fields$.next([empsAndChangedField, singleChoice, multipleChoice]);
         this.busy = false;
     }
 
     public submit() {
-        if (this.model$.getValue().singleEmpChoice === 2) {
-            switch (this.model$.getValue().multiEmpChoice) {
+        let model = this.model$.getValue();
+        let option = '';
+        if (model.singleEmpChoice === 2) {
+            switch (model.multiEmpChoice) {
                 case 1:
-                    this.taxRequest('ALL_EMPS');
+                    option = 'ALL_EMPS';
                     break;
                 case 2:
-                    this.taxRequest('ACTIVE_EMPS');
+                    option = 'ACTIVE_EMPS';
                     break;
                 case 3:
-                    this.taxRequest('CHANGED_ONLY');
+                    option = 'CHANGED_ONLY';
                     break;
             }
         } else {
-            this.taxRequest('SINGLE_EMP', this.employeeID);
+            option = 'SINGLE_EMP';
         }
+
+        this.taxRequest(option, this.employeeID, model.empsAndChanged);
     }
 
-    private taxRequest(option, empId = 0) {
+    private taxRequest(option: string, empId: number = 0, empsAndChanged = false) {
         this.busy = true;
-        this._altinnService.sendTaxRequestAction(option, empId).subscribe((response: AltinnReceipt) => {
+        this._altinnService.sendTaxRequestAction(option, empId, empsAndChanged)
+            .subscribe((response: AltinnReceipt) => {
             if (response.ErrorText) {
                 this.title = 'Feil angående Altinn-forespørsel';
                 if (response.ErrorText === 'An error occurred') {
@@ -150,9 +170,9 @@ export class TaxCardRequest {
         this.uniform.Hidden = false;
         let fields = this.fields$.getValue();
         if (this.model$.getValue().singleEmpChoice === 2) {
-            fields[1].Hidden = false;
+            fields[2].Hidden = false;
         } else {
-            fields[1].Hidden = true;
+            fields[2].Hidden = true;
         }
         this.fields$.next(fields);
     }
