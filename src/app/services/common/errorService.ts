a4998bf9bc3ee87a79577878d677d0c84dcf4a5e
Karl Gustav <kkgg84+git@gmail.com>
2017-05-10 13:55:47 +0200

    feature(Errormessages): Getting error messages that is deep in reply
    Because the logic we have now only gets error messages that is in the
    root of the reply object.
    
    Also sanitize the html in the toasters so that we can use basic html in
    them, most notably; newlines, aka <br />.
    
    closes: #2898 
    code review: #3219

--- src/app/services/common/errorService.ts
+++ src/app/services/common/errorService.ts
@@ -32,30 +32,55 @@ export class ErrorService {
         this.addErrorToast(toastMsg || message);
     }
 
-    public  extractMessage(error: any): string {
-        if (error.message) {
-            return error.message;
-        } else if (error.Message) {
-            return error.Message;
-        } else if (error._body) {
-            let errContent = typeof error._body === 'string' ? JSON.parse(error._body) : error._body;
-            if (errContent && errContent.Messages && errContent.Messages.length > 0) {
-                return errContent.Messages.map(m => m.Message).join('.\n') + '.\n';
-            } else if (errContent && errContent.Message) {
-                return errContent.Message;
-            } else if (errContent._validationResults) {
-                let arrays = Object.keys(errContent._validationResults).map(
-                    key => errContent._validationResults[key]
-                );
-                return arrays.map(validations => validations.map(validation => validation.Message).join('.\n')).join('\n');
+    public extractMessage(err: any): string {
+        const errBody = err.json ? err.json() : err;
+        if (errBody.message) {
+            return errBody.message;
+        } else if (errBody.Message) {
+            return errBody.Message;
+        } else if (errBody.Messages) {
+            if (errBody.Messages.length > 0) {
+                return errBody.Messages.map(m => m.Message).join('<br />');
             } else {
                 return '[Unparsable error occurred, see logs for more info]';
             }
-        } else if (error.statusText) {
-            return error.statusText;
+        } else if (err.status === 400) {
+            return this.extractValidationResults(errBody).join('<br />');
+        } else if (errBody.statusText) {
+            return errBody.statusText;
         } else {
-            return error.toString();
+            return errBody.toString();
         }
+    }
+
+    public extractValidationResults(error: any): string[] {
+        const flatten = (arr, rest) => arr.concat(rest);
+
+        function findValue(object: any) {
+            const validationErrorKey = '_validationResults';
+            const validationErrors: string[] = [];
+            if (object) {
+                Object.keys(object).forEach(key => {
+                    const value = object[key];
+                    if (key === validationErrorKey) {
+                        Object.keys(value)
+                            .map(k => value[k])
+                            .reduce(flatten, [])
+                            .map(x => `${x.Message} on entity ${x.EntityType}`)
+                            .forEach(message => validationErrors.push(message));
+                    } else if (Array.isArray(value)) {
+                        Array.prototype.push
+                            .apply(validationErrors, value.map(v => findValue(v)).reduce(flatten, []));
+                    } else if (typeof value === 'object') {
+                        Array.prototype.push.apply(validationErrors, findValue(value));
+                    }
+                });
+            }
+
+            return validationErrors;
+        }
+
+        return findValue(error);
     }
 
     public addErrorToast(message: string) {
