cc4a21e6a636393a833543761c1a970753e27852
Karl Gustav <kkgg84+git@gmail.com>
2017-06-15 08:00:10 +0200

    feature(Versions): Add backend version and build date to versions view
    Because it was missing and now we have gotten the backend api to extract
    them.
    
    ref: https://unimicro.atlassian.net/browse/UK-27
    code review: #3323

--- src/app/components/about/versions/versionsView.ts
+++ src/app/components/about/versions/versionsView.ts
@@ -1,27 +1,59 @@
-import {Component} from '@angular/core';
+import {Component, ChangeDetectorRef} from '@angular/core';
 import {TabService, UniModules} from '../../layout/navbar/tabstrip/tabService';
 import {IToolbarConfig} from '../../common/toolbar/toolbar';
 import * as moment from 'moment';
+import {UniHttp} from '../../../../framework/core/http/http';
+import {ErrorService} from '../../../services/common/errorService';
 
 declare const APP_VERSION: string;
 declare const APP_BUILD_DATE: string;
+const VERSION_DATE_FORMAT = 'DD.MM.YYYY HH:mm';
 
 @Component({
     selector: 'uni-versions-view',
     templateUrl: './versionsView.html'
 })
 export class UniVersionsView {
-    private fullVersion = APP_VERSION;
-    private minVersion = this.fullVersion.substr(0, 6);
-    private buildDate = moment(APP_BUILD_DATE).format('DD.MM.YYYY HH:mm');
+    private frontend = {
+        fullVersion: APP_VERSION,
+        minVersion: APP_VERSION.substr(0, 6),
+        buildDate: moment(APP_BUILD_DATE).format(VERSION_DATE_FORMAT),
+    };
+    private backend: {
+        fullVersion: string
+        minVersion: string
+        buildDate: string
+    };
     private toolbarConfig: IToolbarConfig = {title: 'Versjoner'};
 
-    constructor(private tabService: TabService) {
+
+    constructor(
+        private tabService: TabService,
+        private http: UniHttp,
+        private errorService: ErrorService,
+        private ref: ChangeDetectorRef
+    ) {
         this.tabService.addTab({
             name: 'Versjoner',
             url: '/about/versions',
             moduleID: UniModules.Versions,
             active: true
         });
+        this.http
+            .usingRootDomain()
+            .asGET()
+            .withEndPoint('status/version')
+            .send({})
+            .map(result => result.json())
+            .map(res => ({
+                fullVersion: res.Version,
+                minVersion: res.Version.substr(0, 6),
+                buildDate: moment(res.Date).format(VERSION_DATE_FORMAT)
+            }))
+            .subscribe(
+                backendVersions => this.backend = backendVersions,
+                err => this.errorService.handle(err)
+            );
+
     }
 }
