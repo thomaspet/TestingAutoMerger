32210e71575b00e18d1bd5f1d3131cae34c1eb8a
Terje Pedersen <terje@senikk.com>
2017-06-21 16:23:51 +0200

    accountingreports result view improvements (#3312)

--- src/app/components/accounting/accountingreports/chartHelper.ts
+++ src/app/components/accounting/accountingreports/chartHelper.ts
@@ -1,8 +1,37 @@
 import * as Chart from 'chart.js';
 export class ChartHelper {
     public static generateChart(elementID: string, data: IChartDataSet) {
-        let element = document.getElementById(elementID);
+        if (data.chartType === 'bar') {
+            this.generateBarChart(elementID, data);
+        } else {
+            this.generateLineChart(elementID, data);
+        }
+    }
 
+    private static generateBarChart(elementID: string, data: IChartDataSet) {
+        let element = document.getElementById(elementID);
+        let chart = Chart.Bar(<any> element, {
+            data: {
+                labels: data.labels,
+                datasets: data.datasets ? data.datasets : [
+                    {
+                        data: data.data,
+                        label: data.label,
+                        borderColor: data.borderColor,
+                        fill: false,
+                        lineTension: 0
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    }
+
+    private static generateLineChart(elementID: string, data: IChartDataSet) {
+        let element = document.getElementById(elementID);
         let chart = Chart.Line(<any> element, {
             data: {
                 labels: data.labels,
