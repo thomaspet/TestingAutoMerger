dddc1ee9cb35056d05c8a77d397ba397a727ceb4
Anders <anders.urrang@unimicro.no>
2017-07-10 15:23:30 +0200

    fix(errorHandler): refactor and fix type errors

--- src/app/UniErrorHandler.ts
+++ src/app/UniErrorHandler.ts
@@ -1,10 +1,9 @@
 import { ErrorHandler, Inject } from '@angular/core';
 import { ErrorService } from './services/services';
 
-
 type WrappedError = {
-    originalError: string
-}
+    originalError: string | WrappedError
+};
 
 declare const Raygun;
 
@@ -20,56 +19,21 @@ export class UniMicroAngularInternalErrorHandlerOverride implements ErrorHandler
             Raygun.send(err);
         }
 
-        const originalError = this.findOriginalError(err);
-        const originalStack = this.findOriginalStack(err);
-        const context = this.findContext(err);
-
         console.error(`EXCEPTION: ${err.message || err.toString()}`);
 
-        if (originalError) {
-            console.error(`ORIGINAL EXCEPTION: ${originalError.message || originalError.toString()}`);
-        }
-
-        if (originalStack) {
-            console.error('ORIGINAL STACKTRACE:');
-            console.error(originalStack);
-        }
-
+        const context = this.findContext(err);
         if (context) {
-            console.error('ERROR CONTEXT:');
-            console.error(context);
+            console.error(`CONTEXT: ${context}`);
         }
+
+        const stack = err.stack || err.originalStack;
+        console.error(`STACK: ${stack}`);
+
 
         this.errorService.addErrorToast(this.errorService.extractMessage(err));
-
         throw err;
     }
 
-    private findOriginalError(error: any): any {
-        let e = (error as WrappedError).originalError;
-        while (e && (e as WrappedError).originalError) {
-            e = (e as WrappedError).originalError;
-        }
-
-        return e;
-    }
-
-    private findOriginalStack(error: any): string {
-        if (!(error instanceof Error)) {
-            return null;
-        }
-
-        let e: any = error;
-        let stack: string = e.stack;
-        while (e instanceof Error && (e as WrappedError).originalError) {
-            e = (e as WrappedError).originalError;
-            if (e instanceof Error && e.stack) {
-                stack = e.stack;
-            }
-        }
-
-        return stack;
-    }
 
     private findContext(error: any): any {
         if (error) {
