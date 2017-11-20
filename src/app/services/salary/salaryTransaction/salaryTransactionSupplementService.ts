31273ee6670f9f8541ef53bfe50a040ab59bb9a2
Sondre Eide Vik <sondre_vik@hotmail.com>
2017-11-20 14:14:05 +0100

    fix(Supplements) fix checkbox supplement (#3878)
    now we we show checkbox as finished if there is a checkbox
    
    UL-275

--- src/app/services/salary/salaryTransaction/salaryTransactionSupplementService.ts
+++ src/app/services/salary/salaryTransaction/salaryTransactionSupplementService.ts
@@ -48,14 +48,18 @@ export class SupplementService extends BizHttp<SalaryTransactionSupplement> {
     public anyUnfinished(supps: SalaryTransactionSupplement[], wtSupps: WageTypeSupplement[] = []) {
         return supps
             .some(supp => {
-                let wtSupplement = wtSupps.find(wtSupp => wtSupp.ID === supp.WageTypeSupplementID) || supp.WageTypeSupplement;
+                let wtSupplement = wtSupps
+                    .find(wtSupp => wtSupp.ID === supp.WageTypeSupplementID) || supp.WageTypeSupplement;
                 return this.isUnfinished(supp, wtSupplement);
             });
     }
 
     public isUnfinished(supp: SalaryTransactionSupplement, wtSupp: WageTypeSupplement) {
-        return ( !wtSupp || wtSupp.ValueType !== Valuetype.IsBool)
-            && !supp.ValueDate
+        if (wtSupp && wtSupp.ValueType === Valuetype.IsBool) {
+            return false;
+        }
+
+        return !supp.ValueDate
             && !supp.ValueDate2
             && !supp.ValueMoney || supp.ValueMoney === 0
             && !supp.ValueString;
