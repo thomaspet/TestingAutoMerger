6f9730e491f7e0255668a20ae4f69cd0518db6a2
dennis <dennis@unimicro.no>
2018-01-16 13:25:26 +0100

    use AccountNumber instead of IBAN as displayValue for supplier

--- src/app/components/accounting/supplier/details/supplierDetails.ts
+++ src/app/components/accounting/supplier/details/supplierDetails.ts
@@ -523,7 +523,7 @@ export class SupplierDetails implements OnInit {
         defaultBankAccount.Options = {
             entity: BankAccount,
             listProperty: 'Info.BankAccounts',
-            displayValue: 'IBAN',
+            displayValue: 'AccountNumber',
             linkProperty: 'ID',
             storeResultInProperty: 'Info.DefaultBankAccount',
             storeIdInProperty: 'Info.DefaultBankAccountID',
