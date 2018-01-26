f9a6023242dd619c10accc8fbdc7efa5560101c3
Krister <krister.torsvik@gmail.com>
2018-01-26 09:33:00 +0100

    fix(confirmModalWithInput): error with import (#4089)
    because import from IUniModal got updated but confirmModal was not
    updated

--- src/framework/uniModal/presets/confirmModalWithInput.ts
+++ src/framework/uniModal/presets/confirmModalWithInput.ts
@@ -1,5 +1,5 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {IUniModal, IModalOptions, ConfirmActions} from '../modalService';
+import {IUniModal, IModalOptions, ConfirmActions} from '@uni-framework/uniModal/interfaces';
 
 @Component({
     selector: 'uni-confirm-modal-with-input',
