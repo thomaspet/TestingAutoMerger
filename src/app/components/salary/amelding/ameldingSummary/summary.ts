261f4723a378d1a2df9a219926e3a0fc5a6966a7
Arild <arild@unimicro.no>
2018-02-27 13:44:53 +0100

    fix(amelding) permisjoner (#4221)
    show leave
    UL-514

--- src/app/components/salary/amelding/ameldingSummary/summary.ts
+++ src/app/components/salary/amelding/ameldingSummary/summary.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {UniTableConfig, UniTableColumnType, UniTableColumn} from '../../../../../framework/ui/unitable/index';
 import * as moment from 'moment';
 
@@ -7,7 +7,7 @@ import * as moment from 'moment';
     templateUrl: './summary.html'
 })
 
-export class AmeldingSummaryView {
+export class AmeldingSummaryView implements OnChanges {
     @Input() public currentSumUp: any;
     @Input() public currentAMelding: any;
     private employeeTableConfig: UniTableConfig;
@@ -40,9 +40,11 @@ export class AmeldingSummaryView {
             }
         }
         if (this.currentAMelding) {
-            this.createdDate = moment(this.currentAMelding.created).format('DD.MM.YYYY HH:mm');
+            this.createdDate = moment(this.currentAMelding.created)
+                .format('DD.MM.YYYY HH:mm');
             if (this.currentAMelding.sent) {
-                this.sentDate = moment(this.currentAMelding.sent).format('DD.MM.YYYY HH:mm');
+                this.sentDate = moment(this.currentAMelding.sent)
+                    .format('DD.MM.YYYY HH:mm');
             }
         }
 
@@ -56,21 +58,21 @@ export class AmeldingSummaryView {
 
     private mapData() {
         this.entitiesWithData = [];
-        let period = this.currentAMelding.period;
+        const period = this.currentAMelding.period;
 
         if (this.currentSumUp.entities) {
             this.currentSumUp.entities.forEach(entity => {
-                let entityName: string = entity.name;
-                let entityOrgNumber: string = entity.orgNumber;
-                let entityExpanded: boolean = entity.expanded;
-                let entitySums: Object = entity.sums;
+                const entityName: string = entity.name;
+                const entityOrgNumber: string = entity.orgNumber;
+                const entityExpanded: boolean = entity.expanded;
+                const entitySums: Object = entity.sums;
 
                 if (entity.employees) {
                     this.employeeAndEmployments = [];
                     this.employeeleaves = [];
                     entity.employees.forEach(employee => {
-                        let employeeName = employee.name;
-                        let employeeNumber = employee.employeeNumber;
+                        const employeeName = employee.name;
+                        const employeeNumber = employee.employeeNumber;
 
                         if (employee.arbeidsforhold) {
                             employee.arbeidsforhold.forEach(arbeidsforhold => {
@@ -82,12 +84,11 @@ export class AmeldingSummaryView {
                                     endDate: arbeidsforhold.endDate
                                 });
 
-                                this.employeeleaves = [];
                                 if (arbeidsforhold.permisjon) {
                                     arbeidsforhold.permisjon.forEach(permisjon => {
-                                        let startdatePeriod = parseInt(permisjon.startdato.split('-', 2)[1]);
-                                        let enddatePeriod = permisjon.sluttdato
-                                            ? parseInt(permisjon.sluttdato.split('-', 2)[1])
+                                        const startdatePeriod = parseInt(permisjon.startdato.split('-', 2)[1], 10);
+                                        const enddatePeriod = permisjon.sluttdato
+                                            ? parseInt(permisjon.sluttdato.split('-', 2)[1], 10)
                                             : undefined;
                                         if (startdatePeriod === period || enddatePeriod === period) {
                                             this.employeeleaves.push({
@@ -127,56 +128,65 @@ export class AmeldingSummaryView {
     }
 
     private setupEmployees() {
-        let empNoCol = new UniTableColumn('employeeNumber', 'Nr', UniTableColumnType.Number).setWidth('4rem');
-        let nameCol = new UniTableColumn('name', 'Navn', UniTableColumnType.Text);
-        let emplmntCol = new UniTableColumn(
-            'arbeidsforholdId', 'ID arbeidsforhold', UniTableColumnType.Number
-        ).setWidth('10rem');
-        let startCol = new UniTableColumn('startDate', 'Startdato', UniTableColumnType.LocalDate).setWidth('8rem');
-        let endCol = new UniTableColumn('endDate', 'Sluttdato', UniTableColumnType.LocalDate).setWidth('8rem');
+        const empNoCol = new UniTableColumn('employeeNumber', 'Nr', UniTableColumnType.Number)
+            .setWidth('4rem');
+        const nameCol = new UniTableColumn('name', 'Navn', UniTableColumnType.Text);
+        const emplmntCol = new UniTableColumn('arbeidsforholdId', 'ID arbeidsforhold', UniTableColumnType.Number)
+            .setWidth('10rem');
+        const startCol = new UniTableColumn('startDate', 'Startdato', UniTableColumnType.LocalDate)
+            .setWidth('8rem');
+        const endCol = new UniTableColumn('endDate', 'Sluttdato', UniTableColumnType.LocalDate)
+            .setWidth('8rem');
 
         this.employeeTableConfig = new UniTableConfig('salary.amelding.summary.employees', false, true, 30)
         .setColumns([empNoCol, nameCol, emplmntCol, startCol, endCol]);
     }
 
     private setupLeaves() {
-        let idCol = new UniTableColumn('permisjonsId', 'Id', UniTableColumnType.Number).setWidth('4rem');
-        let numberCol = new UniTableColumn('employeenumber', 'Nr', UniTableColumnType.Number).setWidth('4rem');
-        let nameCol = new UniTableColumn('employeename', 'Ansattnavn', UniTableColumnType.Text);
-        let startCol = new UniTableColumn('startdato', 'Startdato', UniTableColumnType.LocalDate).setWidth('8rem');
-        let endCol = new UniTableColumn('sluttdato', 'Sluttdato', UniTableColumnType.LocalDate).setWidth('8rem');
-        let pcntCol = new UniTableColumn('permisjonsprosent', 'Prosent', UniTableColumnType.Percent).setWidth('5rem');
-        let descCol = new UniTableColumn('beskrivelse', 'Beskrivelse', UniTableColumnType.Text);
+        const idCol = new UniTableColumn('permisjonsId', 'Id', UniTableColumnType.Number)
+            .setWidth('4rem');
+        const numberCol = new UniTableColumn('employeenumber', 'Nr', UniTableColumnType.Number)
+            .setWidth('4rem');
+        const nameCol = new UniTableColumn('employeename', 'Ansattnavn', UniTableColumnType.Text);
+        const startCol = new UniTableColumn('startdato', 'Startdato', UniTableColumnType.LocalDate)
+            .setWidth('8rem');
+        const endCol = new UniTableColumn('sluttdato', 'Sluttdato', UniTableColumnType.LocalDate)
+            .setWidth('8rem');
+        const pcntCol = new UniTableColumn('permisjonsprosent', 'Prosent', UniTableColumnType.Percent)
+            .setWidth('5rem');
+        const descCol = new UniTableColumn('beskrivelse', 'Beskrivelse', UniTableColumnType.Text);
 
         this.leaveTableConfig = new UniTableConfig('salary.amelding.summary.leaves', false, true, 30)
-        .setColumns([idCol, numberCol, nameCol, startCol, endCol, pcntCol, descCol]);
+            .setColumns([idCol, numberCol, nameCol, startCol, endCol, pcntCol, descCol]);
     }
 
     private setupTransactions() {
-        let typeCol = new UniTableColumn('incomeType', 'Type', UniTableColumnType.Text);
-        let benefitCol = new UniTableColumn('benefit', 'Fordel', UniTableColumnType.Text);
-        let descrCol = new UniTableColumn('description', 'Beskrivelse', UniTableColumnType.Text);
-        let taxCol = new UniTableColumn('Base_EmploymentTax', 'Trekk', UniTableColumnType.Text).setWidth('4rem');
-        taxCol.setTemplate((rowModel) => {
-            if (rowModel.Base_EmploymentTax) {
-                return 'Ja';
-            } else {
-                return 'Nei';
-            }
-        });
-        let agaCol = new UniTableColumn('tax', 'Aga', UniTableColumnType.Text).setWidth('4rem');
-        agaCol.setTemplate((rowModel) => {
-            if (rowModel.tax) {
-                return 'Ja';
-            } else {
-                return 'Nei';
-            }
-        });
-        let amountCol = new UniTableColumn('amount', 'Beløp', UniTableColumnType.Money)
-        .setWidth('6rem')
-        .setCls('column-align-right');
+        const typeCol = new UniTableColumn('incomeType', 'Type', UniTableColumnType.Text);
+        const benefitCol = new UniTableColumn('benefit', 'Fordel', UniTableColumnType.Text);
+        const descrCol = new UniTableColumn('description', 'Beskrivelse', UniTableColumnType.Text);
+        const taxCol = new UniTableColumn('Base_EmploymentTax', 'Trekk', UniTableColumnType.Text)
+            .setWidth('4rem')
+            .setTemplate((rowModel) => {
+                if (rowModel.Base_EmploymentTax) {
+                    return 'Ja';
+                } else {
+                    return 'Nei';
+                }
+            });
+        const agaCol = new UniTableColumn('tax', 'Aga', UniTableColumnType.Text)
+            .setWidth('4rem')
+            .setTemplate((rowModel) => {
+                if (rowModel.tax) {
+                    return 'Ja';
+                } else {
+                    return 'Nei';
+                }
+            });
+        const amountCol = new UniTableColumn('amount', 'Beløp', UniTableColumnType.Money)
+            .setWidth('6rem')
+            .setCls('column-align-right');
 
         this.transactionTableConfig = new UniTableConfig('salary.amelding.summary.transactions', false, true, 30)
-        .setColumns([typeCol, benefitCol, descrCol, taxCol, agaCol, amountCol]);
+            .setColumns([typeCol, benefitCol, descrCol, taxCol, agaCol, amountCol]);
     }
 }
