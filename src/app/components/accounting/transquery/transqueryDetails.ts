ca5c57bc8f5f828b93e7bd3d4a81587dca52ae8c
Anders Urrang <anders.urrang@unimicro.no>
2018-04-27 09:33:46 +0200

    fix(transquery): use JournalEntryNumber with year when routing

--- src/app/components/accounting/transquery/transqueryDetails.ts
+++ src/app/components/accounting/transquery/transqueryDetails.ts
@@ -431,12 +431,12 @@ export class TransqueryDetails implements OnInit {
 
                 this.allowManualSearch = false;
             }
-        } else if (routeParams['JournalEntryNumberNumeric']) {
+        } else if (routeParams['JournalEntryNumber']) {
             filter.push({
-                field: 'JournalEntryNumberNumeric',
+                field: 'JournalEntryNumber',
                 operator: 'eq',
-                value: routeParams['JournalEntryNumberNumeric'],
-                searchValue: routeParams['JournalEntryNumberNumeric'],
+                value: routeParams['JournalEntryNumber'],
+                searchValue: routeParams['JournalEntryNumber'],
                 group: 0,
                 selectConfig: null
             });
@@ -524,12 +524,12 @@ export class TransqueryDetails implements OnInit {
         const columns = [
             new UniTableColumn('JournalEntryNumberNumeric', 'Bnr.', UniTableColumnType.Link)
                 .setTemplate(row => row.JournalEntryLineJournalEntryNumberNumeric || 'null')
-                .setLinkResolver(row => `/accounting/transquery;JournalEntryNumberNumeric=${row.JournalEntryLineJournalEntryNumberNumeric}`)
+                .setLinkResolver(row => `/accounting/transquery;JournalEntryNumber=${row.JournalEntryNumber}`)
                 .setFilterOperator('eq')
                 .setWidth('100px'),
                 new UniTableColumn('JournalEntryNumber', 'Bnr. med år', UniTableColumnType.Link)
                 .setDisplayField('JournalEntryLineJournalEntryNumber')
-                .setLinkResolver(row => `/accounting/transquery;JournalEntryNumberNumeric=${row.JournalEntryLineJournalEntryNumberNumeric}`)
+                .setLinkResolver(row => `/accounting/transquery;JournalEntryNumber=${row.JournalEntryNumber}`)
                 .setFilterOperator('eq')
                 .setVisible(false),
             new UniTableColumn('Account.AccountNumber', 'Kontonr.', UniTableColumnType.Link)
