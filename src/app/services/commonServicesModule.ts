801f6584c68541da4e32356b4af2ffd91b1d8532
Sondre Eide Vik <sondre.vik@unimicro.no>
2018-05-24 11:06:57 +0200

    fix(Travel) add travelservice to common service module

--- src/app/services/commonServicesModule.ts
+++ src/app/services/commonServicesModule.ts
@@ -197,6 +197,7 @@ export class CommonServicesModule {
                 DimensionSettingsService,
                 CustomDimensionService,
                 ApiKeyService,
+                TravelService,
             ]
         };
     }
