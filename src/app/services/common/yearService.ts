0f5fa54ecdf867c75d9de4da1c5e602ea0db1314
Anders Urrang <anders.urrang@unimicro.no>
2018-07-26 10:29:29 +0200

    fix(yearService): add fallback to current year and clean up
    - Add fallback to current year if none is found in localstorage or
      CompanySettings. This fixes an issue where active year would be
      set to "0" on some older companies (seems fixed on new ones)
    - Fix lint errors, indentation, naming and import order

--- src/app/services/common/yearService.ts
+++ src/app/services/common/yearService.ts
@@ -1,20 +1,20 @@
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import {CompanySettings} from '../../unientities';
 import {Injectable} from '@angular/core';
-import {CompanySettingsService} from './companySettingsService';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-import {TabService} from '../../components/layout/navbar/tabstrip/tabService';
 import {Router} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import 'rxjs/add/observable/of';
+
+import {CompanySettings} from '@app/unientities';
+import {CompanySettingsService} from './companySettingsService';
+import {TabService} from '@app/components/layout/navbar/tabstrip/tabService';
 import {BrowserStorageService} from '@uni-framework/core/browserStorageService';
 
 @Injectable()
 export class YearService {
-    private ACTIVE_YEAR: string = 'ActiveYear';
+    private ACTIVE_YEAR_KEY: string = 'ActiveYear';
     private activeYear: number;
 
     public selectedYear$: BehaviorSubject<number>;
-    //private tabService : tabService;
 
     constructor(
         private companySettings: CompanySettingsService,
@@ -24,40 +24,43 @@ export class YearService {
     ) {
             this.selectedYear$ = new BehaviorSubject<number>(this.activeYear);
 
-            this.getActiveYear().subscribe(val => {
-                this.activeYear = val;
+            this.getActiveYear().subscribe(year => {
+                this.activeYear = year;
                 this.selectedYear$.next(this.activeYear);
             });
         }
 
     public setSelectedYear(year: number): void {
         if (year !== this.getSavedYear()) {
-            this.browserStorage.setItem(this.ACTIVE_YEAR, year.toString());
+            this.browserStorage.setItem(this.ACTIVE_YEAR_KEY, year.toString());
             this.selectedYear$.next(year);
         }
     }
 
-    public getActiveYear(): Observable<number>{
-        let val = this.browserStorage.getItem(this.ACTIVE_YEAR);
-        if(val) {
-             return Observable.of(parseInt(val));
+    public getActiveYear(): Observable<number> {
+        const val = this.browserStorage.getItem(this.ACTIVE_YEAR_KEY);
+        if (val) {
+            return Observable.of(parseInt(val, 10));
         }
         return this.GetDefaultYear();
     }
 
     public getSavedYear(): number {
-        return parseInt(this.browserStorage.getItem(this.ACTIVE_YEAR));
+        return parseInt(this.browserStorage.getItem(this.ACTIVE_YEAR_KEY), 10);
 
     }
 
     public clearActiveYear(): void {
-        this.browserStorage.removeItem(this.ACTIVE_YEAR);
+        this.browserStorage.removeItem(this.ACTIVE_YEAR_KEY);
     }
 
 
-     private GetDefaultYear(): Observable<number> {
-         return this.companySettings.Get(1).map((x: CompanySettings) => x.CurrentAccountingYear);
-     }
+    private GetDefaultYear(): Observable<number> {
+        return this.companySettings.Get(1).map((settings: CompanySettings) => {
+            const year = settings.CurrentAccountingYear;
 
-
+            // CompanySettings seems to have '0' as year for some companies..
+            return year && +year > 0 ? year : new Date().getFullYear();
+        });
+    }
 }
