81fc2f80d68977aeef7bb4b04626b03f5998a0a5
Sondre Eide Vik <sondre_vik@hotmail.com>
2018-09-27 08:03:23 +0200

    fix(JournalEntry) use invoiceCol and check on customerInvoiceID (#4730)
    because invoiceCol actually sets CustomerInvoice. Also checks on customerInvoiceID because it is the correct value to decide if this is a payment
    
    UL-789

--- src/app/services/accounting/journalEntryService.ts
+++ src/app/services/accounting/journalEntryService.ts
@@ -240,7 +240,7 @@ export class JournalEntryService extends BizHttp<JournalEntry> {
         });
 
         const jeObs = Observable
-            .of(journalEntryDataNew.filter(x => !x.InvoiceNumber))
+            .of(journalEntryDataNew.filter(x => !x.CustomerInvoiceID))
             .switchMap(jeData => {
                 return existingJournalEntryIDs.length
                     ? Observable.forkJoin(Observable.of(jeData), this.GetAll('filter=ID eq ' + existingJournalEntryIDs.join(' or ID eq ')))
@@ -255,7 +255,7 @@ export class JournalEntryService extends BizHttp<JournalEntry> {
                 : Observable.of([]));
 
         const paymentObs = Observable
-            .of(journalEntryDataNew.filter(x => x.InvoiceNumber))
+            .of(journalEntryDataNew.filter(x => x.CustomerInvoiceID))
             .map(data => this.createInvoicePaymentDataObjects(data))
             .switchMap(data => this.invoiceService.payInvoices(data));
 
