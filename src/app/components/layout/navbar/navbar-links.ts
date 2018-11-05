54b393b72fba0b01636eb28123497012cea49153
Marius Jakobsen <marius@uinmicro.no>
2018-11-05 19:54:36 +0100

    temp(Marketplace): remove integrations from menu temporary

--- src/app/components/layout/navbar/navbar-links.ts
+++ src/app/components/layout/navbar/navbar-links.ts
@@ -113,11 +113,11 @@ export const NAVBAR_LINKS: INavbarLinkSection[] = [
                         url: '/marketplace/modules',
                         moduleID: UniModules.Marketplace,
                     },
-                    {
+/*                     {
                         name: 'Integrasjoner',
                         url: '/marketplace/integrations',
                         moduleID: UniModules.Marketplace,
-                    },
+                    }, */
                 ]
             },
         ]
