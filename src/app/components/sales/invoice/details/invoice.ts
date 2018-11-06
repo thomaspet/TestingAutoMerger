3af6136731a3d32e0b285d0022d34d841cc60fe6
Kjetil Ek <kjetil.ek@appex.no>
2018-11-06 14:02:57 +0100

    feat(accounting): add validation before sending/confirming ehf is sent
    because currently i looks like EHF is sent, but it is only sent to the distribution job server, and might crash for many different reasons
    
    ref CM-76

--- src/app/components/sales/invoice/details/invoice.ts
+++ src/app/components/sales/invoice/details/invoice.ts
@@ -1717,19 +1717,63 @@ export class InvoiceDetails implements OnInit, AfterViewInit {
     }
 
     private sendEHF(doneHandler: (msg: string) => void = null) {
-        this.reportService.distributeWithType(this.invoice.ID, 'Models.Sales.CustomerInvoice', 'EHF' ).subscribe(
-            () => {
-                this.toastService.addToast('EHF sendt', ToastType.good, 3, 'Til ' + this.invoice.Customer.Info.Name);
-                if (doneHandler) {
-                    doneHandler('EHF sendt');
-                    this.invoice.PrintStatus = 300;
-                    this.updateSaveActions();
-                }
-            },
-            (err) => {
-                if (doneHandler) { doneHandler('En feil oppstod ved sending av EHF!'); }
-                this.errorService.handle(err);
-            });
+        let notEnoughInfoToCheckEhf: boolean = false;
+
+        if (this.invoice.Customer.PeppolAddress || this.invoice.Customer.OrgNumber) {
+            const peppoladdress =
+                this.invoice.Customer.PeppolAddress ?
+                    this.invoice.Customer.PeppolAddress :
+                    '9908:' + this.invoice.Customer.OrgNumber;
+
+            if (peppoladdress) {
+                this.ehfService.GetAction(
+                    null, 'is-ehf-receiver', 'peppoladdress=' + peppoladdress + '&entitytype=CustomerInvoice'
+                ).subscribe(enabled => {
+                    if (enabled) {
+                        this.reportService.distributeWithType(this.invoice.ID, 'Models.Sales.CustomerInvoice', 'EHF' ).subscribe(
+                            () => {
+                                this.toastService.addToast(
+                                    'Faktura lagt i kø for EHF-distribusjon',
+                                    ToastType.good,
+                                    ToastTime.medium,
+                                    'Status på sendingen oppdateres løpende under Nøkkeltall \\ Distribusjon');
+
+                                    if (doneHandler) {
+                                        doneHandler('EHF lagt i kø for distribusjon');
+                                        this.invoice.PrintStatus = 300;
+                                        this.updateSaveActions();
+                                    }
+                                },
+                                (err) => {
+                                    if (doneHandler) { doneHandler('En feil oppstod ved sending av EHF!'); }
+                                    this.errorService.handle(err);
+                                });
+                    } else {
+                        this.toastService.addToast(
+                            'Kan ikke sende faktura som EHF',
+                            ToastType.bad,
+                            ToastTime.long,
+                            'Mottakeren er ikke en gyldig EHF-mottaker'
+                        );
+                        doneHandler('Sending feilet');
+                    }
+                }, err => this.errorService.handle(err));
+            } else {
+                notEnoughInfoToCheckEhf = true;
+            }
+        } else {
+            notEnoughInfoToCheckEhf = true;
+        }
+
+        if (notEnoughInfoToCheckEhf) {
+            this.toastService.addToast(
+                'Kan ikke sende faktura som EHF',
+                ToastType.bad,
+                ToastTime.long,
+                'Ugylig Peppol adresse eller organisasjonsnr angitt på kunden'
+            );
+            doneHandler('Sending feilet');
+        }
     }
 
     private askSendEHF(doneHandler: (msg: string) => void = null) {
