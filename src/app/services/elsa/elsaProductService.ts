5e112cb4bc72fb32e3ae8f4ebe5baed71951197b
Anders Urrang <anders.urrang@unimicro.no>
2018-11-09 12:14:04 +0100

    feat(elsaProductService): cache results
    Because caching is good, and not caching is bad

--- src/app/services/elsa/elsaProductService.ts
+++ src/app/services/elsa/elsaProductService.ts
@@ -1,44 +1,54 @@
 import {Injectable} from '@angular/core';
-import {UniHttp} from '../../../framework/core/http/http';
+import {Response} from '@angular/http';
+import {UniHttp} from '@uni-framework/core/http/http';
 import {Observable} from 'rxjs/Observable';
 import {ElsaProduct, ElsaPurchase} from '@app/services/elsa/elsaModels';
 
-
 @Injectable()
 export class ElsaProductService {
+    private cache: {[endpoint: string]: Observable<Response>} = {};
+
     constructor(private uniHttp: UniHttp) {}
 
+    public invalidateCache() {
+        this.cache = {};
+    }
+
+    private requestData(endpoint: string) {
+        let request = this.cache[endpoint];
+        if (!request) {
+            request = this.uniHttp
+                .asGET()
+                .usingElsaDomain()
+                .withEndPoint(endpoint)
+                .send()
+                .publishReplay(1)
+                .refCount();
+
+            this.cache[endpoint] = request;
+        }
+
+        return request.map(res => res.json());
+    }
+
     public Get(id: number): Observable<ElsaProduct> {
-        return this.uniHttp
-            .asGET()
-            .usingElsaDomain()
-            .withEndPoint(`/api/products/${id}`)
-            .send()
-            .map(req => req.json());
+        return this.requestData(`/api/products/${id}`);
     }
 
     public GetAll(): Observable<ElsaProduct[]> {
-        return this.uniHttp
-            .asGET()
-            .usingElsaDomain()
-            .withEndPoint('/api/products')
-            .send()
-            .map(req => req.json());
+        return this.requestData('/api/products');
     }
 
     public FindProductByName(name: string): Observable<ElsaProduct> {
-        return this.uniHttp
-            .asGET()
-            .usingElsaDomain()
-            .withEndPoint('/api/products')
-            .send()
-            .map(res => res && res.json())
-            .map(products => {
-                return products.find(product => product.name === name);
-            });
+        return this.requestData('/api/products').map(products => {
+            return products.find(product => product.name === name);
+        });
     }
 
     public PurchaseProductOnCurrentCompany(product: ElsaProduct): Observable<ElsaPurchase> {
+        // Shouldnt't need to invalidateCache here, since we're not actually
+        // changing anything on products, only adding a purchase.
+        // (bad practice to do it here, but this should be temporary until elsa is refactored..)
         return this.uniHttp
             .asPOST()
             .usingElsaDomain()
@@ -48,7 +58,7 @@ export class ElsaProductService {
     }
 
     public ProductTypeToPriceText(product: ElsaProduct): string {
-        let text = [''];
+        const text = [''];
         if (product.isPerUser) {
             text.push('per bruker');
         }
