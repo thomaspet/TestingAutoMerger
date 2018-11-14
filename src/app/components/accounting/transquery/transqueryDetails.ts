fb8c8d31d1789571deab006f2e3232e774e4c282
Jorge <jorge@unimicro.no>
2018-11-14 09:13:40 +0100

    fix(transquery): set properly type
    typescript throws an error because there was a string used as number

--- src/app/components/accounting/transquery/transqueryDetails.ts
+++ src/app/components/accounting/transquery/transqueryDetails.ts
@@ -203,9 +203,9 @@ export class TransqueryDetails implements OnInit {
                 if (searchParams.Account && !isNaN(searchParams.Account)) {
                     formFilters.push(`Account.AccountNumber eq ${searchParams.Account}`);
                 }
-                let amount = searchParams.Amount;
+                let amount = searchParams.Amount.toString();
                 amount = amount.replace(',', '.');
-                if (amount && !isNaN(amount)) {
+                if (amount && !isNaN(+amount)) {
                     formFilters.push(`Amount eq ${amount}`);
                 }
 
