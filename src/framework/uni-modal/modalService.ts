1665f50134071dcf3556be0e6f4e4fe09a855a84
Anders Urrang <anders.urrang@unimicro.no>
2018-12-14 16:23:01 +0100

    fix(modalService): fix style issue with very large dialogs

--- src/framework/uni-modal/modalService.ts
+++ src/framework/uni-modal/modalService.ts
@@ -153,7 +153,6 @@ export class UniModalService {
             }
         }
 
-        componentRootNode.style.margin = '0 auto';
         const backdrop = this.createBackdrop(options);
         backdrop.appendChild(componentRootNode);
 
