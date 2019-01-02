f881b79e9fec95d63c561d8b0cee8b86084a712c
Anders Urrang <anders.urrang@unimicro.no>
2019-01-02 15:02:41 +0100

    chore: slight performance improvement to clickOutside
    The previous solution with subscriptions that were unsubscribed
    on destroy didnt seem sufficient. From what I can tell uniform
    recreates some stuff multiple times during initializiation
    which caused the cleanup to "miss" in some instances. This meant
    we still got "dead" event listeners that slowed down the application
    over time.
    
    Rewrite to use HostListener which is automatically cleaned up by
    angular instead.

--- src/framework/click-outside/click-outside.ts
+++ src/framework/click-outside/click-outside.ts
@@ -1,5 +1,4 @@
-import {Directive, ElementRef, Output, EventEmitter} from '@angular/core';
-import {fromEvent, Subscription} from 'rxjs';
+import {Directive, ElementRef, Output, EventEmitter, HostListener} from '@angular/core';
 
 @Directive({
     selector: '[clickOutside]'
@@ -7,42 +6,29 @@ import {fromEvent, Subscription} from 'rxjs';
 export class ClickOutsideDirective {
     @Output() clickOutside: EventEmitter<any> = new EventEmitter();
 
-    eventSubscription: Subscription;
+    hostViewInitialized: boolean;
 
-    constructor (private elementRef: ElementRef) {}
+    constructor(private elementRef: ElementRef) {}
 
     ngAfterViewInit() {
-        // Timeout to alow any click that opened the host component
-        // to propagate before setting up.
-        // Because we dont want that click to trigger close as well
         setTimeout(() => {
-            this.eventSubscription = fromEvent(document, 'click').subscribe(
-                (event: MouseEvent) => this.onClick(event)
-            );
+            this.hostViewInitialized = true;
         });
     }
 
-    ngOnDestroy() {
-        if (this.eventSubscription && this.eventSubscription.unsubscribe) {
-            this.eventSubscription.unsubscribe();
+    @HostListener('document:click', ['$event.target'])
+    public onClick(targetElement) {
+        if (this.hostViewInitialized && targetElement) {
+            try {
+                // Avoid closing on angular material/cdk overlays
+                if (targetElement.classList.contains('cdk-overlay-backdrop')) {
+                    return;
+                }
+
+                if (!this.elementRef.nativeElement.contains(targetElement)) {
+                    this.clickOutside.emit(null);
+                }
+            } catch (e) {}
         }
-    }
-
-    public onClick(event: MouseEvent) {
-        const targetElement = <HTMLElement> event.target;
-        if (!targetElement) {
-            return;
-        }
-
-        try {
-            // Avoid closing on angular material/cdk overlays
-            if (targetElement.classList.contains('cdk-overlay-backdrop')) {
-                return;
-            }
-
-            if (!this.elementRef.nativeElement.contains(targetElement)) {
-                this.clickOutside.emit(null);
-            }
-        } catch (e) {}
     }
 }
