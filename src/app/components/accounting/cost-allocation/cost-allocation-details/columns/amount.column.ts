f7f061d6770fced4a9557dbdff13ad8e8336f750
Jorge Ferrando <jorge@unimicro.no>
2019-01-24 16:14:24 +0100

    add percent column

--- src/app/components/accounting/cost-allocation/cost-allocation-details/columns/amount.column.ts
+++ src/app/components/accounting/cost-allocation/cost-allocation-details/columns/amount.column.ts
@@ -1,5 +1,7 @@
 import { UniTableColumn, UniTableColumnType } from '@uni-framework/ui/unitable';
 
 export const ammountColumn = () => {
-    return new UniTableColumn('Amount', 'Beløp', UniTableColumnType.Percent).setWidth('90px');
+    return new UniTableColumn('Amount', 'Beløp', UniTableColumnType.Money)
+        .setVisible(false)
+        .setWidth('90px');
 };
