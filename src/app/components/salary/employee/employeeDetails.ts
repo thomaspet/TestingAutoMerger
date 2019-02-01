85cad21c77ba52f8c4a88feb87743db9402294db
MODALEN\Arild <arild@unimicro.no>
2019-02-01 09:47:43 +0100

    fix(employeeleave) nullguard employment on permision

--- src/app/components/salary/employee/employeeDetails.ts
+++ src/app/components/salary/employee/employeeDetails.ts
@@ -1390,7 +1390,7 @@ export class EmployeeDetails extends UniView implements OnDestroy {
                         if (!leave['_isEmpty'] && (leave['_isDirty'] || leave.Deleted)) {
                             changeCount++;
 
-                            if (!leave.Employment.DimensionsID && leave.Employment.Dimensions) {
+                            if (leave.Employment && !leave.Employment.DimensionsID && leave.Employment.Dimensions) {
                                 leave.Employment.Dimensions['_createguid'] = this.employmentService.getNewGuid();
                             }
 
