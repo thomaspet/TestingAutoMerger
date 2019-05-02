46a726ae438fffa81f2b1e1b0b990ed164efe02d
Anders Urrang <anders@urrang.no>
2019-05-02 16:03:35 +0200

    feat(roles): add pipe for translating role names
    SW-314

---
+++ src/app/components/settings/users/role-translation-pipe.ts
@@ -0,0 +1,38 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+const ROLE_TRANSLATIONS = {
+    'Accounting.Admin': 'Full tilgang',
+    'Accounting.Journal': 'Bilagsføring',
+    'Accounting.Reporting': 'Rapportering',
+    'Accounting.Approval': 'Fakturagodkjenning',
+    'Approval.Accounting': 'Forenklet fakturagodkjenning',
+
+    'Sales.Admin': 'Full tilgang',
+    'Sales.Manager': 'Salgsledelse',
+    'Sales.Invoice': 'Faktura',
+    'Sales.Order': 'Ordre',
+    'Sales.Quote': 'Tilbud',
+    'Sales.Reporting': 'Rapportering',
+
+    'Payroll.Admin': 'Full tilgang',
+    'Payroll.Travel': 'Reiseregning',
+    'Payroll.HR': 'Personalannsvar - lønn',
+    'Payroll.Payroll': 'Personalannsvar - lønn',
+
+    'Timetracking.Admin': 'Full tilgang',
+    'Timetracking.HR': 'Personalansvar - timer',
+    'Timetracking.Manager': 'Timegodkjenning',
+    'Timetracking.Reporting': 'Rapportering',
+    'Timetracking.Worker': 'Timeregistrering',
+};
+
+@Pipe({
+    name: 'translateRole',
+    pure: true
+})
+export class RoleTranslationPipe implements PipeTransform {
+
+    transform(roleName: string): string {
+        return ROLE_TRANSLATIONS[roleName] || roleName;
+    }
+}
