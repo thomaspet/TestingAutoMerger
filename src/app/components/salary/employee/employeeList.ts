1a9d470afaadead17fc5f12ff5fd1ecd67324a3f
Jørgen Kristiansen <j.kristiansen19@gmail.com>
2019-05-09 11:15:22 +0200

    fix(EmployeeList): Update to ticker
    Updated list to ticker to get infinite scroll and same design as other lists + fix bug where fileupload that starts job locks upload button
    
    ref: CM-530 CM-531

--- src/app/components/salary/employee/employeeList.ts
+++ src/app/components/salary/employee/employeeList.ts
@@ -1,10 +1,6 @@
 import {Component} from '@angular/core';
 import {Router} from '@angular/router';
-import {UniTableConfig, UniTableColumnType, UniTableColumn} from '../../../../framework/ui/unitable/index';
-import {Observable} from 'rxjs';
-import {Employee} from '../../../unientities';
 import {TabService, UniModules} from '../../layout/navbar/tabstrip/tabService';
-import {EmployeeService, ErrorService} from '../../../services/services';
 
 @Component({
     selector: 'employee-list',
@@ -12,10 +8,6 @@ import {EmployeeService, ErrorService} from '../../../services/services';
 })
 
 export class EmployeeList {
-    public employeeTableConfig: UniTableConfig;
-    public employees$: Observable<Employee>;
-    public busy: boolean;
-
     public toolbarActions = [{
         label: 'Ny ansatt',
         action: this.newEmployee.bind(this),
@@ -25,55 +17,16 @@ export class EmployeeList {
 
     constructor(
         private router: Router,
-        private tabService: TabService,
-        private _employeeService: EmployeeService,
-        private errorService: ErrorService
+        private tabService: TabService
     ) {
-        this.busy = true;
-        this.employees$ =
-            _employeeService
-                .GetAll(
-                    'orderby=EmployeeNumber ASC',
-                    ['BusinessRelationInfo.DefaultEmail',
-                    'SubEntity.BusinessRelationInfo']
-                )
-                .finally(() => this.busy = false)
-                .catch((err, obs) => this.errorService.handleRxCatch(err, obs));
-
-        const idCol = new UniTableColumn('EmployeeNumber', 'Nr').setWidth(150);
-
-        const nameCol = new UniTableColumn('BusinessRelationInfo.Name', 'Navn', UniTableColumnType.Text)
-            .setWidth(300);
-
-        const emailCol = new UniTableColumn('BusinessRelationInfo.DefaultEmail.EmailAddress', 'E-post', UniTableColumnType.Link)
-            .setLinkResolver(employee => {
-                if (!employee.BusinessRelationInfo
-                    || !employee.BusinessRelationInfo.DefaultEmail
-                    || !employee.BusinessRelationInfo.DefaultEmail.EmailAddress
-                ) {
-                    return '';
-                }
-
-                return `mailto:${employee.BusinessRelationInfo.DefaultEmail.EmailAddress}`;
-            });
-
-        const birthDateCol = new UniTableColumn('BirthDate', 'Fødselsdato', UniTableColumnType.LocalDate);
-
-        const subEntityCol = new UniTableColumn(
-            'SubEntity.BusinessRelationInfo.Name', 'Virksomhet', UniTableColumnType.Text
-        ).setWidth(350);
-
-        this.employeeTableConfig = new UniTableConfig('salary.employee.employeeList', false)
-            .setColumns([idCol, nameCol, emailCol, birthDateCol, subEntityCol])
-            .setSearchable(true);
-
         this.tabService.addTab(
-            { name: 'Ansatte', url: '/salary/employees', moduleID: UniModules.Employees, active: true }
+            {
+                name: 'Ansatte',
+                url: '/salary/employees',
+                moduleID: UniModules.Employees,
+                active: true
+            }
         );
-    }
-
-    public employeeSelected(employee: Employee) {
-        this.router.navigate(['/salary/employees/', employee.ID]);
     }
 
     public newEmployee() {
