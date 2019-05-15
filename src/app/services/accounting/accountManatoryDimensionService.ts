ae1f1ac20d762137c8486533aa1514767bbe86fa
SisselUM <47241643+SisselUM@users.noreply.github.com>
2019-05-15 14:12:38 +0200

    Skulle vært med i forrige commit

--- src/app/services/accounting/accountManatoryDimensionService.ts
+++ src/app/services/accounting/accountManatoryDimensionService.ts
@@ -1,5 +1,5 @@
 import { BizHttp } from "@uni-framework/core/http/BizHttp";
-import { AccountManatoryDimension, CustomerInvoiceItem } from "@uni-entities";
+import { AccountManatoryDimension, CustomerInvoiceItem, Dimensions } from "@uni-entities";
 import { UniHttp } from "@uni-framework/core/http/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
@@ -23,6 +23,11 @@ export class AccountManatoryDimensionService extends BizHttp<AccountManatoryDime
             params.push(ad);
         });
         return super.ActionWithBody(null, params, `get-manatory-dimensions-reports`, RequestMethod.Put);
+    }
+
+    public getMandatoryDimensionsReportByDimension(accountID: number, dimensions: Dimensions): Observable<any> {
+        dimensions.ID = 0;
+        return super.ActionWithBody(null, dimensions, `get-manatory-dimensions-report-by-dimensions&accountID=${accountID}`, RequestMethod.Put);
     }
 
     public getMandatoryDimensionsReport(accountID: number, dimensionsID: number): Observable<any> {
