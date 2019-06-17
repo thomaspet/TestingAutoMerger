ed645da1b3373cce0798eb399870bd197c557e75
Krister <krister.torsvik@gmail.com>
2019-06-17 10:32:20 +0200

    Fix/kt/revert wagetype (#5411)
    * Revert "fixed update after sync, letting users see year of duplicate wagetypes until fix is implemented"
    
    This reverts commit 214b56340892876af6da43c968b438e6a8e802c2.
    
    * Revert "feature(wageTypeList): infinite scroll MIN-348"
    
    This reverts commit 3d402db63db23d581e7609d3c688b9308d946973.

--- src/app/components/salary/wagetype/wagetypeList.ts
+++ src/app/components/salary/wagetype/wagetypeList.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {URLSearchParams} from '@angular/http';
 
 import {UniTableConfig, UniTableColumnType, UniTableColumn} from '../../../../framework/ui/unitable/index';
-import {WageTypeService, ErrorService, StatisticsService} from '../../../services/services';
+import {WageTypeService, ErrorService} from '../../../services/services';
+import {Observable} from 'rxjs';
 
 import {WageType} from '../../../unientities';
 import {TabService, UniModules} from '../../layout/navbar/tabstrip/tabService';
@@ -14,37 +14,30 @@ import {ToastService, ToastType} from '../../../../framework/uniToast/toastServi
     selector: 'wagetypes',
     templateUrl: './wagetypeList.html'
 })
-export class WagetypeList {
+export class WagetypeList implements OnInit {
+
+    public tableConfig: UniTableConfig;
+    public wageTypes$: Observable<WageType>;
     public contextMenuItems: IContextMenuItem[] = [];
     public busy: boolean;
-    wagetypeTableConfig: UniTableConfig;
-    lookupFunction: (urlParams: URLSearchParams) => any;
 
-    public toolbarActions = [
-        {
-            label: 'Ny lønnsart',
-            action: this.createWageType.bind(this),
-            main: true,
-            disabled: false
-        }
-    ];
+    public toolbarActions = [{
+        label: 'Ny lønnsart',
+        action: this.createWageType.bind(this),
+        main: true,
+        disabled: false
+    }];
 
     constructor(
         private _router: Router,
         private tabSer: TabService,
         private _wageTypeService: WageTypeService,
-        private statisticsService: StatisticsService,
         private errorService: ErrorService,
         private _toastService: ToastService
     ) {
-        this._wageTypeService.invalidateCache();
-
-        this.tabSer.addTab({
-            name: 'Lønnsarter',
-            url: 'salary/wagetypes',
-            moduleID: UniModules.Wagetypes,
-            active: true
-        });
+        this.tabSer.addTab(
+            { name: 'Lønnsarter', url: 'salary/wagetypes', moduleID: UniModules.Wagetypes, active: true }
+        );
 
         this.contextMenuItems = [
             {
@@ -55,22 +48,22 @@ export class WagetypeList {
                 disabled: () => false
             }
         ];
-        this.createTableConfig();
     }
 
     public onCustomClick() {
         this.syncWagetypes();
     }
 
-    public createTableConfig() {
-        this.setLookupFunction();
+    public ngOnInit() {
+        this._wageTypeService.invalidateCache();
+        this.busy = true;
+        this.getWagetypes();
 
         const idCol = new UniTableColumn('WageTypeNumber', 'Nr', UniTableColumnType.Number)
             .setWidth('3rem')
             .setAlignment('center');
 
         const nameCol = new UniTableColumn('WageTypeName', 'Navn', UniTableColumnType.Text);
-
         const accountNumberCol = new UniTableColumn('AccountNumber', 'Hovedbokskonto', UniTableColumnType.Text)
             .setWidth('10rem')
             .setAlignment('right');
@@ -78,77 +71,41 @@ export class WagetypeList {
         const rateCol = new UniTableColumn('Rate', 'Sats', UniTableColumnType.Money);
 
         const basePaymentCol = new UniTableColumn('Base_Payment', 'Utbetales')
-            .setTemplate(wageType => (wageType.Base_Payment ? 'Ja' : 'Nei'));
+            .setTemplate(wageType =>  wageType.Base_Payment ? 'Ja' : 'Nei'
+            );
 
-        const yearCol = new UniTableColumn('ValidYear', 'År', UniTableColumnType.Number);
+        let pageSize = window.innerHeight - 350;
+        pageSize = pageSize <= 33 ? 10 : Math.floor(pageSize / 34); // 34 = heigth of a single row
 
-        this.wagetypeTableConfig = new UniTableConfig('salary.wagetype.list', false)
-            .setColumns([
-                idCol,
-                nameCol,
-                accountNumberCol,
-                rateCol,
-                basePaymentCol,
-                yearCol,
-            ])
+        this.tableConfig = new UniTableConfig('salary.wagetype.list', false, true, pageSize)
+            .setColumns([idCol, nameCol, accountNumberCol, rateCol, basePaymentCol])
             .setSearchable(true);
     }
 
-    public wagetypeSelected(wagetype: WageType) {
-        this._router.navigate(['/salary/wagetypes/', wagetype.ID]);
+    private getWagetypes() {
+        this.wageTypes$ = this._wageTypeService
+            .GetAll('orderBy=WageTypeNumber ASC')
+            .finally(() => { this.busy = false; })
+            .catch((err, obs) => this.errorService.handleRxCatch(err, obs));
+    }
+
+    public rowSelected(event) {
+        this._router.navigateByUrl('/salary/wagetypes/' + event.ID);
     }
 
     public createWageType() {
         this._router.navigateByUrl('/salary/wagetypes/0');
     }
 
-    setLookupFunction = () => {
-        this.lookupFunction = (urlParams: URLSearchParams) => {
-            const params = urlParams || new URLSearchParams();
-
-            // Use split to get filter value and the custom create filter string with Number and Name fields
-            const filterValue = urlParams.get('filter');
-            const filterSplit = filterValue
-                ? filterValue.split(`'`)
-                : filterValue;
-            let filterString = '';
-            if (filterSplit && filterSplit.length) {
-                filterString = `contains(WageTypeNumber,'${
-                    filterSplit[1]
-                }') or contains(WageTypeName,'${filterSplit[1]}')`;
-            }
-
-            params.set('model', 'WageType');
-            params.set(
-                'select',
-                'ID as ID,WageTypeNumber as WageTypeNumber,WageTypeName as WageTypeName,AccountNumber as AccountNumber' +
-                    ',Rate as Rate,Base_Payment as Base_Payment,ValidYear as ValidYear'
-            );
-            params.set('filter', filterString);
-            params.set('orderby', 'WageTypeNumber ASC,ValidYear DESC');
-
-            return this.statisticsService
-                .GetAllByUrlSearchParams(params).do(() => this.busy = false)
-                .catch((err, obs) => this.errorService.handleRxCatch(err, obs));
-        };
-    }
-
     public syncWagetypes() {
         this.busy = true;
         this._wageTypeService.invalidateCache();
-        this._wageTypeService
-            .syncWagetypes()
-            .do(() => this.setLookupFunction())
-            .finally(() => (this.busy = false))
-            .subscribe(
-                response => {
-                    this._toastService.addToast(
-                        'Lønnsarter synkronisert',
-                        ToastType.good,
-                        4
-                    );
-                },
-                err => this.errorService.handle(err)
-            );
+        this._wageTypeService.syncWagetypes()
+            .do(() => this.getWagetypes())
+            .finally(() => this.busy = false)
+            .subscribe((response) => {
+                this._toastService.addToast('Lønnsarter synkronisert', ToastType.good, 4);
+            }
+        , err => this.errorService.handle(err));
     }
 }
