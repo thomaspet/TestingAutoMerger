2701d5f7a11ad2b825aa13b8689e69a358dea185
Anders Urrang <anders.urrang@unimicro.no>
2019-09-20 14:47:23 +0200

    chore: remove test code from aboutModule

--- src/app/components/about/aboutModule.ts
+++ src/app/components/about/aboutModule.ts
@@ -2,13 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AppCommonModule} from '../common/appCommonModule';
 import {UniVersionsView} from './versions/versionsView';
-import {UniFrameworkModule} from '@uni-framework/frameworkModule';
 
 @NgModule({
     imports: [
         CommonModule,
         AppCommonModule,
-        UniFrameworkModule // remove me! just used for testing
     ],
     declarations: [
         UniVersionsView
