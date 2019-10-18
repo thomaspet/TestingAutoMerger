616539d4322d63ab25bb796e96b1442f11a27d0f
Ishak <46317157+ahamed-ishak@users.noreply.github.com>
2019-10-18 15:26:13 +0530

    fix(Aprila): check AprilaOrderStatus only for aprila customers (#5756)

--- src/app/components/sales/invoice/details/invoice.ts
+++ src/app/components/sales/invoice/details/invoice.ts
@@ -1794,7 +1794,8 @@ export class InvoiceDetails implements OnInit, AfterViewInit {
                             this.customerInvoiceService.Transition(invoice.ID, null, 'invoice').subscribe(
                                 (res) => {
                                     this.selectConfig = undefined;
-                                    if (isCreditNote && res['CustomValues'].AprilaOrderStatus) {
+                                    if (isCreditNote && this.aprilaOption.hasPermission && res['CustomValues'] &&
+                                     res['CustomValues'].AprilaOrderStatus) {
                                         this.openAprilaCreditNoteModal(res['CustomValues'].AprilaOrderStatus);
                                     }
                                 },
