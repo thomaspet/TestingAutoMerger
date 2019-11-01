2284777aa92ccc55796b0ad227b6a2085bc1e194
Sondre Østebøvik <50408839+UniSondre@users.noreply.github.com>
2019-11-01 14:37:26 +0100

    remove unused style import

--- src/app/components/common/boostChat/boostChat.ts
+++ src/app/components/common/boostChat/boostChat.ts
@@ -11,7 +11,6 @@ type ExtendedWindow = typeof window & {
         <img style="box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.2); border-radius: 50%" [src]="chatbotIcon">
     </span>
     `,
-    styleUrls: ['./boostChat.sass']
 })
 export class BoostChat {
     private chatPanel: any = null;
