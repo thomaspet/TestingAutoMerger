8cc99b96c85abf6624efe508d03801a1b40e5af1
Jørgen Kristiansen <j.kristiansen19@gmail.com>
2019-11-04 13:19:28 +0100

    update(Dashboard): New dashboard config
    Rearranging widgets again after message from Jill

--- src/themes/ext01/dashboard-config.ts
+++ src/themes/ext01/dashboard-config.ts
@@ -2,18 +2,18 @@
 export const DASHBOARD_CONFIG = {
     large: [
         {x: 0, y: 0, widgetID: 'bank_balance'},
-        {x: 4, y: 0, widgetID: 'operatingprofit_line' },
-        {x: 0, y: 3, widgetID: 'overdue_invoices'},
+        {x: 4, y: 0, widgetID: 'public_duedates'},
+        {x: 0, y: 3, widgetID: 'operatingprofit_line' },
         {x: 8, y: 3, widgetID: 'reminder_list'},
         {x: 0, y: 6, widgetID: 'unpaid_supplierinvoice_sr'},
-        {x: 4, y: 6, widgetID: 'public_duedates'},
+        {x: 4, y: 6, widgetID: 'overdue_invoices'}
     ],
     medium: [
         {x: 0, y: 0, widgetID: 'bank_balance'},
-        {x: 4, y: 0, widgetID: 'operatingprofit_line' },
-        {x: 0, y: 3, widgetID: 'overdue_invoices'},
-        {x: 7, y: 3, widgetID: 'reminder_list'},
+        {x: 4, y: 0, widgetID: 'public_duedates'},
+        {x: 0, y: 3, widgetID: 'operatingprofit_line' },
+        {x: 8, y: 3, widgetID: 'reminder_list'},
         {x: 0, y: 6, widgetID: 'unpaid_supplierinvoice_sr'},
-        {x: 4, y: 6, widgetID: 'public_duedates'},
+        {x: 4, y: 6, widgetID: 'overdue_invoices'}
     ]
 };
