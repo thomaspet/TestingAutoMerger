ef5f9821c749eaa0377b2f58d53b414158429a41
Anders Urrang <anders.urrang@unimicro.no>
2020-02-07 09:58:08 +0100

    fix(auth): avoid canDeactivateGuard stopping logout redirect
    PB-1033

--- src/app/canDeactivateGuard.ts
+++ src/app/canDeactivateGuard.ts
@@ -1,8 +1,11 @@
 import {CanDeactivate} from '@angular/router';
 
 export class CanDeactivateGuard implements CanDeactivate<any> {
-
     public canDeactivate(component, currentRoute, currentState, nextState) {
+        if (nextState.url.includes('/login')) {
+            return true;
+        }
+
         if (component.canDeactivate) {
             const routeToActivate = nextState && nextState.url;
             return component.canDeactivate(routeToActivate);
