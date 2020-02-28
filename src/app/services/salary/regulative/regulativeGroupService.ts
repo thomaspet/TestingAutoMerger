6b7b130339b0a12b56c3253bb7efad152a982ec0
Sondre Eide Vik <sondre.vik@unimicro.no>
2020-02-28 11:29:39 +0100

    fix(Regulative) don't check with user (#6072)
    because this product is really just per company, and doesn't really have user reference.
    
    MIN-579

--- src/app/services/salary/regulative/regulativeGroupService.ts
+++ src/app/services/salary/regulative/regulativeGroupService.ts
@@ -2,16 +2,14 @@ import { BizHttp, UniHttp } from '@uni-framework/core/http';
 import { RegulativeGroup } from '@uni-entities';
 import { Injectable } from '@angular/core';
 import { ElsaPurchaseService } from '@app/services/elsa/elsaPurchasesService';
-import { switchMap, map } from 'rxjs/operators';
-import { of, forkJoin } from 'rxjs';
-import { UserService } from '@app/services/common/userService';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class RegulativeGroupService extends BizHttp<RegulativeGroup> {
     constructor(
         protected http: UniHttp,
         private elsaPurchaseService: ElsaPurchaseService,
-        private userService: UserService,
     ) {
         super(http);
         this.relativeURL = RegulativeGroup.RelativeUrl;
@@ -19,12 +17,9 @@ export class RegulativeGroupService extends BizHttp<RegulativeGroup> {
     }
 
     getAll(query: string, expands: string[]) {
-        return forkJoin(
-                this.userService.getCurrentUser(),
-                this.elsaPurchaseService.getAll('ProductName=LONN_UTVIDET')
-            )
+        return this.elsaPurchaseService
+            .getPurchaseByProductName('LONN_UTVIDET')
             .pipe(
-                map(([user, purchasesOnUser]) => purchasesOnUser.find(p => p.GlobalIdentity === user.GlobalIdentity)),
                 switchMap(p => !p
                     ? of([])
                     : super.GetAll(query, expands)
