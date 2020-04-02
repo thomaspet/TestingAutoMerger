fdf074740eb01c9697f0e6282e419494d459239d
Anders Urrang <anders.urrang@unimicro.no>
2020-04-02 09:19:09 +0200

    chore: make sure we only bundle the norwegian moment locale (english on by default)

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 module.exports = {
     plugins: [
         new MomentLocalesPlugin({
-            localesToKeep: ['es-us', 'nb'],
+            localesToKeep: ['nb'],
         }),
     ],
     resolve: {
