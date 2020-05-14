a3735a2da0b07c19a942a240bac928d3becfd7d2
Anders Urrang <anders.urrang@unimicro.no>
2020-05-14 14:22:46 +0200

    feat(app): dont run canDeactivate when changing company
    Some of our canDeactivate checks have timing issues leading to data being saved on the wrong company when canDeactivate is checked during the company change flow.
    
    Since theres no way we can guarantee that the canDeactivate checks will always finish saving before completing with a boolean value we can't rely on them for company switching.
    
    The safest option is to just discard unsaved changes when changing company.
    The user will get a warning about this that they can chose to never see again.
    
    PB-1254

--- src/app/canDeactivateGuard.ts
+++ src/app/canDeactivateGuard.ts
@@ -1,16 +1,35 @@
 import {CanDeactivate} from '@angular/router';
-import { Injectable } from "@angular/core";
+import {Injectable} from '@angular/core';
+import {UniModalService, ChangingCompanyWarning} from '@uni-framework/uni-modal';
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class CanDeactivateGuard implements CanDeactivate<any> {
+    constructor(private modalService: UniModalService) {}
+
     public canDeactivate(component, currentRoute, currentState, nextState) {
-        if (nextState.url.includes('/login')) {
+        if (nextState.url.startsWith('/init')) {
             return true;
         }
 
         if (component.canDeactivate) {
             const routeToActivate = nextState && nextState.url;
-            return component.canDeactivate(routeToActivate);
+            if (nextState.url === '/reload') {
+                const showWarningDialog = !localStorage.getItem('changing_company_warning_hidden');
+                return !showWarningDialog || this.modalService.open(ChangingCompanyWarning).onClose.pipe(map(res => {
+                    if (res) {
+                        if (res.hideWarning) {
+                            localStorage.setItem('changing_company_warning_hidden', 'true');
+                        }
+
+                        return res.canChangeCompany;
+                    }
+
+                    return false;
+                }));
+            } else {
+                return component.canDeactivate(routeToActivate);
+            }
         }
 
         return true;
