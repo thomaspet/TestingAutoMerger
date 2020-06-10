fcd6245e13e3d5c32d998bc7a17b12a4f1328bd1
MODALEN\tj <tj@unimicro.no>
2020-06-10 14:47:37 +0200

    expand financialyear validation to 10 years

--- src/app/components/layout/navbar/company-dropdown/companyDropdown.ts
+++ src/app/components/layout/navbar/company-dropdown/companyDropdown.ts
@@ -136,12 +136,12 @@ export class UniCompanyDropdown {
     public promptToCreateNewYear(newSelectedYear: number, previousSelectedYear: number) {
         // add simple validation, max 5 years back, 2 years ahead
         const currentDateYear = new Date().getFullYear();
-        if (newSelectedYear > currentDateYear + 2 || newSelectedYear < currentDateYear - 5) {
+        if (newSelectedYear > currentDateYear + 2 || newSelectedYear < currentDateYear - 10) {
             this.toastService.addToast(
                 'Ugyldig år valgt',
                 ToastType.bad,
                 ToastTime.medium,
-                'Kan ikke velge et nytt regnskapsår som er mer enn 2 år frem i tid, eller 5 år tilbake i tid'
+                'Kan ikke velge et nytt regnskapsår som er mer enn 2 år frem i tid, eller 10 år tilbake i tid'
             );
             return;
         }
