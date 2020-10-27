24f39c55f6cfaf5c303dff974031bfe909ec7d84
Sondre Østebøvik <50408839+UniSondre@users.noreply.github.com>
2020-10-27 17:18:24 +0100

    hotfix(new-company): dont set pending if bureau
    #6692

--- src/app/components/license-info/company-list/company-list.ts
+++ src/app/components/license-info/company-list/company-list.ts
@@ -181,7 +181,10 @@ export class CompanyList {
             });
         } else {
             this.modalService.open(NewCompanyModal, {
-                data: { contractID: this.contractID }
+                data: {
+                    contractID: this.contractID,
+                    contractType: this.contractType
+                }
             });
         }
     }
