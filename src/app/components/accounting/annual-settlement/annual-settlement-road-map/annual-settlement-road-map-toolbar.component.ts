e651aba78c81f00b29194d59df3232d23710cf6d
jorgeunimicro <jorge@unimicro.no>
2021-01-22 12:46:52 +0100

    use dropdown component on road map toolbar and add a confirm modal

--- src/app/components/accounting/annual-settlement/annual-settlement-road-map/annual-settlement-road-map-toolbar.component.ts
+++ src/app/components/accounting/annual-settlement/annual-settlement-road-map/annual-settlement-road-map-toolbar.component.ts
@@ -1,27 +1,32 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {IUniSaveAction} from '@uni-framework/save/save';
-import {IToolbarConfig} from '@app/components/common/toolbar/toolbar';
+import {IToolbarConfig, ToolbarDropdownButton} from '@app/components/common/toolbar/toolbar';
 import {TabService, UniModules} from '@app/components/layout/navbar/tabstrip/tabService';
 import {AnnualSettlementService} from '@app/components/accounting/annual-settlement/annual-settlement.service';
+import {ConfirmActions, UniModalService} from '@uni-framework/uni-modal';
+import {switchMap} from 'rxjs/operators';
+import {of} from 'rxjs/observable/of';
 
 @Component({
     selector: 'annual-settlement-road-map-toolbar-component',
     template: `
         <uni-toolbar
             [config]="toolbarconfig"
-            [saveactions]="saveActions"
+            [dropdownButton]="saveActions"
         ></uni-toolbar>
     `
 })
 export class AnnualSettlementRoadMapToolbarComponent {
     @Input() annualSettlement;
     @Output() runAction = new EventEmitter(true);
-    saveActions: IUniSaveAction[] = [];
+    saveActions: ToolbarDropdownButton;
     toolbarconfig: IToolbarConfig = {
         title: 'Årsavslutning'
     };
 
-    constructor(private service: AnnualSettlementService, private tabService: TabService) {
+    constructor(
+        private service: AnnualSettlementService,
+        private modalService: UniModalService,
+        private tabService: TabService) {
         this.addTab();
     }
 
@@ -33,25 +38,41 @@ export class AnnualSettlementRoadMapToolbarComponent {
     }
     ngOnInit() {
         if (this.annualSettlement) {
-            this.saveActions = [
-                <IUniSaveAction> {
-                    label: 'Flere valg'
-                },
-                <IUniSaveAction>{
-                    action: (done) => {
-                        this.service
-                            .reset(this.annualSettlement)
-                            .subscribe(() => {
-                                this.runAction.emit({
-                                    name: 'reset-annualsettlement'
-                                });
-                                done();
+            this.saveActions = {
+                label: 'Flere valg',
+                class: 'secondary',
+                items: [
+                    {
+                        action: () => {
+                            this.modalService.confirm({
+                                buttonLabels: {
+                                    accept: 'Nullstill',
+                                    cancel: 'Avbryt'
+                                },
+                                header: 'Nullstill årsavslutning',
+                                message: 'Ved nullstilling av årsavslutning vil vi slette det du har gjort på årsavslutning slik at du ' +
+                                    'kan starte på nytt. Dersom du allerede har levert til Altinn er det viktig at du fullfører ' +
+                                    'veiviser og leverer på nytt slik at du erstatter innleveringen din i Altinn.<br/><br/>' +
+                                    'Ønsker du å nullstille årsavslutningen?'
+                            }).onClose.pipe(
+                                switchMap((modalResult) => {
+                                    if (modalResult === ConfirmActions.ACCEPT) {
+                                        return this.service.reset(this.annualSettlement);
+                                    }
+                                    return of(ConfirmActions.REJECT);
+                                })
+                            ).subscribe((actionResult) => {
+                                if (actionResult !== ConfirmActions.REJECT) {
+                                    this.runAction.emit({
+                                        name: 'reset-annualsettlement'
+                                    });
+                                }
                             });
-                    },
-                    label: 'Reset annual settlement',
-                    disabled: false
-                }
-            ];
+                        },
+                        label: 'Reset annual settlement'
+                    }
+                ]
+            };
         }
     }
 }
