5963c769f948a1c0537dfa6d5b9a66a494a23896
Sondre Østebøvik <50408839+UniSondre@users.noreply.github.com>
2021-02-02 11:33:01 +0100

    fix(license-info): new client contracttypes
    RIG-516
    pr: #7061

--- src/app/components/license-info/new-contract-modal/new-contract-modal.ts
+++ src/app/components/license-info/new-contract-modal/new-contract-modal.ts
@@ -36,6 +36,7 @@ export class NewContractModal implements IUniModal {
         this.busy = true;
         this.elsaContractService.getCustomContractTypes().subscribe(
             contractTypes => {
+                contractTypes = contractTypes.filter(c => c.ContractType > 20);
                 this.contractTypes = contractTypes;
                 this.selectedContractType = contractTypes[0];
                 this.busy = false;
@@ -53,7 +54,6 @@ export class NewContractModal implements IUniModal {
         contract.Name = this.contractName;
         contract.ContractType = this.selectedContractType.ContractType;
         contract.CustomerID = this.options.data.customerID;
-        console.log(contract);
         this.elsaContractService.createContract(contract).subscribe(
             newContract => {
                 this.busy = false;
