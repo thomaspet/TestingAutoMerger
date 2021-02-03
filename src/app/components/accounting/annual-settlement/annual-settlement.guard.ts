cb8eb719342bf8aea5c54ce218340646f1b4c986
jorgeunimicro <jorge@unimicro.no>
2021-02-03 11:06:39 +0100

    not allow go into a route that is not allowed by company type

---
+++ src/app/components/accounting/annual-settlement/annual-settlement.guard.ts
@@ -0,0 +1,12 @@
+import {CanDeactivate} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {AnnualSettlementService} from '@app/components/accounting/annual-settlement/annual-settlement.service';
+
+@Injectable()
+export class CompanyAllowedByTypeGuard implements CanDeactivate<any> {
+    constructor(private annualSettlementService: AnnualSettlementService) {
+    }
+    public canDeactivate(component, currentRoute, currentState, nextState) {
+        return this.annualSettlementService.checkIfCompanyIsAllowedByType();
+    }
+}
