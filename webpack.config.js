37b7a71b650b84d9ff5bea6bbe868ad91f309b87
Anders Urrang <urrang@users.noreply.github.com>
2021-02-04 15:34:21 +0100

    fix(build): exclude node_modules from sourcemap generation (#7077)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 // Use this file for extending the default Angular CLI webpack config
 // The @angular-builders/custom-webpack dev dependency allows us to do this
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = (config, options, targetOptions) => {
-    const sourcemaps = targetOptions.target === 'serve' ? 'eval' : undefined;
-    config.devtool = sourcemaps;
+    if (targetOptions.target === 'serve') {
+        config.plugins.push(
+            new webpack.SourceMapDevToolPlugin({
+                exclude: /node_modules/
+            })
+        );
+    }
+
     config.resolve.alias['chart.js'] = 'chart.js/dist/Chart.min.js';
     config.plugins.push(
         new MomentLocalesPlugin({
@@ -14,17 +21,3 @@ module.exports = (config, options, targetOptions) => {
 
     return config;
 };
-
-// module.exports = {
-//     plugins: [
-//         new MomentLocalesPlugin({
-//             localesToKeep: ['nb'],
-//         }),
-//     ],
-//     resolve: {
-//         alias: {
-//             // Avoid using the bundled chartjs version with moment
-//             'chart.js': 'chart.js/dist/Chart.min.js'
-//         }
-//     }
-// }
